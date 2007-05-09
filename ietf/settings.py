ab92a838a9937167c9a719d163d8116153043a00
Bill Fenner <fenner@fenron.net>
2007-05-09 22:08:48 +0000

    The Latin1 encoding setting actually causes problems, since django  assumes that data coming back from the database is unicode so the  Latin1 data fails to decode properly.  - Legacy-Id: 114

--- ietf/settings.py
+++ ietf/settings.py
@@ -19,7 +19,6 @@ DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysq
 DATABASE_NAME = 'django_idtracker'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'django'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'playing'         # Not used with sqlite3.
-DATABASE_OPTIONS={'charset': 'Latin1',}
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
