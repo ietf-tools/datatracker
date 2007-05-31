2de4f6d6d73573596af943203a16bd08a713459a
Henrik Levkowetz <henrik@levkowetz.com>
2007-05-31 21:11:03 +0000

    More test setup  - Legacy-Id: 199

---
+++ test/settings_local_test.py
@@ -0,0 +1,14 @@
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'oa0w4@r(=_0qvlnt#9c*_)3rclq4m^zd+19z)zk!=fo=d!ibyw'
+
+DATABASE_NAME = 'ietfdb'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'django'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'zappa00'         # Not used with sqlite3.
+
+USE_I18N = False
+
+TIME_ZONE = 'Europe/Stockholm'
+
+SERVER_MODE = 'test'
+
