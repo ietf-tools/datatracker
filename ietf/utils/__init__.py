4a289b821f62023777b041c56465e28bbae8fb7d
Henrik Levkowetz <henrik@levkowetz.com>
2007-05-31 09:44:12 +0000

    Minor cleanup based on pyflakes feedback  - Legacy-Id: 188

--- ietf/utils/__init__.py
+++ ietf/utils/__init__.py
@@ -4,3 +4,11 @@ from cache_foreign_key import FKAsOneToOne
 from templated_form import makeTemplatedForm
 
 makeFormattingForm = makeTemplatedForm
+
+__all__ = ["orl", "flattenl", "log", "FKAsOneToOne", "makeTemplatedForm", "makeFormattingForm"]
+
+# See http://docs.python.org/tut/node8.html regarding the use of __all__ and
+# also regarding the practice of using "from xxx import *" in interactive
+# sessions vs. in source files.
+
+
