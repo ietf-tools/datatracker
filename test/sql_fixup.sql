3ff684ff06e1e51b9aa370af7610f866acd158b7
Bill Fenner <fenner@fenron.net>
2007-06-01 18:25:39 +0000

    SQL commands to convert IETF database dump to django schema.  - Legacy-Id: 208

---
+++ test/sql_fixup.sql
@@ -0,0 +1,28 @@
+ALTER TABLE  `email_addresses` ADD  `id` INT NOT NULL AUTO_INCREMENT
+ PRIMARY KEY FIRST ;
+ALTER TABLE  `postal_addresses` ADD  `id` INT NOT NULL AUTO_INCREMENT
+ PRIMARY KEY FIRST ;
+ALTER TABLE  `phone_numbers` ADD  `id` INT NOT NULL AUTO_INCREMENT
+ PRIMARY KEY FIRST ;
+
+ALTER TABLE  `ballots` DROP PRIMARY KEY;
+ALTER TABLE  `ballots` ADD  `id` INT NOT NULL AUTO_INCREMENT
+ PRIMARY KEY FIRST ;
+ALTER TABLE  `ballots` ADD UNIQUE (`ballot_id` , `ad_id`);
+ALTER TABLE  `ballots_comment` DROP PRIMARY KEY;
+ALTER TABLE  `ballots_comment` ADD  `id` INT NOT NULL AUTO_INCREMENT
+ PRIMARY KEY FIRST ;
+ALTER TABLE  `ballots_comment` ADD UNIQUE (`ballot_id` , `ad_id`);
+ALTER TABLE  `ballots_discuss` DROP PRIMARY KEY;
+ALTER TABLE  `ballots_discuss` ADD  `id` INT NOT NULL AUTO_INCREMENT
+ PRIMARY KEY FIRST ;
+ALTER TABLE  `ballots_discuss` ADD UNIQUE (`ballot_id` , `ad_id`);
+ALTER TABLE  `iesg_history` DROP PRIMARY KEY;
+ALTER TABLE  `iesg_history` ADD  `id` INT NOT NULL AUTO_INCREMENT
+ PRIMARY KEY FIRST ;
+
+
+INSERT INTO announced_from VALUES (98, 'IETF Executive Director <exec-director@ietf.org>', NULL);
+INSERT INTO announced_to VALUES (9, 'Unknown', NULL);
+INSERT INTO area_status VALUES (3, 'Unknown');
+
