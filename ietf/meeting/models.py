600002f1f80d20012410c5685d87d2798218cc3c
Henrik Levkowetz <henrik@levkowetz.com>
2007-06-04 00:50:48 +0000

    Cleaning out miscellaneous non-functional stuff from ietf/meeting/models.py  - Legacy-Id: 223

--- ietf/meeting/models.py
+++ ietf/meeting/models.py
@@ -1,11 +1,1 @@
-from django.db import models
-#from django.db import models
-
-from ietf.proceedings import models
-#from ietf.proceedings.models import Meeting, MeetingVenue, WgMeetingSession, WgAgenda
-#from ietf.proceedings.models import NonSession, NonSessionRef,SessionName
- 
-import datetime
-
-
 # Create your models here.
