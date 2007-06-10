341811e84e6026e14f356e28a0ceef758db6aff7
Henrik Levkowetz <henrik@levkowetz.com>
2007-06-10 14:14:16 +0000

    Patched a bug in BeautifulSoup (discovered by PyFlakes) and quietened the remaining nonconsequential comments from PyFlakes regarding BeautifulSoup  - Legacy-Id: 290

--- ietf/contrib/BeautifulSoup.py
+++ ietf/contrib/BeautifulSoup.py
@@ -835,7 +835,7 @@ def isString(s):
     """Convenience method that works with all 2.x versions of Python
     to determine whether or not something is stringlike."""
     try:
-        return isinstance(s, unicode) or isintance(s, basestring) 
+        return isinstance(s, unicode) or isinstance(s, basestring) 
     except NameError:
         return isinstance(s, str)
 
