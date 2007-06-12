890b8a1ada899a3684b0d8d73dba1072100e2b46
Henrik Levkowetz <henrik@levkowetz.com>
2007-06-12 18:34:26 +0000

    Fix potential exception in soup2html again.  - Legacy-Id: 341

--- ietf/utils/soup2text.py
+++ ietf/utils/soup2text.py
@@ -87,7 +87,7 @@ def render(node, encoding='latin-1', pre=False):
                         blocks.append(child.text+"\n\n")
                         node.is_block = True
                     else:
-                        if child.name in space_tags and not (words[-1] and words[-1][-1] in [" ", "\t", "\n"]):
+                        if child.name in space_tags and not (words and words[-1] and words[-1][-1] in [" ", "\t", "\n"]):
                             words.append(" ")
                         words.append(child.text)
         else:
