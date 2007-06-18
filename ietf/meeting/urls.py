48032496a2e821599ab5f19bdb2df7e0306e897a
Michael Lee <michael.lee@neustar.biz>
2007-06-18 18:15:14 +0000

    Update urls.py of meeting to fix redirect for meeting materials page and agenda page.  - Legacy-Id: 498

--- ietf/meeting/urls.py
+++ ietf/meeting/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns
 from ietf.meeting import views
 
 urlpatterns = patterns('',
-    (r'^(?P<meeting_num>\d+)/agenda.(?P<html_or_txt>\S+)$', views.show_html_agenda),
-    (r'^(?P<meeting_num>\d+)/materials.html$', views.show_html_materials),
+    (r'^(?P<meeting_num>\d+)/agenda-(?P<html_or_txt>\S+)/$', views.show_html_agenda),
+    (r'^(?P<meeting_num>\d+)/materials/$', views.show_html_materials),
 )
 
