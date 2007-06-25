17c445e74a86e3c4df68cf7df425ed14e46ee78d
Michael Lee <michael.lee@neustar.biz>
2007-06-25 05:37:34 +0000

    nwg_list_submit - restore MultiEmailField property of admin field. Defining admin in nonwg_fields causes a bug which does not pre-populate admin field when editing. Ticket will be created.  - Legacy-Id: 626

--- ietf/mailinglists/views.py
+++ ietf/mailinglists/views.py
@@ -32,7 +32,7 @@ nonwg_fields = {
     'ds_name': None,
     'ds_email': None,
     'msg_to_ad': None,
-    #'admin': MultiEmailField(label='List Administrator(s)', widget=forms.Textarea(attrs={'rows': 3, 'cols': 50})),
+    'admin': MultiEmailField(label='List Administrator(s)', widget=forms.Textarea(attrs={'rows': 3, 'cols': 50})),
 }
 
 nonwg_attrs = {
@@ -43,7 +43,7 @@ nonwg_attrs = {
 
 nonwg_widgets = {
     'list_url': UrlMultiWidget(choices=(('http://', 'http://'), ('https://', 'https://'), ('mailto:', 'mailto:'))),
-    'admin': forms.Textarea(attrs = {'rows': 3, 'cols': 50}),
+    #'admin': forms.Textarea(attrs = {'rows': 3, 'cols': 50}),
     'purpose': forms.Textarea(attrs = {'rows': 4, 'cols': 70}),
     'subscribe_url': UrlMultiWidget(choices=(('n/a', 'Not Applicable'), ('http://', 'http://'), ('https://', 'https://'))),
     'subscribe_other': forms.Textarea(attrs = {'rows': 3, 'cols': 50}),
