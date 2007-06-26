9d5faab01b6db0933f93b88f29d687d99eac607c
Michael Lee <michael.lee@neustar.biz>
2007-06-26 19:16:58 +0000

    Fix IESG announcements script to handle mixed-intended_status fleet (group of documents)  - Legacy-Id: 659

--- ietf/iesg/views.py
+++ ietf/iesg/views.py
@@ -3,6 +3,8 @@
 from ietf.idtracker.models import IDInternal, InternetDraft
 from django.views.generic.list_detail import object_list
 from django.http import Http404
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 import datetime 
 
 def date_threshold():
@@ -18,14 +20,32 @@ def inddocs(request):
 
 def wgdocs(request,cat):
    is_recent = 0
+   queryset_list=[]
+   queryset_list_doc=[]
    if cat == 'recent':
       is_recent = 1
-      queryset_list = InternetDraft.objects.filter(b_approve_date__gte = date_threshold(), intended_status__in=[1,2,6,7],idinternal__via_rfc_editor=0).order_by("-b_approve_date")
-      queryset_list_doc = InternetDraft.objects.filter(b_approve_date__gte = date_threshold(), intended_status__in=[3,5],idinternal__via_rfc_editor=0).order_by("-b_approve_date")
+      queryset = InternetDraft.objects.filter(b_approve_date__gte = date_threshold(), intended_status__in=[1,2,6,7],idinternal__via_rfc_editor=0,idinternal__primary_flag=1).order_by("-b_approve_date")
+      queryset_doc = InternetDraft.objects.filter(b_approve_date__gte = date_threshold(), intended_status__in=[3,5],idinternal__via_rfc_editor=0, idinternal__primary_flag=1).order_by("-b_approve_date")
    elif cat == 'previous':
-      queryset_list = InternetDraft.objects.filter(b_approve_date__lt = date_threshold(), b_approve_date__gte = '1998-10-15', intended_status__in=[1,2,6,7]).order_by("-b_approve_date")
-      queryset_list_doc = InternetDraft.objects.filter(b_approve_date__lt = date_threshold(), b_approve_date__gte = '1998-10-15', intended_status__in=[3,5]).order_by("-b_approve_date")
+      queryset = InternetDraft.objects.filter(b_approve_date__lt = date_threshold(), b_approve_date__gte = '1997-12-1', intended_status__in=[1,2,6,7],idinternal__via_rfc_editor=0,idinternal__primary_flag=1).order_by("-b_approve_date")
+      queryset_doc = InternetDraft.objects.filter(b_approve_date__lt = date_threshold(), b_approve_date__gte = '1998-10-15', intended_status__in=[3,5],idinternal__via_rfc_editor=0,idinternal__primary_flag=1).order_by("-b_approve_date")
    else:
      raise Http404
-   return object_list(request, queryset=queryset_list, template_name='iesg/ietf_doc.html', allow_empty=True, extra_context={'object_list_doc':queryset_list_doc, 'is_recent':is_recent })
+   for item in list(queryset):
+      queryset_list.append(item)
+      try:
+        ballot_id=item.idinternal.ballot_id
+      except AttributeError:
+        ballot_id=0
+      for sub_item in list(InternetDraft.objects.filter(idinternal__ballot=ballot_id,idinternal__primary_flag=0)):
+         queryset_list.append(sub_item)
+   for item2 in list(queryset_doc):
+      queryset_list_doc.append(item2)
+      try:
+        ballot_id=item2.idinternal.ballot_id
+      except AttributeError:
+        ballot_id=0
+      for sub_item2 in list(InternetDraft.objects.filter(idinternal__ballot=ballot_id,idinternal__primary_flag=0)):
+         queryset_list_doc.append(sub_item2)
+   return render_to_response( 'iesg/ietf_doc.html', {'object_list': queryset_list, 'object_list_doc':queryset_list_doc, 'is_recent':is_recent}, context_instance=RequestContext(request) )
 
