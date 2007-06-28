8c1d9e62fd9e22878fa1edd268b84f10f8850057
Henrik Levkowetz <henrik@levkowetz.com>
2007-06-28 01:00:05 +0000

    Set version of the trunk to 2.xx-trunk after branching off 2.00  - Legacy-Id: 729

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "2.00"
+__version__ = "2.01-dev"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev:$"
+__rev__     = "$Rev:$ (dev) Latest release: Rev. 726 "
 
 __id__      = "$Id$"
