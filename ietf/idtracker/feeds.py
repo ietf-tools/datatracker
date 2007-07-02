af9236fb58dfdd5310c11ea284088fbbfe88b6bd
Bill Fenner <fenner@fenron.net>
2007-07-02 18:10:20 +0000

    Use IDInternal as the primary object, since RFCs can have comments.  - Legacy-Id: 827

--- ietf/idtracker/feeds.py
+++ ietf/idtracker/feeds.py
@@ -2,18 +2,23 @@
 
 from django.contrib.syndication.feeds import Feed, FeedDoesNotExist
 from django.utils.feedgenerator import Atom1Feed
-from ietf.idtracker.models import InternetDraft, DocumentComment
+from ietf.idtracker.models import IDInternal, DocumentComment
 import datetime
+import re
 
 class DocumentComments(Feed):
     feed_type = Atom1Feed
     def get_object(self, bits):
 	if len(bits) != 1:
-	    raise InternetDraft.DoesNotExist
-	return InternetDraft.objects.get(filename=bits[0])
+	    raise IDInternal.DoesNotExist
+	rfc = re.match('rfc(\d+)', bits[0])
+	if rfc:
+	    return IDInternal.objects.get(draft_id=int(rfc.group(1)), rfc_flag=1)
+	else:
+	    return IDInternal.objects.get(draft__filename=bits[0], rfc_flag=0)
 
     def title(self, obj):
-	return "I-D Tracker comments for %s" % obj.filename
+	return "I-D Tracker comments for %s" % obj.document().filename
 
     def link(self, obj):
 	if obj is None:
@@ -21,10 +26,10 @@ class DocumentComments(Feed):
 	return obj.get_absolute_url()
 
     def description(self, obj):
-	self.title(obj)
+	return self.title(obj)
 
     def items(self, obj):
-	return DocumentComment.objects.filter(document=obj.id_document_tag, public_flag=1).order_by("-date")[:15]
+	return obj.public_comments().order_by("-date")[:15]
 
     def item_pubdate(self, item):
 	time = datetime.time(*[int(t) for t in item.time.split(":")])
