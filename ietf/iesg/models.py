3742c3f69f736b7ed0735de39c2646ce2923f057
Bill Fenner <fenner@fenron.net>
2007-07-09 15:54:34 +0000

    Add get_absolute_url() to the TelechatMinutes model.  - Legacy-Id: 872

--- ietf/iesg/models.py
+++ ietf/iesg/models.py
@@ -6,6 +6,8 @@ class TelechatMinutes(models.Model):
     telechat_date = models.DateField(null=True, blank=True)
     telechat_minute = models.TextField(blank=True)
     exported = models.IntegerField(null=True, blank=True)
+    def get_absolute_url(self):
+	return "/iesg/telechat/%d/" % self.id
     class Meta:
         db_table = 'telechat_minutes'
 
