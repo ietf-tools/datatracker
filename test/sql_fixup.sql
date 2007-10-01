b4ca9caa8265f0fbf51065d03dbdc81fd7646825
Michael Lee <michael.lee@neustar.biz>
2007-10-01 03:11:07 +0000

    chnage the sql_fixup.sql - s/MailingList/mailing_list to use real table name  - Legacy-Id: 951

--- test/sql_fixup.sql
+++ test/sql_fixup.sql
@@ -1,5 +1,5 @@
 -- This file holds needed corrections to the database until they have been applied to
 -- the live database.  This file is applied after importing a new dump of the live DB.
 
-ALTER TABLE `MailingList` ADD `require_tmda` INT( 10 ) NOT NULL AFTER `domain_name` ;
+ALTER TABLE `mailing_list` ADD `require_tmda` INT( 10 ) NOT NULL AFTER `domain_name` ;
 
