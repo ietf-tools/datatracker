85ee43f7ad81e74ecbaa83ffeef5113ab32b9039
Henrik Levkowetz <henrik@levkowetz.com>
2008-11-21 17:28:07 +0000

    Set version info to release version 2.13 before branching.  - Legacy-Id: 1290

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "2.13-dev"
+__version__ = "2.13"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$ (dev) Latest release: Rev. 1265 "
+__rev__     = "$Rev$"
 
 __id__      = "$Id$"
