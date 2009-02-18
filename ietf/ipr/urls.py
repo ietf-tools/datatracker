293898ef3f222ab44b2757050c835ff567bd1750
Henrik Levkowetz <henrik@levkowetz.com>
2009-02-18 10:39:27 +0000

    Add a machine-readable view listing IPR declarations per draft/rfc  - Legacy-Id: 1321

--- ietf/ipr/urls.py
+++ ietf/ipr/urls.py
@@ -6,6 +6,7 @@ from ietf.ipr import models, views, new, search
 urlpatterns = patterns('',
      (r'^$', views.showlist),
      (r'^about/$', views.default),
+     (r'^by-draft/$', views.list_drafts),
      #(r'^(?P<removed>removed/)?(?P<ipr_id>\d+)/$', views.show),
      (r'^(?P<ipr_id>\d+)/$', views.show),
      (r'^update/$', views.updatelist),
--- ietf/ipr/views.py
+++ ietf/ipr/views.py
@@ -3,8 +3,9 @@
 import django.utils.html
 from django.shortcuts import render_to_response as render, get_object_or_404
 from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.utils.html import escape
-from django.http import Http404
+from django.http import HttpResponse, Http404
 from ietf.idtracker.models import IETFWG
 from ietf.ipr.models import IprDetail, SELECT_CHOICES, LICENSE_CHOICES
 from ietf.ipr.view_sections import section_table
@@ -41,6 +42,26 @@ def list_all(request, template):
             'specific_disclosures': specific_disclosures.order_by(* ['-submitted_date', ] ),
             'thirdpty_disclosures': thirdpty_disclosures.order_by(* ['-submitted_date', ] ),
         }, context_instance=RequestContext(request) )
+
+def list_drafts(request):
+    iprs = IprDetail.objects.filter(status=1)
+    docipr = {}
+    docs = []
+    for ipr in iprs:
+        for draft in ipr.drafts.all():
+            name = draft.document.filename
+            if not name in docipr:
+                docipr[name] = []
+            docipr[name] += [ ipr.ipr_id ]
+        for rfc in ipr.rfcs.all():
+            name = "RFC%04d" % rfc.document.rfc_number
+            if not name in docipr:
+                docipr[name] = []
+            docipr[name] += [ ipr.ipr_id ]
+    docs = [ {"name":key, "iprs":value, } for key,value in docipr.items() ]
+    return HttpResponse(render_to_string("ipr/drafts.html", { "docs":docs, },
+                    context_instance=RequestContext(request)),
+                    mimetype="text/plain")
 
 # Details views
 
