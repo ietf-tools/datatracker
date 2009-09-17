14f1011e993bcedf02333a3dbf1c49fac20974dd
Pasi Eronen <pe@iki.fi>
2009-09-17 17:14:33 +0000

    Fix author name (Unicode) and pubdate (datetime vs. date) for Django 1.x  - Legacy-Id: 1674

--- ietf/ipr/feeds.py
+++ ietf/ipr/feeds.py
@@ -3,6 +3,7 @@
 from django.contrib.syndication.feeds import Feed
 from django.utils.feedgenerator import Atom1Feed
 from ietf.ipr.models import IprDetail
+from datetime import datetime, time
 
 class LatestIprDisclosures(Feed):
     feed_type = Atom1Feed
@@ -16,11 +17,18 @@ class LatestIprDisclosures(Feed):
         return IprDetail.objects.filter(status__in=[1,3]).order_by('-submitted_date')[:15]
         
     def item_pubdate(self, item):
-        return item.submitted_date
+        # this method needs to return a datetime instance, even
+        # though the database has only date, not time 
+	return datetime.combine(item.submitted_date, time(0,0,0))
     def item_author_name(self, item):
 	s = item.get_submitter()
         if s:
-            return unicode(s.name, encoding='utf-8', errors='replace')
+            if isinstance(s.name, unicode):
+                # for django.VERSION[0] > 0
+                return s.name
+            else:
+                # for django.VERSION[0] == 0
+                return unicode(s.name, encoding='utf-8', errors='replace')
         return None
     def item_author_email(self, item):
 	s = item.get_submitter()
