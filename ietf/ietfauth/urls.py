2ec7c6ea09dc356e6c5cb12b34749f83fba6400b
Pasi Eronen <pe@iki.fi>
2009-10-08 17:41:15 +0000

    Removed account creation/password changing functionality  - Legacy-Id: 1722

--- ietf/ietfauth/urls.py
+++ ietf/ietfauth/urls.py
@@ -7,13 +7,8 @@ from ietf.my.views import my
 urlpatterns = patterns('django.contrib.auth.views',
 	(r'^login/$', 'login'),
 	(r'^logout/$', 'logout'),
-	(r'^password_change/$', 'password_change'),
-	(r'^password_change/done/$', 'password_change_done'),
 )
 urlpatterns += patterns('',
         (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'registration/account_info.html'}),
-	(r'^request/$', views.password_request),
-	(r'^return/$', views.password_return),
-	(r'^return/(?P<action>\w+)/$', 'django.views.generic.simple.direct_to_template', {'template': 'registration/action_done.html'}),
 	(r'^profile/$', my)
 )
