304824268e4c1d8b86d3b56d46a5037da039da94
Pasi Eronen <pe@iki.fi>
2009-11-07 07:26:58 +0000

    Support UTF-8 in emails (merged from Lars)  - Legacy-Id: 1803

--- ietf/utils/mail.py
+++ ietf/utils/mail.py
@@ -114,7 +114,10 @@ def send_mail(request, to, frm, subject, template, context, *args, **kwargs):
     return send_mail_text(request, to, frm, subject, txt, *args, **kwargs)
 
 def send_mail_text(request, to, frm, subject, txt, cc=None, extra=None, toUser=None, bcc=None):
-    msg = MIMEText(txt)
+    if isinstance(txt, unicode):
+        msg = MIMEText(encode('utf-8'), 'plain', 'UTF-8')
+    else:
+        msg = MIMEText(txt)
     if isinstance(frm, tuple):
 	frm = formataddr(frm)
     if isinstance(to, list) or isinstance(to, tuple):
