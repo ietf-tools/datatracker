9e23b479b9c5ef586738c2addad2865b0cb2a3a8
Pasi Eronen <pe@iki.fi>
2009-11-23 19:39:55 +0000

    Replaced non-WG mailing list page with redirect to ietf.org  - Legacy-Id: 1871

--- ietf/mailinglists/urls.py
+++ ietf/mailinglists/urls.py
@@ -2,14 +2,13 @@
 
 from django.conf.urls.defaults import patterns
 from ietf.idtracker.models import Area, IETFWG
-from ietf.mailinglists.models import NonWgMailingList
 
 urlpatterns = patterns('django.views.generic.list_detail',
      (r'^area/$', 'object_list', { 'queryset': Area.objects.filter(status=1).select_related().order_by('acronym.acronym'), 'template_name': 'mailinglists/areas_list.html' }),
-     (r'^nonwg/$', 'object_list', { 'queryset': NonWgMailingList.objects.filter(status__gt=0) }),
-     (r'wg/$', 'object_list', { 'queryset': IETFWG.objects.filter(email_archive__startswith='http'), 'template_name': 'mailinglists/wgwebmail_list.html' }),
+     (r'^wg/$', 'object_list', { 'queryset': IETFWG.objects.filter(email_archive__startswith='http'), 'template_name': 'mailinglists/wgwebmail_list.html' }),
 )
 urlpatterns += patterns('',
-     (r'^nonwg/update/$', 'django.views.generic.simple.redirect_to', { 'url': 'http://datatracker.ietf.org/list/nonwg/'}),
+     (r'^nonwg/$', 'django.views.generic.simple.redirect_to', { 'url': 'http://www.ietf.org/list/nonwg.html'}),
+     (r'^nonwg/update/$', 'django.views.generic.simple.redirect_to', { 'url': 'http://www.ietf.org/list/nonwg.html'}),
      (r'^request/$', 'django.views.generic.simple.redirect_to', { 'url': 'http://www.ietf.org/list/request.html' }),
 )
