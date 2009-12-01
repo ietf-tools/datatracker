840c82e54b3e86ca3f3baa531d98ab25a6a4be5e
Pasi Eronen <pe@iki.fi>
2009-12-01 08:31:28 +0000

    Removed the now-executed commands from sql_fixup.sql  - Legacy-Id: 1884

--- test/sql_fixup.sql
+++ test/sql_fixup.sql
@@ -1,7 +1,2 @@
 -- This file holds needed corrections to the database until they have been applied to
 -- the live database.  This file is applied after importing a new dump of the live DB.
-delete from auth_user;
-delete from auth_user_groups;
-delete from auth_group;
-delete from auth_group_permissions;
-drop table ietfauth_usermap;
