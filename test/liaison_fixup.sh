dc9d0647af292f65076a232dd90e1bd9548acc56
Pasi Eronen <pe@iki.fi>
2010-02-22 14:38:42 +0000

    Move old (1995-2003) liaison statements from the template file to the database  - Legacy-Id: 2004

---
+++ test/liaison_fixup.sh
@@ -0,0 +1,305 @@
+mv itut-sg15-ls-re-draft-ietf-ccamp-gmpls-ason-reqts.html file800.html
+ln -s file800.html itut-sg15-ls-re-draft-ietf-ccamp-gmpls-ason-reqts.html
+mv itut-sg15-ls-response-g7713-2.html file801.html
+ln -s file801.html itut-sg15-ls-response-g7713-2.html
+mv itut-sg15-ls-response-to-ietf.html file802.html
+ln -s file802.html itut-sg15-ls-response-to-ietf.html
+mv itut-sg13-ls-l1vpn-sg13.pdf file803.pdf
+ln -s file803.pdf itut-sg13-ls-l1vpn-sg13.pdf
+mv itut-sg13-ls-l1vpn-sg13-attachmentA.pdf file804.pdf
+ln -s file804.pdf itut-sg13-ls-l1vpn-sg13-attachmentA.pdf
+mv itut-sg13-ls-l1vpn-sg13-attachmentB.pdf file805.pdf
+ln -s file805.pdf itut-sg13-ls-l1vpn-sg13-attachmentB.pdf
+mv itut-sg13-ls-l2vpn-over-mpls.pdf file806.pdf
+ln -s file806.pdf itut-sg13-ls-l2vpn-over-mpls.pdf
+mv itut-sg13-ls-tdm-mpls.pdf file807.pdf
+ln -s file807.pdf itut-sg13-ls-tdm-mpls.pdf
+mv itut-ls-dismanwg-liaison-reply.pdf file808.pdf
+ln -s file808.pdf itut-ls-dismanwg-liaison-reply.pdf
+mv itut-ls-ngn-studies.pdf file809.pdf
+ln -s file809.pdf itut-ls-ngn-studies.pdf
+mv itut-ls-ip-project.pdf file810.pdf
+ln -s file810.pdf itut-ls-ip-project.pdf
+mv atm-forum-ls-pwe3-wg.txt file811.txt
+ln -s file811.txt atm-forum-ls-pwe3-wg.txt
+mv W3C-ls-minutes-call.txt file812.txt
+ln -s file812.txt W3C-ls-minutes-call.txt
+mv mpls-fr-alliance-ls-pwe3-wg.txt file813.txt
+ln -s file813.txt mpls-fr-alliance-ls-pwe3-wg.txt
+mv iso-iec-ls-jtc-1sc-29-wg11.htm file814.htm
+ln -s file814.htm iso-iec-ls-jtc-1sc-29-wg11.htm
+mv ietf-ccamp-lr-itut-sg15-ason.html file815.html
+ln -s file815.html ietf-ccamp-lr-itut-sg15-ason.html
+mv itut-sg15-ls-xDSL-MIBs-OMCI.html file816.html
+ln -s file816.html itut-sg15-ls-xDSL-MIBs-OMCI.html
+mv itut-sg15-ls-procedures-RSVP.html file817.html
+ln -s file817.html itut-sg15-ls-procedures-RSVP.html
+mv itut-sg15-ls-OMCI.html file818.html
+ln -s file818.html itut-sg15-ls-OMCI.html
+mv ietf-transport-ls-itu-sg15-liaison-response.txt file819.txt
+ln -s file819.txt ietf-transport-ls-itu-sg15-liaison-response.txt
+mv ietf-transport-ls-itu-sg15-RSVP-change.txt file820.txt
+ln -s file820.txt ietf-transport-ls-itu-sg15-RSVP-change.txt
+mv itut-sg4-ls-q811-q812.html file821.html
+ln -s file821.html itut-sg4-ls-q811-q812.html
+mv itut-sg4-ls-structured-cause.html file822.html
+ln -s file822.html itut-sg4-ls-structured-cause.html
+mv t1s1-ls-dtmf-digit-collection.pdf file823.pdf
+ln -s file823.pdf t1s1-ls-dtmf-digit-collection.pdf
+mv itut-sg13-ls-tdm-mpls.htm file824.htm
+ln -s file824.htm itut-sg13-ls-tdm-mpls.htm
+mv 3gpp2-ls-diameter-nasreq.pdf file825.pdf
+ln -s file825.pdf 3gpp2-ls-diameter-nasreq.pdf
+mv 3gpp2-ls-dmu.pdf file826.pdf
+ln -s file826.pdf 3gpp2-ls-dmu.pdf
+mv disman-response-itut-ls.txt file827.txt
+ln -s file827.txt disman-response-itut-ls.txt
+mv atm-forum-ls-ietf-pwe3.html file828.html
+ln -s file828.html atm-forum-ls-ietf-pwe3.html
+mv itut-sg15-ls-ason-status.html file829.html
+ln -s file829.html itut-sg15-ls-ason-status.html
+mv itut-sg15-ls-rsvpte-crldp.html file830.html
+ln -s file830.html itut-sg15-ls-rsvpte-crldp.html
+mv itut-ls-optical-transport.html file831.html
+ln -s file831.html itut-ls-optical-transport.html
+mv itut-ls-optical-transport-attachment.html file832.html
+ln -s file832.html itut-ls-optical-transport-attachment.html
+mv itut-sg17-ls-voip-networks.pdf file833.pdf
+ln -s file833.pdf itut-sg17-ls-voip-networks.pdf
+mv itut-sg17-ls-delayed-contribution.pdf file834.pdf
+ln -s file834.pdf itut-sg17-ls-delayed-contribution.pdf
+mv itut-sg17-ls-x805-end2end-communications.pdf file835.pdf
+ln -s file835.pdf itut-sg17-ls-x805-end2end-communications.pdf
+mv itut-sg17-ls-frame-relay.pdf file836.pdf
+ln -s file836.pdf itut-sg17-ls-frame-relay.pdf
+mv itut-sg17-ls-support-frame-relay.pdf file837.pdf
+ln -s file837.pdf itut-sg17-ls-support-frame-relay.pdf
+mv itut-sg17-ls-proposed-amendment.pdf file838.pdf
+ln -s file838.pdf itut-sg17-ls-proposed-amendment.pdf
+mv itut-sg13-ls-signalling.pdf file839.pdf
+ln -s file839.pdf itut-sg13-ls-signalling.pdf
+mv itut-sg13-ls-ipproject.pdf file840.pdf
+ln -s file840.pdf itut-sg13-ls-ipproject.pdf
+mv itut-sg13-ls-communication.pdf file841.pdf
+ln -s file841.pdf itut-sg13-ls-communication.pdf
+mv itut-sg13-ls-mplsperf.pdf file842.pdf
+ln -s file842.pdf itut-sg13-ls-mplsperf.pdf
+mv itut-tsg13-ls-interworking.pdf file843.pdf
+ln -s file843.pdf itut-tsg13-ls-interworking.pdf
+mv atm-ls.txt file844.txt
+ln -s file844.txt atm-ls.txt
+mv itut-sg15-ls-space-relation-ason.htm file845.htm
+ln -s file845.htm itut-sg15-ls-space-relation-ason.htm
+mv itut-sg15-ls-proposed-template-against.htm file846.htm
+ln -s file846.htm itut-sg15-ls-proposed-template-against.htm
+mv itut-sg15-ls-ason-routing-reqs-attach.htm file847.htm
+ln -s file847.htm itut-sg15-ls-ason-routing-reqs-attach.htm
+mv itut-sg15-ls-ason-routing-reqs.htm file848.htm
+ln -s file848.htm itut-sg15-ls-ason-routing-reqs.htm
+mv itut-sg15-ls-ccamp-ason.pdf file849.pdf
+ln -s file849.pdf itut-sg15-ls-ccamp-ason.pdf
+mv itut-sg15-ls-ccamp-ason-attach.pdf file850.pdf
+ln -s file850.pdf itut-sg15-ls-ccamp-ason-attach.pdf
+mv itusg2-ls-rfc2916.txt file851.txt
+ln -s file851.txt itusg2-ls-rfc2916.txt
+mv oif-ls-network-elements.pdf file852.pdf
+ln -s file852.pdf oif-ls-network-elements.pdf
+echo '<html><body><t>See http://www.w3.org/2003/06/17-w3c-ietf.html</t></body></html>' > file853.html
+mv mobileip-response-itut-ls.txt file854.txt
+ln -s file854.txt mobileip-response-itut-ls.txt
+mv itutsg13-ls-MIPoMPLS.pdf file855.pdf
+ln -s file855.pdf itutsg13-ls-MIPoMPLS.pdf
+mv itutsg13-ls-MIPoMPLS-recommendation.pdf file856.pdf
+ln -s file856.pdf itutsg13-ls-MIPoMPLS-recommendation.pdf
+mv itut-sg16-ls-transport-layer.pdf file857.pdf
+ln -s file857.pdf itut-sg16-ls-transport-layer.pdf
+mv itut-sg16-ls-workshop-disaster.pdf file858.pdf
+ln -s file858.pdf itut-sg16-ls-workshop-disaster.pdf
+mv 3gpp2-ls-schedule.pdf file859.pdf
+ln -s file859.pdf 3gpp2-ls-schedule.pdf
+mv t1m1-ls-oamp.txt file860.txt
+ln -s file860.txt t1m1-ls-oamp.txt
+mv sigtran-ls-sg15.txt file861.txt
+ln -s file861.txt sigtran-ls-sg15.txt
+mv LS-ieee-80211.txt file862.txt
+ln -s file862.txt LS-ieee-80211.txt
+mv LS-t1x1-ccamp.pdf file863.pdf
+ln -s file863.pdf LS-t1x1-ccamp.pdf
+echo '<html><body><t>See http://www.w3.org/2003/03/13-ietf-w3c.html</t></body></html>' > file864.html
+mv ATMFORUM-TDM-over-MPLS.txt file865.txt
+ln -s file865.txt ATMFORUM-TDM-over-MPLS.txt
+mv IETF-RSVP-TE.txt file866.txt
+ln -s file866.txt IETF-RSVP-TE.txt
+mv LS13.htm file867.htm
+ln -s file867.htm LS13.htm
+mv ITU-SG4-RFC3430.txt file868.txt
+ln -s file868.txt ITU-SG4-RFC3430.txt
+mv LS27.htm file869.htm
+ln -s file869.htm LS27.htm
+mv ITU-SG4-DISMAN.txt file870.txt
+ln -s file870.txt ITU-SG4-DISMAN.txt
+mv ls15-39-1.htm file871.htm
+ln -s file871.htm ls15-39-1.htm
+mv ls15-49.htm file872.htm
+ln -s file872.htm ls15-49.htm
+mv ls15-39.htm file873.htm
+ln -s file873.htm ls15-39.htm
+mv ls15-36.htm file874.htm
+ln -s file874.htm ls15-36.htm
+mv ls15-38.htm file875.htm
+ln -s file875.htm ls15-38.htm
+mv ls15-15.htm file876.htm
+ln -s file876.htm ls15-15.htm
+mv D-711.htm file877.htm
+ln -s file877.htm D-711.htm
+mv 3GPP.html file878.html
+ln -s file878.html 3GPP.html
+mv SG2-arpa.txt file879.txt
+ln -s file879.txt SG2-arpa.txt
+mv SG2-ENUM-SECURITY.txt file880.txt
+ln -s file880.txt SG2-ENUM-SECURITY.txt
+mv COM17-LS12IETF.pdf file881.pdf
+ln -s file881.pdf COM17-LS12IETF.pdf
+mv 2133.pdf file882.pdf
+ln -s file882.pdf 2133.pdf
+mv AVT-WG11.txt file883.txt
+ln -s file883.txt AVT-WG11.txt
+mv SP-020842.htm file884.htm
+ln -s file884.htm SP-020842.htm
+mv COM2-055.htm file885.htm
+ln -s file885.htm COM2-055.htm
+mv COM2-056.htm file886.htm
+ln -s file886.htm COM2-056.htm
+mv COM2-076.htm file887.htm
+ln -s file887.htm COM2-076.htm
+mv COM13-LS35.htm file888.htm
+ln -s file888.htm COM13-LS35.htm
+mv COM13-LS03.pdf file889.pdf
+ln -s file889.pdf COM13-LS03.pdf
+mv COM13-LS07.pdf file890.pdf
+ln -s file890.pdf COM13-LS07.pdf
+echo '<html><body><t>See http://www.w3.org/2002/11/12-ietf-w3c</t></body></html>' > file891.html
+mv ITU-tsg15opt.html file892.html
+ln -s file892.html ITU-tsg15opt.html
+mv LS12-16.htm file893.htm
+ln -s file893.htm LS12-16.htm
+mv LS23-16.htm file894.htm
+ln -s file894.htm LS23-16.htm
+mv 3GPP-SIP-IMS.txt file895.txt
+ln -s file895.txt 3GPP-SIP-IMS.txt
+mv 3GPP-SIP-IMS-Response.txt file896.txt
+ln -s file896.txt 3GPP-SIP-IMS-Response.txt
+mv WIG.htm file897.htm
+ln -s file897.htm WIG.htm
+mv SC29-WG11.txt file898.txt
+ln -s file898.txt SC29-WG11.txt
+mv ITU-ietf-1.txt file899.txt
+ln -s file899.txt ITU-ietf-1.txt
+mv ITU-ietf-2.txt file900.txt
+ln -s file900.txt ITU-ietf-2.txt
+mv ITU-ietf-3.txt file901.txt
+ln -s file901.txt ITU-ietf-3.txt
+mv ITU-ietf-4.txt file902.txt
+ln -s file902.txt ITU-ietf-4.txt
+mv ITU-ietf-5.txt file903.txt
+ln -s file903.txt ITU-ietf-5.txt
+mv ISO-SC29-MPEG4-simple-response.txt file904.txt
+ln -s file904.txt ISO-SC29-MPEG4-simple-response.txt
+mv 3GPP-SA5.htm file905.htm
+ln -s file905.htm 3GPP-SA5.htm
+mv ITUT11-BICC.txt file906.txt
+ln -s file906.txt ITUT11-BICC.txt
+mv ITU-EEdwards.txt file907.txt
+ln -s file907.txt ITU-EEdwards.txt
+mv NNI_Liaison_IETFver1.html file908.html
+ln -s file908.html NNI_Liaison_IETFver1.html
+mv oif2002.229.04.html file909.html
+ln -s file909.html oif2002.229.04.html
+mv ITU-SIGTRAN-g799.1.htm file910.htm
+ln -s file910.htm ITU-SIGTRAN-g799.1.htm
+mv ISO-SC29-MPEG4-simple.htm file911.htm
+ln -s file911.htm ISO-SC29-MPEG4-simple.htm
+mv ITU-SG2-040-2.htm file912.htm
+ln -s file912.htm ITU-SG2-040-2.htm
+mv ITU-SG2-042-2.htm file913.htm
+ln -s file913.htm ITU-SG2-042-2.htm
+mv itu-ls-signalling-protocol.html file914.html
+ln -s file914.html itu-ls-signalling-protocol.html
+mv itutsg15-ls-oif-discovery.html file915.html
+ln -s file915.html itutsg15-ls-oif-discovery.html
+mv ITU-ENUM-text.html file916.html
+ln -s file916.html ITU-ENUM-text.html
+mv ITU-DISMAN.txt file917.txt
+ln -s file917.txt ITU-DISMAN.txt
+mv ITU-SG13-MPLS-OAM-RESPONSE.txt file918.txt
+ln -s file918.txt ITU-SG13-MPLS-OAM-RESPONSE.txt
+mv N12175.txt file919.txt
+ln -s file919.txt N12175.txt
+mv ITU-SG16-draft-parsons-itu-t38.txt file920.txt
+ln -s file920.txt ITU-SG16-draft-parsons-itu-t38.txt
+mv ITU-SG16-draft-mule-sip-t38callflows.txt file921.txt
+ln -s file921.txt ITU-SG16-draft-mule-sip-t38callflows.txt
+mv ITU-SG16-emergency-ts.txt file922.txt
+ln -s file922.txt ITU-SG16-emergency-ts.txt
+mv ISO-SC29-MPEG4-Content.txt file923.txt
+ln -s file923.txt ISO-SC29-MPEG4-Content.txt
+mv ISO-SC29-JPEG2000.txt file924.txt
+ln -s file924.txt ISO-SC29-JPEG2000.txt
+mv ITU-SG13-MPLS-OAM.txt file925.txt
+ln -s file925.txt ITU-SG13-MPLS-OAM.txt
+mv itu-ls-detector-control.html file926.html
+ln -s file926.html itu-ls-detector-control.html
+mv ATMFORUM-MEGACO-PKGS.txt file927.txt
+ln -s file927.txt ATMFORUM-MEGACO-PKGS.txt
+mv ATMFORUM-MEGACO-PKGS-R.txt file928.txt
+ln -s file928.txt ATMFORUM-MEGACO-PKGS-R.txt
+mv ATMFORUM-PWE3.html file929.html
+ln -s file929.html ATMFORUM-PWE3.html
+mv ITU-OIF.html file930.html
+ln -s file930.html ITU-OIF.html
+mv REP_Q11_Milan_2002.html file931.html
+ln -s file931.html REP_Q11_Milan_2002.html
+mv ieee802.11.txt file932.txt
+ln -s file932.txt ieee802.11.txt
+mv ITU-SC29.txt file937.txt
+ln -s file937.txt ITU-SC29.txt
+mv 29n45591.htm file936.htm
+ln -s file936.htm 29n45591.htm
+mv 29n45611.htm file935.htm
+ln -s file935.htm 29n45611.htm
+mv Annex6.5.htm file938.htm
+ln -s file938.htm Annex6.5.htm
+mv ITU-CCAMP.txt file939.txt
+ln -s file939.txt ITU-CCAMP.txt
+mv ITU-OTNT.txt file940.txt
+ln -s file940.txt ITU-OTNT.txt
+mv ITU-TD44.txt file941.txt
+ln -s file941.txt ITU-TD44.txt
+mv ITU-VOICE-GATEWAY.txt file942.txt
+ln -s file942.txt ITU-VOICE-GATEWAY.txt
+mv ITU-1831.htm file943.htm
+ln -s file943.htm ITU-1831.htm
+mv ITU-1832.htm file944.htm
+ln -s file944.htm ITU-1832.htm
+mv ITU-1833.htm file945.htm
+ln -s file945.htm ITU-1833.htm
+mv ITU-1834.txt file946.txt
+ln -s file946.txt ITU-1834.txt
+mv ITU-2066.htm file947.htm
+ln -s file947.htm ITU-2066.htm
+mv ITU-2045.htm file948.htm
+ln -s file948.htm ITU-2045.htm
+mv ITU-2031.htm file949.htm
+ln -s file949.htm ITU-2031.htm
+mv ITU-2032.htm file950.htm
+ln -s file950.htm ITU-2032.htm
+mv ITU-2058.htm file951.htm
+ln -s file951.htm ITU-2058.htm
+mv ITU-1018.htm file952.htm
+ln -s file952.htm ITU-1018.htm
+mv X213amd1.htm file953.htm
+ln -s file953.htm X213amd1.htm
+mv sg8-iana-registrations.txt file954.txt
+ln -s file954.txt sg8-iana-registrations.txt
+mv ISO-ISOC-coopagreement.txt file955.txt
+ln -s file955.txt ISO-ISOC-coopagreement.txt
