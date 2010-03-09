315a4e0367d76f1ad67677932b31e735e5190495
Pasi Eronen <pe@iki.fi>
2010-03-09 06:54:43 +0000

    Show datatracker version+date in new look, too  - Legacy-Id: 2050

--- ietf/context_processors.py
+++ ietf/context_processors.py
@@ -7,4 +7,4 @@ def server_mode(request):
     return {'server_mode': settings.SERVER_MODE}
     
 def revision_info(request):
-    return {'revision_time': __date__[7:32], 'revision_date': __date__[34:-3], 'revision_num': __rev__[6:-2], "revision_id": __id__[5:-2], "version_num": __version__ }
+    return {'revision_time': __date__[7:32], 'revision_date': __date__[7:17], 'revision_num': __rev__[6:-2], "revision_id": __id__[5:-2], "version_num": __version__ }
