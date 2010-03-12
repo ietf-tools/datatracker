cddd505752297c66fe7b2da1806bbe89e2490250
Pasi Eronen <pe@iki.fi>
2010-03-12 19:00:20 +0000

    Fix /doc/(rfc,dead,current)/ redirects  - Legacy-Id: 2073

--- ietf/idindex/urls.py
+++ ietf/idindex/urls.py
@@ -6,14 +6,14 @@ from ietf.idindex import views
 
 urlpatterns = patterns('',
      (r'^$', 'django.views.generic.simple.redirect_to', { 'url': '/doc/'}),
-     (r'^(?P<object_id>\d+)/(related/)?$', views.redirect_id),
-     (r'^(?P<filename>[^/]+)/(related/)?$', views.redirect_filename),
-     (r'^wgid/(?P<id>\d+)/$', views.wgdocs_redirect_id),
-     (r'^wg/(?P<acronym>[^/]+)/$', views.wgdocs_redirect_acronym),
      (r'^all/$', 'django.views.generic.simple.redirect_to', { 'url': '/doc/all/'}),
      (r'^rfc/$', 'django.views.generic.simple.redirect_to', { 'url': '/doc/all/#rfc'}),
      (r'^dead/$', 'django.views.generic.simple.redirect_to', { 'url': '/doc/all/#dead'}),
      (r'^current/$', 'django.views.generic.simple.redirect_to', { 'url': '/doc/active/'}),
+     (r'^(?P<object_id>\d+)/(related/)?$', views.redirect_id),
+     (r'^(?P<filename>[^/]+)/(related/)?$', views.redirect_filename),
+     (r'^wgid/(?P<id>\d+)/$', views.wgdocs_redirect_id),
+     (r'^wg/(?P<acronym>[^/]+)/$', views.wgdocs_redirect_acronym),
      (r'^all_id(?:_txt)?.html$', 'django.views.generic.simple.redirect_to', { 'url': 'http://www.ietf.org/id/all_id.txt' }),
 )
 
