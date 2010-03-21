9d562c4ba406c75a5d04520ed2212f705a81de82
Henrik Levkowetz <henrik@levkowetz.com>
2010-03-21 02:04:25 +0000

    Set version info and settings back to development mode  - Legacy-Id: 2157

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "2.47"
+__version__ = "2.48-dev"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$"
+__rev__     = "$Rev$ (dev) Latest release: Rev. 2155 "
 
 __id__      = "$Id$"
