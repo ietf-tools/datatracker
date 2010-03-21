fbb1389ebff8578562be541702c1dd7e2aedfc76
Pasi Eronen <pe@iki.fi>
2010-03-21 17:13:54 +0000

    Replaced /idtracker/help/evaluation/ with redirect to www.ietf.org  - Legacy-Id: 2177

--- ietf/idtracker/urls.py
+++ ietf/idtracker/urls.py
@@ -7,7 +7,7 @@ from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('django.views.generic.simple',
      (r'^help/state/$', 'direct_to_template', { 'template': 'idtracker/states.html', 'extra_context': { 'states': IDState.objects.all(), 'substates': IDSubState.objects.all() } }),
-     (r'^help/evaluation/$', 'direct_to_template', { 'template': 'idtracker/view_evaluation_desc.html' }),
+     (r'^help/evaluation/$', redirect_to, {'url':'http://www.ietf.org/iesg/voting-procedures.html' }),
 )
 urlpatterns += patterns('',
      (r'^status/$', views.status),
