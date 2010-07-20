9fa7f64460bc9f11615378a9e96f34f463ef2b11
Henrik Levkowetz <henrik@levkowetz.com>
2010-07-20 16:41:50 +0000

    Set version info to release version 3.01 before branching.  - Legacy-Id: 2358

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "3.01-dev"
+__version__ = "3.01"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$ (dev) Latest release: Rev. 2346 "
+__rev__     = "$Rev$"
 
 __id__      = "$Id$"
