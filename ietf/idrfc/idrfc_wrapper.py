6ae0bafc67186104a459f98f9ea1d2d416317e64
Henrik Levkowetz <henrik@levkowetz.com>
2010-08-17 14:45:08 +0000

    Additional fix for issue #367.  - Legacy-Id: 2484

--- ietf/idrfc/idrfc_wrapper.py
+++ ietf/idrfc/idrfc_wrapper.py
@@ -417,6 +417,7 @@ class IetfProcessData:
             return self._idinternal.comments().filter(
                 Q(comment_text__istartswith="Draft Added by ")|
                 Q(comment_text__istartswith="Draft Added in state ")|
+                Q(comment_text__istartswith="State changed to ")|
                 Q(comment_text__istartswith="State Changes to ")|
                 Q(comment_text__istartswith="Sub state has been changed to ")|
                 Q(comment_text__istartswith="State has been changed to ")|
