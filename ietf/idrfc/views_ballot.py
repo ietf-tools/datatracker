2e075206673fdbc514b8dc54beb5d4ccf7a58ba8
Henrik Levkowetz <henrik@levkowetz.com>
2010-11-06 09:32:05 +0000

    Merged [2618] from lars.eggert@nokia.com: Fix for #525. Adds more information to the document history tab, specifically changes to writeup, announcement and last-call text.  - Legacy-Id: 2633 Note: SVN reference [2618] has been migrated to Git commit d7a942c2fbd08a9922fde0d848d25037d35d56c5

--- ietf/idrfc/views_ballot.py
+++ ietf/idrfc/views_ballot.py
@@ -366,6 +366,7 @@ def lastcalltext(request, name):
             last_call_form = LastCallTextForm(request.POST, instance=ballot)
             if last_call_form.is_valid():
                 ballot.last_call_text = last_call_form.cleaned_data["last_call_text"]
+		add_document_comment(request, doc, "Last Call text changed")
                 ballot.save()
 
                 if "send_last_call_request" in request.POST:
@@ -430,6 +431,7 @@ def ballot_writeupnotes(request, name):
             ballot_writeup_form = BallotWriteupForm(request.POST, instance=ballot)
             if ballot_writeup_form.is_valid():
                 ballot.ballot_writeup = ballot_writeup_form.cleaned_data["ballot_writeup"]
+		add_document_comment(request, doc, "Ballot writeup text changed")
                 ballot.save()
 
         if "issue_ballot" in request.POST:
@@ -504,10 +506,12 @@ def ballot_approvaltext(request, name):
             approval_text_form = ApprovalTextForm(request.POST, instance=ballot)            
             if approval_text_form.is_valid():
                 ballot.approval_text = approval_text_form.cleaned_data["approval_text"]
+		add_document_comment(request, doc, "Approval announcement text changed")
                 ballot.save()
                 
         if "regenerate_approval_text" in request.POST:
             ballot.approval_text = generate_approval_mail(request, doc)
+	    add_document_comment(request, doc, "Approval announcement text regenerated")
             ballot.save()
 
             # make sure form has the updated text
