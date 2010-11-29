e32255576baeb56e58df6fa4c1ddc4fb7ddc7397
Emilio A. Sánchez López <esanchez@yaco.es>
2010-11-29 09:09:46 +0000

    Some permission related funcions. See #557  - Legacy-Id: 2689

---
+++ ietf/wgchairs/accounts.py
@@ -0,0 +1,39 @@
+def is_group_chair(person, group):
+    if group.chairs().filter(person=person):
+        return True
+    return False
+
+
+def get_person_for_user(user):
+    try:
+        return user.get_profile().person()
+    except:
+        return None
+
+
+def can_do_wg_workflow_in_group(user, group):
+    person = get_person_for_user(user)
+    if not person:
+        return False
+    return is_group_chair(person, group)
+
+
+def can_do_wg_workflow_in_document(user, document):
+    person = get_person_for_user(user)
+    if not person or not document.group:
+        return False
+    return can_do_wg_wgorkflow_in_group(document.group)
+
+
+def can_manage_workflow_in_group(user, group):
+    person = get_person_for_user(user)
+    if not person:
+        return False
+    return is_group_chair(person, group)
+
+
+def can_manage_delegates_in_group(user, group):
+    person = get_person_for_user(user)
+    if not person:
+        return False
+    return is_group_chair(person, group)
