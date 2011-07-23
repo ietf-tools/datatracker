413bcdabbd5a166de158197b13c9e0b6cae901e5
Henrik Levkowetz <henrik@levkowetz.com>
2011-07-23 12:49:56 +0000

    Set up app-specific email addresses so the respective developers will be notified about failures in code they have worked with.  - Legacy-Id: 3205

--- ietf/iesg/__init__.py
+++ ietf/iesg/__init__.py
@@ -1,2 +1,20 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
+# coding: latin-1
+
+from types import ModuleType
+import urls, models, views, feeds, admin
+
+# These people will be sent a stack trace if there's an uncaught exception in
+# code any of the modules imported above:
+DEBUG_EMAILS = [
+    ('Ole Laursen', 'olau@iola.dk'),
+]
+
+for k in locals().keys():
+    m = locals()[k]
+    if isinstance(m, ModuleType):
+        if hasattr(m, "DEBUG_EMAILS"):
+            DEBUG_EMAILS += list(getattr(m, "DEBUG_EMAILS"))
+        setattr(m, "DEBUG_EMAILS", DEBUG_EMAILS)
+
 
