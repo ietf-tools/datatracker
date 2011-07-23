6fcf8ad76d8dca22ca8b281b95cc440f7a68e20a
Henrik Levkowetz <henrik@levkowetz.com>
2011-07-23 12:20:16 +0000

    Admin support for StreamedID, so one can look at it.  - Legacy-Id: 3202

--- ietf/ietfworkflows/admin.py
+++ ietf/ietfworkflows/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from ietf.ietfworkflows.models import (AnnotationTag, WGWorkflow,
-                                       Stream)
+                                       Stream, StreamedID)
 from workflows.admin import StateInline
 
 
@@ -12,6 +12,11 @@ class AnnotationTagInline(admin.TabularInline):
 class IETFWorkflowAdmin(admin.ModelAdmin):
     inlines = [StateInline, AnnotationTagInline]
 
+class StreamedIdAdmin(admin.ModelAdmin):
+    list_display = [ 'id', 'draft', 'stream', 'content_type', 'content_id', 'group', ]
+    search_fields = [ 'draft__filename', ]
+    pass
+admin.site.register(StreamedID, StreamedIdAdmin)
 
 admin.site.register(WGWorkflow, IETFWorkflowAdmin)
 admin.site.register(Stream, admin.ModelAdmin)
