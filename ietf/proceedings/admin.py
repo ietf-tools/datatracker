ef9fda026621ee47dba1161117772edabe5b33bf
Henrik Levkowetz <henrik@levkowetz.com>
2011-07-27 16:32:32 +0000

    Tweak the admin list for WgMeetingSession to show wg acronym instead of group id.  - Legacy-Id: 3261

--- ietf/proceedings/admin.py
+++ ietf/proceedings/admin.py
@@ -72,7 +72,7 @@ class SessionStatusAdmin(admin.ModelAdmin):
 admin.site.register(SessionStatus, SessionStatusAdmin)
 
 class WgMeetingSessionAdmin(admin.ModelAdmin):
-    list_display = ['session_id', 'meeting', 'group_acronym_id', 'number_attendee', 'status', 'approval_ad', 'scheduled_date', 'last_modified_date', 'special_req', 'ad_comments']
+    list_display = ['session_id', 'meeting', 'acronym', 'number_attendee', 'status', 'approval_ad', 'scheduled_date', 'last_modified_date', 'special_req', 'ad_comments']
     list_filter = ['meeting', ]
     pass
 admin.site.register(WgMeetingSession, WgMeetingSessionAdmin)
