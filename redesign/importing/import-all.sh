24488a957b0ce67f98a4f2d454e23ed0681f5ceb
Ole Laursen <olau@iola.dk>
2011-08-19 10:11:41 +0000

    Add import-meetings.py  - Legacy-Id: 3343

--- redesign/importing/import-all.sh
+++ redesign/importing/import-all.sh
@@ -1,6 +1,9 @@
+# basic dependencies
 python import-persons.py
 python import-groups.py
 python import-roles.py
+
+python import-meetings.py
 python import-announcements.py
 python import-document-state.py
-python import-ipr.py
+python import-ipr.py # sets up links to drafts/RFCs so needs them
