da625e5db31b7cd87f0ee7088a75382e285153b5
Ole Laursen <olau@iola.dk>
2012-01-25 15:16:28 +0000

    Order messages in reverse time in admin  - Legacy-Id: 3859

--- ietf/message/admin.py
+++ ietf/message/admin.py
@@ -6,6 +6,7 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ["subject", "by", "time", "groups"]
     search_fields = ["subject", "body"]
     raw_id_fields = ["by"]
+    ordering = ["-time"]
 
     def groups(self, instance):
         return ", ".join(g.acronym for g in instance.related_groups.all())
@@ -17,5 +18,6 @@ class SendQueueAdmin(admin.ModelAdmin):
     list_filter = ["time", "send_at", "sent_at"]
     search_fields = ["message__body"]
     raw_id_fields = ["by"]
+    ordering = ["-time"]
 
 admin.site.register(SendQueue, SendQueueAdmin)
