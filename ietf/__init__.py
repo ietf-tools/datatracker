523c2ace541cdf4ca7643b0f2eb610ff30254033
Henrik Levkowetz <henrik@levkowetz.com>
2012-03-06 18:30:42 +0000

    Set version to 4.00.  - Legacy-Id: 4042

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "4.00rc1"
+__version__ = "4.00"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$ (dev) Latest release: Rev. 3633 "
+__rev__     = "$Rev: 3975"
 
 __id__      = "$Id$"
