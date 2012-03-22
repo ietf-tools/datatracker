c6f59d2c7c94eb80e86ee1a8a81a3aae287badb5
Ole Laursen <olau@iola.dk>
2012-03-22 18:20:03 +0000

    Give document tab URLs names.  - Legacy-Id: 4167

--- ietf/idrfc/urls.py
+++ ietf/idrfc/urls.py
@@ -42,9 +42,9 @@ urlpatterns = patterns('',
     url(r'^ad/(?P<name>[A-Za-z0-9.-]+)/$', views_search.by_ad, name="doc_search_by_ad"),
 
     url(r'^(?P<name>[A-Za-z0-9.-]+)/((?P<rev>[0-9-]+)/)?$', views_doc.document_main, name="doc_view"),
-    url(r'^(?P<name>[A-Za-z0-9.-]+)/history/$', views_doc.document_history),
-    url(r'^(?P<name>[A-Za-z0-9.-]+)/writeup/$', views_doc.document_writeup),
-    url(r'^(?P<name>[A-Za-z0-9.-]+)/ballot/((?P<ballot>[A-Za-z0-9.-]+)/)?$', views_doc.document_ballot),
+    url(r'^(?P<name>[A-Za-z0-9.-]+)/history/$', views_doc.document_history, name="doc_history"),
+    url(r'^(?P<name>[A-Za-z0-9.-]+)/writeup/$', views_doc.document_writeup, name="doc_writeup"),
+    url(r'^(?P<name>[A-Za-z0-9.-]+)/ballot/((?P<ballot>[A-Za-z0-9.-]+)/)?$', views_doc.document_ballot, name="doc_ballot"),
     (r'^(?P<name>[A-Za-z0-9.-]+)/doc.json$', views_doc.document_debug),
     (r'^(?P<name>[A-Za-z0-9.-]+)/_ballot.data$', views_doc.ballot_html), # why is this url so weird instead of just ballot.html?
     (r'^(?P<name>[A-Za-z0-9.-]+)/ballot.tsv$', views_doc.ballot_tsv),
