9717202fed4f7d4e37819d051cf2730a70679b6e
Ole Laursen <olau@iola.dk>
2012-05-03 12:02:08 +0000

    Create charters in state notrev instead of intrev, as a side effect this fixes a problem with comments not being saved when starting review on charter.  - Legacy-Id: 4357

--- ietf/wginfo/edit.py
+++ ietf/wginfo/edit.py
@@ -118,7 +118,7 @@ def edit(request, acronym=None, action="edit"):
                         rev="00-00",
                         )
                     charter.save()
-                    charter.set_state(State.objects.get(type="charter", slug="infrev"))
+                    charter.set_state(State.objects.get(type="charter", slug="notrev"))
 
                     # Create an alias as well
                     DocAlias.objects.create(
