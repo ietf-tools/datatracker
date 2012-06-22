713f2993cc3add29edbe46895224d844a7d2eaea
Ryan Cross <rcross@amsl.com>
2012-06-22 16:21:24 +0000

    finish cleanup.sh  - Legacy-Id: 4500

--- sec/bin/cleanup.sh
+++ sec/bin/cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-#cd /a/www/ietfsec/current/sec/
-cd /home/rcross/src/amsl/conversion/sec/
+# script to run the cleanup management command
+# to truncate the django_sessions table
+
+export PYTHONPATH=/a/www/ietfsec/current:/a/www/ietf-datatracker/web
+cd /a/www/ietfsec/current/sec/
 /usr/bin/python manage.py cleanup
