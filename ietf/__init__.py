8ff61de5f2c56ce83ce8806f920d5f7c341dafbe
Henrik Levkowetz <henrik@levkowetz.com>
2012-07-28 16:29:35 +0000

    Set version info to release version 4.32 before branching.  - Legacy-Id: 4654

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "4.32-dev"
+__version__ = "4.32"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$ (dev) Latest release: Rev. 4647 "
+__rev__     = "$Rev$"
 
 __id__      = "$Id$"
