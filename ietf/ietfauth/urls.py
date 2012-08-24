843493aa2343a100e5322090b9a14bd3b100916e
Henrik Levkowetz <henrik@levkowetz.com>
2012-08-24 08:50:31 +0000

    Fixed a password reset url which didn't work due to a wrong keyword name in the urlpattern.  - Legacy-Id: 4810

--- ietf/ietfauth/urls.py
+++ ietf/ietfauth/urls.py
@@ -16,7 +16,7 @@ urlpatterns += patterns('ietf.ietfauth.views',
         url(r'^create/$', 'create_account', name='create_account'),
         url(r'^confirm/(?P<username>[\w.@+-]+)/(?P<date>[\d]+)/(?P<realm>[\w]+)/(?P<hash>[a-f0-9]+)/$', 'confirm_account', name='confirm_account'),
         url(r'^reset/$', 'password_reset_view', name='password_reset'),
-        url(r'^reset/confirm/(?P<username>[\w.@+-]+)/(?P<date>[\d]+)/(?P<realm>[\w]+)/(?P<reset_hash>[a-f0-9]+)/$', 'confirm_password_reset', name='confirm_password_reset'),
+        url(r'^reset/confirm/(?P<username>[\w.@+-]+)/(?P<date>[\d]+)/(?P<realm>[\w]+)/(?P<hash>[a-f0-9]+)/$', 'confirm_password_reset', name='confirm_password_reset'),
         url(r'^add_email/confirm/(?P<username>[\w.@+-]+)/(?P<date>[\d]+)/(?P<email>[\w.@+-]+)/(?P<hash>[a-f0-9]+)/$', 'confirm_new_email', name='confirm_new_email'),
         url(r'^ajax/check_username/$', 'ajax_check_username', name='ajax_check_username'),
 )
