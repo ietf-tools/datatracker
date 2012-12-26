5aad2aadffbf4d15131dc4252c1e37e233acdf9c
Emilio Jiménez <jimenezmoral@gmail.com>
2012-12-26 17:24:27 +0000

    Add encrypted field to comment.
    See #913
     - Legacy-Id: 5155

--- ietf/settings.py
+++ ietf/settings.py
@@ -261,9 +261,10 @@ IDSUBMIT_ANNOUNCE_LIST_EMAIL = 'i-d-announce@ietf.org'
 
 # NomCom Tool settings
 ROLODEX_URL = ""
-PUBLIC_KEYS_URL = BASE_DIR + "/public_keys/"
+PUBLIC_KEYS_URL = BASE_DIR + '/public_keys/'
 NOMCOM_FROM_EMAIL = DEFAULT_FROM_EMAIL
 NOMCOM_ADMIN_EMAIL = DEFAULT_FROM_EMAIL
+OPENSSL_COMMAND = '/usr/bin/openssl'
 
 # Days from meeting to cut off dates on submit
 FIRST_CUTOFF_DAYS = 19
