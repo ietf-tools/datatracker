a3ce05bcc70d2a79c82b562fbf523d62a5e7fd9b
Henrik Levkowetz <henrik@levkowetz.com>
2013-05-06 16:35:36 +0000

    A different fix for the shepherd writeup edit test.  - Legacy-Id: 5699

--- ietf/idrfc/utils.py
+++ ietf/idrfc/utils.py
@@ -187,16 +187,11 @@ can_edit_intended_std_level = can_edit_base
 can_edit_consensus = can_edit_base
 can_edit_shepherd = can_edit_base
 
-import debug
-@debug.trace
 def can_edit_shepherd_writeup(doc, user):
-    try:
-        person = user.person
-    except Person.DoesNotExist:
-        person = None
-    return user.is_authenticated() and (
-        (person and doc.shepherd==person)
-        )
+     if user.is_authenticated():
+         if Person.objects.filter(user=user).count():
+             return can_edit_base(doc,user) or (doc.shepherd==user.person)
+     return False
 
 def nice_consensus(consensus):
     mapping = {
