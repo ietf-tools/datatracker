adb916465301f31411c2131ecbe147bf8e1e9494
Ryan Cross <rcross@amsl.com>
2013-05-07 21:31:54 +0000

    add Status Changes to telechat agenda section 2  - Legacy-Id: 5706

--- ietf/secr/telechat/views.py
+++ ietf/secr/telechat/views.py
@@ -94,7 +94,7 @@ def get_section_header(file,agenda):
     agenda section header as a string for use in the doc template
     '''
     h1 = {'2':'Protocol Actions','3':'Document Actions','4':'Working Group Actions'}
-    h2a = {'1':'WG Submissions','2':'Individual Submissions'}
+    h2a = {'1':'WG Submissions','2':'Individual Submissions','3':'Status Changes'}
     h2b = {'1':'WG Submissions','2':'Individual Submissions via AD','3':'Status Changes','4':'IRTF and Independent Submission Stream Documents'}
     h2c = {'1':'WG Creation','2':'WG Chartering'}
     h3a = {'1':'New Item','2':'Returning Item','3':'For Action'}
