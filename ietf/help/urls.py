51284a8871316b5db6144c64bcf1fd6a7ba2f3b0
Henrik Levkowetz <henrik@levkowetz.com>
2013-07-17 20:55:35 +0000

    Added an index page at /help/state, listing the document states that have help information.  - Legacy-Id: 5837

--- ietf/help/urls.py
+++ ietf/help/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('',
     url(r'^state/(?P<doc>[-\w]+)/(?P<type>[-\w]+)/?$', 'ietf.help.views.state'),
     url(r'^state/(?P<doc>[-\w]+)/?$', 'ietf.help.views.state'),
+    url(r'^state/?$', 'ietf.help.views.index'),
 )
 
--- ietf/help/views.py
+++ ietf/help/views.py
@@ -8,6 +8,19 @@ import debug
 from ietf.doc.models import State, StateType
 
 
+def index(request):
+    types = StateType.objects.all()
+    names = [ type.slug for type in types ]
+    for type in types:
+        if "-" in type.slug and type.slug.split('-',1)[0] in names:
+            type.stategroups = None
+        else:
+            groups = StateType.objects.filter(slug__startswith=type.slug)
+            type.stategroups =  [ g.slug[len(type.slug)+1:] for g in groups if not g == type ] or ""
+                
+    return render_to_response('help/index.html', {"types": types},
+        context_instance=RequestContext(request))
+
 def state(request, doc, type=None):
     slug = "%s-%s" % (doc,type) if type else doc
     debug.show('slug')
