86cf15cd9f04148f3173aa728d5fa31ffd4ee811
Henrik Levkowetz <henrik@levkowetz.com>
2013-09-22 19:26:48 +0000

    Fixed some bugs in the EncryptedTextField class.  - Legacy-Id: 6246

--- ietf/nomcom/fields.py
+++ ietf/nomcom/fields.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.encoding import smart_str
 
 from ietf.utils.pipe import pipe
-
+from ietf.utils.log import log
 
 class EncryptedException(Exception):
     pass
@@ -19,10 +19,10 @@ class EncryptedTextField(models.TextField):
             except ValueError as e:
                 raise ValueError("Trying to read the NomCom public key: " + str(e))
 
-            code, out, error = pipe("%s smime -encrypt -in /dev/stdin %s" % (settings.OPENSSL_COMMAND,
-                                    cert_file), comments)
+            command = "%s smime -encrypt -in /dev/stdin %s" % (settings.OPENSSL_COMMAND, cert_file)
+            code, out, error = pipe(command, comments)
             if code != 0:
-                log("openssl error: %s:\n  Error %s: %s" %(command, code, error))        
+                log("openssl error: %s:\n  Error %s: %s" %(command, code, error))
             if not error:
                 instance.comments = out
                 return out
