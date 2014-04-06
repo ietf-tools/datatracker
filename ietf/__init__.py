6f199806a8d7b937b0a4b56a35893d045f5a1de9
Henrik Levkowetz <henrik@levkowetz.com>
2014-04-06 15:28:51 +0000

    Set version info to release version 5.2.1 before branching.  - Legacy-Id: 7575

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "5.2.1-dev"
+__version__ = "5.2.1"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$ (dev) Latest release: Rev. 7524 "
+__rev__     = "$Rev$"
 
 __id__      = "$Id$"
