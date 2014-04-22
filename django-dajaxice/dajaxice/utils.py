80126092cc87f26cd5c5f97e93eaedd07bb37ea2
Henrik Levkowetz <henrik@levkowetz.com>
2014-04-22 18:13:20 +0000

    Get rid of django-dajaxice distribution directory  - Legacy-Id: 7624

--- django-dajaxice/dajaxice/utils.py
+++
@@ -1,8 +0,0 @@
-from django.http import QueryDict
-
-
-def deserialize_form(data):
-    """
-    Create a new QueryDict from a serialized form.
-    """
-    return QueryDict(query_string=unicode(data).encode('utf-8'))
