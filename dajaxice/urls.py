4a2c615c3f93f4c8a8e496c3e0ba69c7b7b229e0
Henrik Levkowetz <henrik@levkowetz.com>
2014-04-22 18:07:17 +0000

    Remove old dajaxice to make room for incoming later version.  - Legacy-Id: 7622

--- dajaxice/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import *
-from .views import DajaxiceRequest
-
-urlpatterns = patterns('dajaxice.views',
-    url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
-    url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
-)
