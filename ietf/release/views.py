ed97ce5a215d4d2b1cb887fc94f80909441a264e
Henrik Levkowetz <henrik@levkowetz.com>
2014-05-02 00:30:25 +0000

    Added changeset and ticket links to the release notes under /release/  - Legacy-Id: 7684

--- ietf/release/views.py
+++ ietf/release/views.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -6,6 +7,14 @@ from django.conf import settings
 from django.http import HttpResponse
 
 import changelog
+
+def trac_links(text):
+    # changeset links
+    text = re.sub(r'\[(\d+)\]', r'<a href="http://wiki.tools.ietf.org/tools/ietfdb/changeset/\1">[\1]</a>', text)
+    # issue links
+    text = re.sub(r'#(\d+)', r'<a href="http://wiki.tools.ietf.org/tools/ietfdb/ticket/\1">#\1</a>', text)
+    return text
+
 
 def release(request, version=None):
     entries = {}
@@ -22,5 +31,6 @@ def release(request, version=None):
     entries = dict((entry.version, entry) for entry in log_entries)
     if version == None or version not in entries:
         version = log_entries[0].version        
+    entries[version].logentry = trac_links(entries[version].logentry)
     return render_to_response('release/release.html', { 'releases': log_entries, 'version': version, 'entry': entries[version], }, context_instance=RequestContext(request))
 
