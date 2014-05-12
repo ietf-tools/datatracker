6f202065e799e1a500c8c36544c035fc37a41f38
Robert Sparks <rjsparks@nostrum.com>
2014-05-12 21:45:55 +0000

    Fixed coloring rows: The js IETF object needs to exist for the code to work. Fixes bug #1303. Commit ready to merge.  - Legacy-Id: 7735

--- static/js/agenda2.js
+++ static/js/agenda2.js
@@ -32,6 +32,8 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+var IETF=[];
+
 function setAgendaColor(color) {
     IETF.agendaPalette.hide();  
     document.getElementById(IETF.agendaRow).className="bg"+color;  
