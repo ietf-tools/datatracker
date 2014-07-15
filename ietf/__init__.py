dac3efcb68e68843de39fdab6c6f85de17446ccc
Henrik Levkowetz <henrik@levkowetz.com>
2014-07-15 09:01:20 +0000

    Set version info and settings back to development mode  - Legacy-Id: 8073

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "5.6.1"
+__version__ = "5.6.2-dev0"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$"
+__rev__     = "$Rev$ (dev) Latest release: Rev. 8071 "
 
 __id__      = "$Id$"
