25cdc289e0bf495585407155b9640509398fdf60
Ole Laursen <olau@iola.dk>
2014-08-13 10:24:12 +0000

    Search emails over email address too in addition to person names  - Legacy-Id: 8266

--- ietf/person/views.py
+++ ietf/person/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.db.models import Q
 
 from ietf.person.models import Email
 from ietf.person.forms import json_emails
 
 def ajax_search_emails(request):
-    emails = Email.objects.filter(person__alias__name__icontains=request.GET.get('q','')).filter(active='true').order_by('person__name').distinct()[:10]
+    q = request.GET.get('q', '').strip()
+    emails = Email.objects.filter(Q(person__alias__name__icontains=q) |
+                                  Q(address__icontains=q)).filter(active='true').order_by('person__name').distinct()[:10]
     return HttpResponse(json_emails(emails), content_type='application/json')
