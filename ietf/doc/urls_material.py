754ee8001655765f0643107a0aab66825807a8df
Robert Sparks <rjsparks@nostrum.com>
2014-09-26 20:47:13 +0000

    snapshot before SIPit - have a working session presentation edit form  - Legacy-Id: 8358

--- ietf/doc/urls_material.py
+++ ietf/doc/urls_material.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('ietf.doc.views_material',
     url(r'^(?P<action>state|title|revise)/$', "edit_material", name="material_edit"),
+    url(r'^sessions/$', "material_presentations", name="material_presentations"),
 )
 
