6e6dd79e0f63fa82c7b279c0b248bfb2e7d62815
Henrik Levkowetz <henrik@levkowetz.com>
2014-10-23 18:56:36 +0000

    Removed unused imports from name slug migration.  - Legacy-Id: 8457

--- ietf/name/migrations/0026_change_name_slug_foreign_key_length.py
+++ ietf/name/migrations/0026_change_name_slug_foreign_key_length.py
@@ -1,9 +1,5 @@
-﻿# -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
-
 
 class Migration(SchemaMigration):
 
