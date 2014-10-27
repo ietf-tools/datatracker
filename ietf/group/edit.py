c706413064d26398ac1b22add47554e0236e60d6
Lars Eggert <lars@eggert.org>
2014-10-27 11:08:30 +0000

    Merge branch 'lars/5.6.4-facelift' into lars/5.7.1-facelift
    Conflicts:
    	ietf/group/edit.py
    	ietf/group/views_stream.py
    	ietf/nomcom/forms.py
    	ietf/person/forms.py
    	ietf/settings.py
    	ietf/templates/doc/change_shepherd.html
    	ietf/templates/doc/document_draft.html
    	ietf/templates/doc/search/search_result_row.html
    	ietf/templates/doc/submit_to_iesg.html
    	ietf/templates/nomcom/edit_position.html
    	ietf/templates/nomcom/list_positions.html
     - Legacy-Id: 8495

--- ietf/group/edit.py
+++ ietf/group/edit.py
@@ -39,7 +39,7 @@ class GroupForm(forms.Form):
     list_email = forms.CharField(max_length=64, required=False)
     list_subscribe = forms.CharField(max_length=255, required=False)
     list_archive = forms.CharField(max_length=255, required=False)
-    urls = forms.CharField(widget=forms.Textarea, label="Additional URLs", help_text="Format: http://site/path (Optional description). Separate multiple entries with newline.", required=False)
+    urls = forms.CharField(widget=forms.Textarea, label="Additional URLs", help_text="Format: https://site/path (Optional description). Separate multiple entries with newline. Prefer HTTPS URLs where possible.", required=False)
 
     def __init__(self, *args, **kwargs):
         self.group = kwargs.pop('group', None)
@@ -60,7 +60,7 @@ class GroupForm(forms.Form):
         self.confirm_msg = ""
         self.autoenable_confirm = False
         if self.group:
-            self.fields['acronym'].widget.attrs['readonly'] = True
+            self.fields['acronym'].widget.attrs['readonly'] = ""
 
         if self.group_type == "rg":
             self.fields['ad'].widget = forms.HiddenInput()
@@ -149,7 +149,7 @@ def get_or_create_initial_charter(group, group_type):
         )
         charter.save()
         charter.set_state(State.objects.get(used=True, type="charter", slug="notrev"))
-                
+
         # Create an alias as well
         DocAlias.objects.create(name=charter.name, document=charter)
 
@@ -220,12 +220,12 @@ def edit(request, group_type=None, acronym=None, action="edit"):
                 group.charter = get_or_create_initial_charter(group, group_type)
 
             changes = []
-                
+
             def desc(attr, new, old):
                 entry = "%(attr)s changed to <b>%(new)s</b> from %(old)s"
                 if new_group:
                     entry = "%(attr)s changed to <b>%(new)s</b>"
-                    
+
                 return entry % dict(attr=attr, new=new, old=old)
 
             def diff(attr, name):
