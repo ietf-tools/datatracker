8a8da7a3977fc5a3db6698c200d88bae40705952
Robert Sparks <rjsparks@nostrum.com>
2014-11-24 21:51:51 +0000

    cleaned out boilerplate migration imports that arent actually used  - Legacy-Id: 8709

--- ietf/group/migrations/0008_materials_managers.py
+++ ietf/group/migrations/0008_materials_managers.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
 from south.v2 import DataMigration
-from django.db import models
 
 class Migration(DataMigration):
 
