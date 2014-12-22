39c87c3016330ef0c157de5e79a83cc7ac3d3da5
Henrik Levkowetz <henrik@levkowetz.com>
2014-12-22 16:40:57 +0000

    Removed tastypie 0.11.1  - Legacy-Id: 8818

--- tastypie/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import unicode_literals
-
-
-__author__ = 'Daniel Lindsley & the Tastypie core team'
-__version__ = (0, 11, 1)
--- tastypie/constants.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals
-
-# Enable all basic ORM filters but do not allow filtering across relationships.
-ALL = 1
-# Enable all ORM filters, including across relationships
-ALL_WITH_RELATIONS = 2
