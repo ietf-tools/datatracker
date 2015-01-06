865746339449931bc9da491759dc55b12bfc21ef
Ryan Cross <rcross@amsl.com>
2015-01-06 23:27:09 +0000

    Reverted unintentional commit to tags branch  - Legacy-Id: 8844

--- ietf/ipr/forms.py
+++ ietf/ipr/forms.py
@@ -172,11 +172,10 @@ class IprDisclosureFormBase(forms.ModelForm):
         super(IprDisclosureFormBase, self).clean()
         cleaned_data = self.cleaned_data
         
-        if not self.instance.pk:
-            # when entering a new disclosure, if same_as_above not checked require submitted
-            if not self.cleaned_data.get('same_as_ii_above'):
-                if not ( self.cleaned_data.get('submitter_name') and self.cleaned_data.get('submitter_email') ):
-                    raise forms.ValidationError('Submitter information must be provided in section VII')
+        # if same_as_above not checked require submitted
+        if not self.cleaned_data.get('same_as_ii_above'):
+            if not ( self.cleaned_data.get('submitter_name') and self.cleaned_data.get('submitter_email') ):
+                raise forms.ValidationError('Submitter information must be provided in section VII')
         
         return cleaned_data
 
@@ -190,15 +189,9 @@ class HolderIprDisclosureForm(IprDisclosureFormBase):
         
     def __init__(self, *args, **kwargs):
         super(HolderIprDisclosureForm, self).__init__(*args, **kwargs)
-        if self.instance.pk:
-            # editing existing disclosure
-            self.fields['patent_info'].required = False
-            self.fields['holder_contact_name'].required = False
-            self.fields['holder_contact_email'].required = False
-        else:
-            # entering new disclosure
+        if not self.instance.pk:
             self.fields['licensing'].queryset = IprLicenseTypeName.objects.exclude(slug='none-selected')
-             
+            
     def clean(self):
         super(HolderIprDisclosureForm, self).clean()
         cleaned_data = self.cleaned_data
