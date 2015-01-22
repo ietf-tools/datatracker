b3823d5768b564047ac700d29dc59983b7ad4c20
Ole Laursen <olau@iola.dk>
2015-01-22 17:42:52 +0000

    Summary: Fix comment on insert_after_in_ordered_dict  - Legacy-Id: 8897

--- ietf/utils/ordereddict.py
+++ ietf/utils/ordereddict.py
@@ -1,5 +1,7 @@
 def insert_after_in_ordered_dict(dictionary, key, value, after):
-    # there's no insert in ordered dict so re-add entries after confirm_acronym instead
+    """There's no "insert" in ordered dict so simulate it instead by
+    re-adding entries. Obviously that's not ideal, but for small dicts the
+    overhead is negligible."""
     dictionary[key] = value
 
     reorder = False
