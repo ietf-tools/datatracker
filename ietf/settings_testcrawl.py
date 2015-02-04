bd2cb8baaa55f608964a4f5d64b7516c5d5f5682
Henrik Levkowetz <henrik@levkowetz.com>
2015-02-04 23:17:58 +0000

    Merged in [8970] from olau@iola.dk:
        Summary: Enable support for a custom settings file for the test crawl
    - and add a simple settings file that enables caching to speed up the
    crawl (shaves ~35% off most pages AFAICT)
     - Legacy-Id: 8984
    Note: SVN reference [8970] has been migrated to Git commit 8d4e1a85282e0245a170c9c2302adc657803636e

---
+++ ietf/settings_testcrawl.py
@@ -0,0 +1,22 @@
+# Standard settings except we enable caching like in the production
+# environment, this is useful for speeding up the test crawl, try for
+# instance:
+#
+#   bin/test-crawl --settings=ietf.settings_testcrawl
+#
+
+from settings import *                  # pyflakes:ignore
+
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+    'ietf.dbtemplate.template.Loader',
+)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
