278313829c151e34cef3a3bec53b643a314b2b72
Henrik Levkowetz <henrik@levkowetz.com>
2015-02-09 20:17:16 +0000

    Set version info and settings back to development mode  - Legacy-Id: 9033

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "5.11.0"
+__version__ = "5.11.1-dev0"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$"
+__rev__     = "$Rev$ (dev) Latest release: Rev. 9031 "
 
 __id__      = "$Id$"
