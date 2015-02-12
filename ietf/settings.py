b334550d9c956e18339f6127784a419a5d78cc6c
Ole Laursen <olau@iola.dk>
2015-02-12 15:16:29 +0000

    Summary: Get rid of FillInRemoteUserIfLoggedInMiddleware, we are not using Apache/REMOTE_USER to login anymore, and it's forcing a vary: cookie header even on pages that don't check on the logged in user (i.e. things not rendering through the base template)  - Legacy-Id: 9075

--- ietf/settings.py
+++ ietf/settings.py
@@ -160,7 +160,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'ietf.middleware.FillInRemoteUserIfLoggedInMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.http.ConditionalGetMiddleware',
     'ietf.middleware.SQLLogMiddleware',
