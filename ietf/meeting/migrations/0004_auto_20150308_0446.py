29c674ca59ccc5c5d253c3b6bf066637c1377e7a
Henrik Levkowetz <henrik@levkowetz.com>
2015-03-08 11:49:03 +0000

    Captured the change in ScheduledSession sort order in a migration.  - Legacy-Id: 9164

---
+++ ietf/meeting/migrations/0004_auto_20150308_0446.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('meeting', '0003_auto_20150304_0738'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='scheduledsession',
+            options={'ordering': ['timeslot__time', 'timeslot__type__slug', 'session__group__parent__name', 'session__group__acronym', 'session__name']},
+        ),
+    ]
