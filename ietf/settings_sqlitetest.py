3d35c4f3a30a3060a51ff41f53daaa1b9f9db9fd
Henrik Levkowetz <henrik@levkowetz.com>
2015-03-19 13:14:41 +0000

    Tweaked sqlitetest config to avoid a minutes-long stepping through migrations for each test run.  - Legacy-Id: 9245

--- ietf/settings_sqlitetest.py
+++ ietf/settings_sqlitetest.py
@@ -6,6 +6,23 @@
 
 from settings import *                  # pyflakes:ignore
 
+# Workaround to avoid spending minutes stepping through the migrations in
+# every test run.  The result of this is to use the 'syncdb' way of creating
+# the test database instead of doing it through the migrations.  Taken from
+# https://gist.github.com/NotSqrt/5f3c76cd15e40ef62d09
+
+## To be removed after upgrade to Django 1.8 ##
+
+class DisableMigrations(object):
+ 
+    def __contains__(self, item):
+        return True
+ 
+    def __getitem__(self, item):
+        return None
+ 
+MIGRATION_MODULES = DisableMigrations()
+
 DATABASES = {
     'default': {
         'NAME': 'test.db',
