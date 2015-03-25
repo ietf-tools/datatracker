01b0ca8efbafd1d1d0998d4bf424f3e846ba8725
Henrik Levkowetz <henrik@levkowetz.com>
2015-03-25 19:07:27 +0000

    Set version info and settings back to development mode  - Legacy-Id: 9337

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "5.13.0"
+__version__ = "5.13.1.dev0"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$"
+__rev__     = "$Rev$ (dev) Latest release: Rev. 9335 "
 
 __id__      = "$Id$"
