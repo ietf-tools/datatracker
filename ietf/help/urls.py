7488adef5ba997c220715774a5f8037c8d6a256a
Henrik Levkowetz <henrik@levkowetz.com>
2015-04-05 19:35:31 +0000

    Fixed a bug in /help/state/ (bad links).  Linked additional state labels in the document pages to the state descriptions under /help/state.  Renamed a template file to better match its function.  - Legacy-Id: 9410

--- ietf/help/urls.py
+++ ietf/help/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^state/(?P<doc>[-\w]+)/(?P<type>[-\w]+)/?$', 'ietf.help.views.state'),
     url(r'^state/(?P<doc>[-\w]+)/?$', 'ietf.help.views.state'),
-    url(r'^state/?$', 'ietf.help.views.index'),
+    url(r'^state/?$', 'ietf.help.views.state_index'),
 )
 
