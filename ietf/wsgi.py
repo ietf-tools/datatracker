9c5de53b54f41c1223451d3b3f14f1c13553e1db
Henrik Levkowetz <henrik@levkowetz.com>
2015-04-12 19:43:03 +0000

    Added virtualenv support to wsgi.py  - Legacy-Id: 9467

--- ietf/wsgi.py
+++ ietf/wsgi.py
@@ -42,6 +42,12 @@ import os
 import sys
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Virtualenv support
+virtualenv_activation = os.path.join(path, "bin", "activate_this.py")
+if os.path.exists(virtualenv_activation):
+    execfile(virtualenv_activation, dict(__file__=virtualenv_activation))
+
 if not path in sys.path:
     sys.path.insert(0, path)
 
