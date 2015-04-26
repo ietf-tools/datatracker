4ec326a50507646aa3f1207b7a1f8c04fe6bc5a0
Henrik Levkowetz <henrik@levkowetz.com>
2015-04-26 19:24:02 +0000

    Removed local copy of tastypie  - Legacy-Id: 9562

--- tastypie/utils/urls.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from django.conf import settings
-
-
-def trailing_slash():
-    if getattr(settings, 'TASTYPIE_ALLOW_MISSING_SLASH', False):
-        return '/?'
-
-    return '/'
