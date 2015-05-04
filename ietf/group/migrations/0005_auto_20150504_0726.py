547a1ae7a81fd09763c043f17844caf1ccbda149
Robert Sparks <rjsparks@nostrum.com>
2015-05-04 15:36:00 +0000

    deflake  - Legacy-Id: 9601

--- ietf/group/migrations/0005_auto_20150504_0726.py
+++ ietf/group/migrations/0005_auto_20150504_0726.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
 
 def create_iab_roles(apps, schema_editor):
     Role = apps.get_model('group','Role')
