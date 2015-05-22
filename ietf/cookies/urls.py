b768dbbc9aba1de65aaf141ad37c088df49e0479
Henrik Levkowetz <henrik@levkowetz.com>
2015-05-22 20:03:11 +0000

    Added a preference to not display the left-hand menubar.  Addresses issues #1659 and #1663.  - Legacy-Id: 9643

--- ietf/cookies/urls.py
+++ ietf/cookies/urls.py
@@ -11,4 +11,6 @@ urlpatterns = patterns('',
      (r'^expires_soon/', views.expires_soon),
      (r'^full_draft/(?P<enabled>.*)$', views.full_draft),
      (r'^full_draft/', views.full_draft),
+     (r'^left_menu/(?P<enabled>.*)$', views.left_menu),
+     (r'^left_menu/', views.left_menu),
 )
--- ietf/cookies/views.py
+++ ietf/cookies/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render_to_response as render
 from django.template import RequestContext
 
-def settings(request, new_enough = -1, expires_soon = -1, full_draft = ""):
+def settings(request, new_enough = -1, expires_soon = -1, full_draft = "", left_menu=""):
     if new_enough < 0:
         if "new_enough" in request.COOKIES and request.COOKIES["new_enough"].isdigit():
             new_enough = int(request.COOKIES["new_enough"])
@@ -21,11 +21,19 @@ def settings(request, new_enough = -1, expires_soon = -1, full_draft = ""):
                 full_draft = "off"
         else:
             full_draft = "off"
+    if left_menu == "":
+        if "left_menu" in request.COOKIES:
+            left_menu = request.COOKIES["left_menu"]
+            if left_menu != 'on' and left_menu != 'off':
+                left_menu = "on"
+        else:
+            left_menu = "on"
     return render("cookies/settings.html",
            {
             "new_enough" : new_enough,
             "expires_soon" : expires_soon,
-            "full_draft" : full_draft
+            "full_draft" : full_draft,
+            "left_menu": left_menu,
             }, context_instance=RequestContext(request))
 
 def new_enough(request, days="14"):
@@ -35,7 +43,7 @@ def new_enough(request, days="14"):
         days = 0
     if days == 0:
         days = 14
-    response = settings(request, days, -1)
+    response = settings(request, new_enough=days)
     response.set_cookie("new_enough", days, 315360000)
     return response
 
@@ -46,13 +54,24 @@ def expires_soon(request, days="14"):
         days = 0
     if days == 0:
         days = 14
-    response = settings(request, -1, days)
+    response = settings(request, expires_soon=days)
     response.set_cookie("expires_soon", days, 315360000)
     return response
 
 def full_draft(request, enabled="off"):
     if enabled != "on" and enabled != "off":
         enabled = "off"
-    response = settings(request, -1, -1, enabled)
+    response = settings(request, full_draft=enabled)
     response.set_cookie("full_draft", enabled, 315360000)
     return response
+
+def left_menu(request, enabled="on"):
+    if enabled != "on" and enabled != "off":
+        enabled = "on"
+    # Propagate the new setting immediately, to render the settings page
+    # iteself according to the setting:
+    request.COOKIES["left_menu"] = enabled 
+    response = settings(request, left_menu=enabled)
+    response.set_cookie("left_menu", enabled, 315360000)
+    return response
+
