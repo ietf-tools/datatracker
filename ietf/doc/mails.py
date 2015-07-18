313704f6379cd1aae15badd262ebd1785437b2d3
Adam Roach <adam@nostrum.com>
2015-07-18 13:56:24 +0000

    Now sends email to chairs of WG of 'possibly-replaced' document, if any  - Legacy-Id: 9794

--- ietf/doc/mails.py
+++ ietf/doc/mails.py
@@ -13,6 +13,7 @@ from ietf.doc.models import WriteupDocEvent, BallotPositionDocEvent, LastCallDoc
 from ietf.doc.utils import needed_ballot_positions
 from ietf.person.models import Person
 from ietf.group.models import Group, Role
+from ietf.doc.models import Document
 
 def email_state_changed(request, doc, text):
     to = [x.strip() for x in doc.notify.replace(';', ',').split(',')]
@@ -500,11 +501,15 @@ def send_review_possibly_replaces_request(request, doc):
     elif doc.stream_id == "irtf":
         to_email.append("IRSG <irsg@irtf.org>")
 
+    possibly_replaces = Document.objects.filter(name__in=[alias.name for alias in doc.related_that_doc("possibly-replaces")])
+    other_chairs = Role.objects.filter(group__in=[other.group for other in possibly_replaces], name="chair").select_related("email", "person")
+    to_email.extend(r.formatted_email() for r in other_chairs)
+
     if not to_email:
         to_email.append("internet-drafts@ietf.org")
 
     if to_email:
-        send_mail(request, to_email, settings.DEFAULT_FROM_EMAIL,
+        send_mail(request, list(set(to_email)), settings.DEFAULT_FROM_EMAIL,
                   'Review of suggested possible replacements for %s-%s needed' % (doc.name, doc.rev),
                   'doc/mail/review_possibly_replaces_request.txt', {
                       'doc': doc,
