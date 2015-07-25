675eb77bf57faf8de004913a932ed18baa3d64a2
Henrik Levkowetz <henrik@levkowetz.com>
2015-07-25 16:32:29 +0000

    Set version info to release version 6.2.0 before branching.  - Legacy-Id: 9880

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "6.1.1.dev0"
+__version__ = "6.2.0"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$ (dev) Latest release: Rev. 9839 "
+__rev__     = "$Rev$"
 
 __id__      = "$Id$"
