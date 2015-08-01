fb3aade0b4ab3654139e901b3223b388d6c00ef6
Henrik Levkowetz <henrik@levkowetz.com>
2015-08-01 14:52:48 +0000

    Set version info and settings back to development mode  - Legacy-Id: 9942

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -1,9 +1,9 @@
 # Copyright The IETF Trust 2007, All Rights Reserved
 
-__version__ = "6.3.0"
+__version__ = "6.3.1.dev0"
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$"
+__rev__     = "$Rev$ (dev) Latest release: Rev. 9940 "
 
 __id__      = "$Id$"
