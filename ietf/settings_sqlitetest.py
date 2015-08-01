4dc4c6dc9a026345f3701d337c6ba9d0f35ab003
Henrik Levkowetz <henrik@levkowetz.com>
2015-08-01 15:00:14 +0000

    Removed dead code  - Legacy-Id: 9950

--- ietf/settings_sqlitetest.py
+++ ietf/settings_sqlitetest.py
@@ -36,11 +36,3 @@ DATABASES = {
         },
     }
 
-class DisableMigrations(object):
-    def __contains__(self, item):
-        return True
- 
-    def __getitem__(self, item):
-        return "notmigrations"
- 
-MIGRATION_MODULES = DisableMigrations() 
