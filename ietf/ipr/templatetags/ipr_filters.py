1a519f26ed5e5db633189089d606fdba041cc0f0
Henrik Levkowetz <henrik@levkowetz.com>
2015-10-01 20:51:26 +0000

    In [10079], the inline template was inadvertently changed from unicode to ascii, which breaks a number of pages.  Fixed this.  - Legacy-Id: 10119 Note: SVN reference [10079] has been migrated to Git commit 104878a6225a1f4d801d02914698bad194f7e727

--- ietf/ipr/templatetags/ipr_filters.py
+++ ietf/ipr/templatetags/ipr_filters.py
@@ -11,10 +11,10 @@ def render_message_for_history(msg):
     """Format message for display in history.  Suppress the 'To' line for incoming responses
     """
     if msg.to.startswith('ietf-ipr+'):
-        return format_html('Date: {}<br>From: {}<br>Subject: {}<br>Cc: {}<br><br>{}',
+        return format_html(u'Date: {}<br>From: {}<br>Subject: {}<br>Cc: {}<br><br>{}',
             msg.time,msg.frm,msg.subject,msg.cc,msg.body)
     else:
-        return format_html('Date: {}<br>From: {}<br>To: {}<br>Subject: {}<br>Cc: {}<br><br>{}',
+        return format_html(u'Date: {}<br>From: {}<br>To: {}<br>Subject: {}<br>Cc: {}<br><br>{}',
             msg.time,msg.frm,msg.to,msg.subject,msg.cc,msg.body)
 
 
