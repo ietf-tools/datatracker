f41553f3d1c2375751e4bffdbf1803a982986537
Henrik Levkowetz <henrik@levkowetz.com>
2015-10-13 19:07:11 +0000

    Added 2 new file existence checks to the check framework, since we're now reading email aliases for groups and documents from files.   Added a call out to run_checks() in the test-crawler, so we don't see failures due to missing files.  - Legacy-Id: 10204

--- ietf/checks.py
+++ ietf/checks.py
@@ -20,3 +20,50 @@ def check_cdn_directory_exists(app_configs, **kwargs):
             id='datatracker.E001',
         ))
     return errors
+
+@checks.register('files')
+def check_group_email_aliases_exists(app_configs, **kwargs):
+    from ietf.group.info import get_group_email_aliases
+    errors = []
+    try:
+        aliases = get_group_email_aliases(None, None)
+        if not aliases:
+            errors.append(checks.Error(
+                "Found no aliases in the group email aliases file",
+                hint="Please run ietf/bin/generate-wg-aliases to generate them.",
+                obj=None,
+                id="datatracker.E0002",
+            ))
+    except IOError as e:
+        errors.append(checks.Error(
+            "Could not read group email aliases:\n   %s" % e,
+            hint="Please run ietf/bin/generate-wg-aliases to generate them.",
+            obj=None,
+            id="datatracker.E0003",
+        ))
+        
+    return errors
+
+@checks.register('files')
+def check_doc_email_aliases_exists(app_configs, **kwargs):
+    from ietf.doc.views_doc import get_doc_email_aliases
+    errors = []
+    try:
+        aliases = get_doc_email_aliases(None)
+        if not aliases:
+            errors.append(checks.Critical(
+                "Found no aliases in the document email aliases file.",
+                hint="Please run ietf/bin/generate-draft-aliases to generate them.",
+                obj=None,
+                id="datatracker.E0004",
+            ))
+    except IOError as e:
+        errors.append(checks.Critical(
+            "Could not read document email aliases:\n   %s" % e,
+            hint="Please run ietf/bin/generate-draft-aliases to generate them.",
+            obj=None,
+            id="datatracker.E0005",
+        ))
+
+    return errors
+    
