a17b3c62f52c9ed385b7ae96fffc2f9e6cd289ed
Henrik Levkowetz <henrik@levkowetz.com>
2015-11-06 18:28:43 +0000

    Added a default settings_local for use in the docker image.  Tweaked the dockerfile some.  - Legacy-Id: 10435

---
+++ docker/settings_local.py
@@ -0,0 +1,16 @@
+SECRET_KEY = 'jzv$o93h_lzw4a0%0oz-5t5lk+ai=3f8x@uo*9ahu8w4i300o6'
+
+DATABASES = {
+    'default': {
+        'NAME': 'dev_ietf_utf8',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'django_dev',
+        'PASSWORD': 'mJM+#@Fgec',
+        },
+    }
+
+DATABASE_TEST_OPTIONS = {
+    'init_command': 'SET storage_engine=InnoDB',
+    }
+
+IDSUBMIT_IDNITS_BINARY = "/usr/local/bin/idnits"
