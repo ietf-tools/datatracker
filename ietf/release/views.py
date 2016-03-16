64f5473499d80be39bf44f61598ded4e484a529c
Henrik Levkowetz <henrik@levkowetz.com>
2016-03-16 14:06:09 +0000

    Added a workaround for a thread import lock problem (http://bugs.python.org/issue7980).  - Legacy-Id: 10941

--- ietf/release/views.py
+++ ietf/release/views.py
@@ -20,6 +20,10 @@ time.strptime('1984', '%Y')             # this will happen lazily when changelog
 import time
 time.strptime('1984', '%Y')
 
+# workaround for thread import lock problem, http://bugs.python.org/issue7980
+import time
+time.strptime('1984', '%Y')
+
 def trac_links(text):
     # changeset links
     text = re.sub(r'\[(\d+)\]', r'<a href="https://wiki.tools.ietf.org/tools/ietfdb/changeset/\1">[\1]</a>', text)
