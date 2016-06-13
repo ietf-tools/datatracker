289dd0c2a9b81712333739099917d64c69e146b0
Henrik Levkowetz <henrik@levkowetz.com>
2016-06-13 14:03:19 +0000

    Minor tweaks.  - Legacy-Id: 11343

--- ietf/person/migrations/0013_add_plain_name_aliases.py
+++ ietf/person/migrations/0013_add_plain_name_aliases.py
@@ -20,10 +20,11 @@ def plain_name(self):
 def add_plain_name_aliases(apps, schema_editor):
     Person = apps.get_model('person','Person')
     Alias  = apps.get_model('person','Alias')
+    print("")
     for person in Person.objects.all():
         name = plain_name(person)
         if name and not Alias.objects.filter(name=name):
-            print("Created alias %-24s for %s" % (name, person.name))
+            print(u"Creating alias %-24s for %s" % (name, person.name))
             alias = Alias(name=name, person=person)
             alias.save()
 
