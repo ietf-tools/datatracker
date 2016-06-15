c0ba793c695756adcc1205453bc02e57ba8ab678
Henrik Levkowetz <henrik@levkowetz.com>
2016-06-15 15:48:58 +0000

    Added the new models for mailinglists ...  - Legacy-Id: 11384

---
+++ ietf/mailinglists/models.py
@@ -0,0 +1,28 @@
+# Copyright The IETF Trust 2016, All Rights Reserved
+
+from django.db import models
+from django.core.validators import validate_email
+
+from ietf.person.models import Person
+
+class List(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=256)
+    advertised = models.BooleanField(default=True)
+    def __unicode__(self):
+        return "<List: %s>" % self.name
+
+class Subscribed(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    email = models.CharField(max_length=64, validators=[validate_email])
+    lists = models.ManyToManyField(List)
+    def __unicode__(self):
+        return "<Subscribed: %s at %s>" % (self.email, self.time)
+
+class Whitelisted(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    email = models.CharField("Email address", max_length=64, validators=[validate_email])
+    by = models.ForeignKey(Person)
+    def __unicode__(self):
+        return "<Whitelisted: %s at %s>" % (self.email, self.time)
+    
