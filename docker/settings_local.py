95bb23328c92897e6cee9dfdc60208ca31299700
Henrik Levkowetz <henrik@levkowetz.com>
2016-06-20 21:57:19 +0000

    Updated docker/settings_local.py  - Legacy-Id: 11428

--- docker/settings_local.py
+++ docker/settings_local.py
@@ -1,3 +1,5 @@
+import os
+
 SECRET_KEY = 'jzv$o93h_lzw4a0%0oz-5t5lk+ai=3f8x@uo*9ahu8w4i300o6'
 
 DATABASES = {
@@ -18,10 +20,4 @@ IDSUBMIT_REPOSITORY_PATH = "test/id/"
 IDSUBMIT_STAGING_PATH = "test/staging/"
 INTERNET_DRAFT_ARCHIVE_DIR = "test/archive/"
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-MEDIA_ROOT = BASE_DIR + '/media/'
-MEDIA_URL = '/media/'
-
-PHOTOS_DIRNAME = 'photos'
-PHOTOS_DIR = MEDIA_ROOT + PHOTOS_DIRNAME
+AGENDA_PATH = 'test/data/proceedings/'
