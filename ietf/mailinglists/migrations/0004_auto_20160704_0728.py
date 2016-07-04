79a3609729731889355228f57514cfd227f45245
Henrik Levkowetz <henrik@levkowetz.com>
2016-07-04 14:29:32 +0000

    Migration for tweakd plural forms.  - Legacy-Id: 11517

---
+++ ietf/mailinglists/migrations/0004_auto_20160704_0728.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mailinglists', '0003_import_subscribers'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='subscribed',
+            options={'verbose_name_plural': 'Subscribed'},
+        ),
+        migrations.AlterModelOptions(
+            name='whitelisted',
+            options={'verbose_name_plural': 'Whitelisted'},
+        ),
+    ]
