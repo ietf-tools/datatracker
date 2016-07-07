8de23cb7f7a18356f6b5a5536108d0460ccf8daf
Henrik Levkowetz <henrik@levkowetz.com>
2016-07-07 12:37:51 +0000

    Fixed some admin plural forms.  - Legacy-Id: 11541

---
+++ ietf/mailinglists/migrations/0004_auto_20160704_0728.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mailinglists', '0003_import_subscribers'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='subscribed',
+            options={'verbose_name_plural': 'Subscribed'},
+        ),
+        migrations.AlterModelOptions(
+            name='whitelisted',
+            options={'verbose_name_plural': 'Whitelisted'},
+        ),
+    ]
