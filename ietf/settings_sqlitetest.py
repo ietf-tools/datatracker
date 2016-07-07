fe65f07d77f9c525731036a819e66a6a90539862
Henrik Levkowetz <henrik@levkowetz.com>
2016-07-07 20:17:40 +0000

    Tweaked the test settings.  - Legacy-Id: 11549

--- ietf/settings_sqlitetest.py
+++ ietf/settings_sqlitetest.py
@@ -41,3 +41,7 @@ if TEST_CODE_COVERAGE_CHECKER and not TEST_CODE_COVERAGE_CHECKER._started: # pyf
     TEST_CODE_COVERAGE_CHECKER.start()                          # pyflakes:ignore
 
 NOMCOM_PUBLIC_KEYS_DIR=os.path.abspath("tmp-nomcom-public-keys-dir")
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'test/media/') # pyflakes:ignore
+MEDIA_URL = '/test/media/'
+PHOTOS_DIR = MEDIA_ROOT + PHOTOS_DIRNAME                            # pyflakes:ignore
