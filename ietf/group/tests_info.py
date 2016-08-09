4631df61eff80ecb2e086cac5729ea9e9ea01ee6
Robert Sparks <rjsparks@nostrum.com>
2016-08-09 18:31:05 +0000

    improve some test coverage. Commit ready for merge.  - Legacy-Id: 11767

--- ietf/group/tests_info.py
+++ ietf/group/tests_info.py
@@ -1201,6 +1201,9 @@ class StatusUpdateTests(TestCase):
         url = urlreverse('ietf.group.views.group_about_status_meeting',kwargs={'acronym':chair.group.acronym,'num':sess.meeting.number}) 
         response = self.client.get(url)
         self.assertEqual(response.status_code,200)
+        url = urlreverse('ietf.group.views.group_about_status_meeting',kwargs={'group_type':chair.group.type_id,'acronym':chair.group.acronym,'num':sess.meeting.number}) 
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
        
 class GroupParentLoopTests(TestCase):
 
