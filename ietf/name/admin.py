78bbd9564a0f285008f6f63e34c9775259507dcc
Henrik Levkowetz <henrik@levkowetz.com>
2016-11-12 05:34:58 +0000

    Added admin entries for a bunch of name models lacking them.  - Legacy-Id: 12325

--- ietf/name/admin.py
+++ ietf/name/admin.py
@@ -3,8 +3,10 @@ from ietf.name.models import (GroupTypeName, GroupStateName, RoleName, StreamNam
     DocRelationshipName, DocTypeName, DocTagName, StdLevelName, IntendedStdLevelName,
     DocReminderTypeName, BallotPositionName, SessionStatusName, TimeSlotTypeName,
     ConstraintName, NomineePositionStateName, FeedbackTypeName, DBTemplateTypeName,
-    DraftSubmissionStateName, RoomResourceName)
-
+    DraftSubmissionStateName, RoomResourceName, GroupMilestoneStateName, MeetingTypeName,
+    LiaisonStatementPurposeName, IprDisclosureStateName, IprLicenseTypeName,
+    IprEventTypeName , IprDisclosureStateName, IprLicenseTypeName, IprEventTypeName,
+    LiaisonStatementState, LiaisonStatementEventTypeName, LiaisonStatementTagName, )
 
 class NameAdmin(admin.ModelAdmin):
     list_display = ["slug", "name", "desc", "used"]
@@ -22,20 +24,27 @@ class GroupTypeNameAdmin(NameAdmin):
     list_display = ["slug", "name", "verbose_name", "desc", "used"]
 admin.site.register(GroupTypeName, GroupTypeNameAdmin)
 
-
-admin.site.register(GroupStateName, NameAdmin)
-admin.site.register(RoleName, NameAdmin)
-admin.site.register(StreamName, NameAdmin)
-admin.site.register(DocTagName, NameAdmin)
-admin.site.register(StdLevelName, NameAdmin)
-admin.site.register(IntendedStdLevelName, NameAdmin)
-admin.site.register(DocReminderTypeName, NameAdmin)
 admin.site.register(BallotPositionName, NameAdmin)
-admin.site.register(SessionStatusName, NameAdmin)
-admin.site.register(TimeSlotTypeName, NameAdmin)
 admin.site.register(ConstraintName, NameAdmin)
-admin.site.register(NomineePositionStateName, NameAdmin)
-admin.site.register(FeedbackTypeName, NameAdmin)
 admin.site.register(DBTemplateTypeName, NameAdmin)
+admin.site.register(DocReminderTypeName, NameAdmin)
+admin.site.register(DocTagName, NameAdmin)
 admin.site.register(DraftSubmissionStateName, NameAdmin)
+admin.site.register(FeedbackTypeName, NameAdmin)
+admin.site.register(GroupMilestoneStateName, NameAdmin)
+admin.site.register(GroupStateName, NameAdmin)
+admin.site.register(IntendedStdLevelName, NameAdmin)
+admin.site.register(IprDisclosureStateName, NameAdmin)
+admin.site.register(IprLicenseTypeName, NameAdmin)
+admin.site.register(IprEventTypeName, NameAdmin)
+admin.site.register(LiaisonStatementState, NameAdmin)
+admin.site.register(LiaisonStatementEventTypeName, NameAdmin)
+admin.site.register(LiaisonStatementTagName, NameAdmin)
+admin.site.register(MeetingTypeName, NameAdmin)
+admin.site.register(NomineePositionStateName, NameAdmin)
+admin.site.register(RoleName, NameAdmin)
 admin.site.register(RoomResourceName, NameAdmin)
+admin.site.register(SessionStatusName, NameAdmin)
+admin.site.register(StdLevelName, NameAdmin)
+admin.site.register(StreamName, NameAdmin)
+admin.site.register(TimeSlotTypeName, NameAdmin)
