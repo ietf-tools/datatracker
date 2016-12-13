d6c837a9ceffc4fae9ef1b681c9335953cb980f7
Henrik Levkowetz <henrik@levkowetz.com>
2016-12-13 11:43:49 +0000

    Set version info to release version 6.40.0 before branching.  - Legacy-Id: 12511

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -4,13 +4,13 @@
 import checks                           # pyflakes:ignore
 
 # Don't add patch number here:
-__version__ = "6.39.2.dev0"
+__version__ = "6.40.0"
 
 # set this to ".p1", ".p2", etc. after patching
 __patch__   = ""
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$ (dev) Latest release: Rev. 12496 "
+__rev__     = "$Rev$"
 
 __id__      = "$Id$"
