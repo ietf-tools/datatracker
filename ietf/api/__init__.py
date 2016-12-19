244a7a96e469c4c9856e3043bf0fbf7ed2df9f0a
Henrik Levkowetz <henrik@levkowetz.com>
2016-12-19 13:52:58 +0000

    Import import_module directly instead of through django, which is deprecated.  - Legacy-Id: 12581

--- ietf/api/__init__.py
+++ ietf/api/__init__.py
@@ -134,7 +134,7 @@ def autodiscover():
     """
 
     from django.conf import settings
-    from django.utils.importlib import import_module
+    from importlib import import_module
     from django.utils.module_loading import module_has_submodule
 
     for app in settings.INSTALLED_APPS:
