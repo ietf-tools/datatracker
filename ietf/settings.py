334445d0d0c8983b492490541a85045688090e8c
Henrik Levkowetz <henrik@levkowetz.com>
2016-12-22 19:44:34 +0000

    Added a warnings ignore for an issue which pops up on ietfa.  - Legacy-Id: 12626

--- ietf/settings.py
+++ ietf/settings.py
@@ -13,6 +13,7 @@ warnings.simplefilter("always", DeprecationWarning)
 warnings.filterwarnings("ignore", message="Report.file_reporters will no longer be available in Coverage.py 4.2", module="coverage.report")
 warnings.filterwarnings("ignore", message="initial_data fixtures are deprecated. Use data migrations instead.", module="django.core.management.commands.loaddata")
 warnings.filterwarnings("ignore", message="The popen2 module is deprecated.  Use the subprocess module.", module="ietf.utils.pipe")
+warnings.filterwarnings("ignore", message="django.contrib.contenttypes.generic is deprecated and will be removed in Django 1.9.", module="south.modelsinspector")
 
 try:
     import syslog
