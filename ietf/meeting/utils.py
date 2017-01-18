7d25922e9b28f0dacae52670a8c5c8ac3b191b50
Henrik Levkowetz <henrik@levkowetz.com>
2017-01-18 19:24:14 +0000

    Enabled correct time sorting and reporting for interim meetings on /group/{{acronym}}/meetings/, and tweaked the date format.  Fixes issue #2145.  - Legacy-Id: 12669

--- ietf/meeting/utils.py
+++ ietf/meeting/utils.py
@@ -13,22 +13,18 @@ from ietf.group.utils import can_manage_materials
 def group_sessions(sessions):
 
     def sort_key(session):
-        if session.meeting.type.slug=='ietf':
-            official_sessions = session.timeslotassignments.filter(schedule=session.meeting.agenda)
-            if official_sessions:
-                return official_sessions.first().timeslot.time
-            elif session.meeting.date:
-                return datetime.datetime.combine(session.meeting.date,datetime.datetime.min.time())
-            else:
-                return session.requested
-        else:
-            # TODO: use timeslots for interims once they have them
+        official_sessions = session.timeslotassignments.filter(schedule=session.meeting.agenda)
+        if official_sessions:
+            return official_sessions.first().timeslot.time
+        elif session.meeting.date:
             return datetime.datetime.combine(session.meeting.date,datetime.datetime.min.time())
+        else:
+            return session.requested
 
     for s in sessions:
         s.time=sort_key(s)
 
-    sessions = sorted(sessions,key=lambda s:s.time,reverse=True)
+    sessions = sorted(sessions,key=lambda s:s.time)
 
     today = datetime.date.today()
     future = []
@@ -41,6 +37,10 @@ def group_sessions(sessions):
             in_progress.append(s)
         else:
             past.append(s)
+
+    # List future and in_progress meetings with ascending time, but past
+    # meetings with descending time
+    past.reverse()
 
     return future, in_progress, past
 
