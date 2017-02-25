84527c9df6f3dff3bae1e1df6cfcc9543f069263
Henrik Levkowetz <henrik@levkowetz.com>
2017-02-25 20:32:20 +0000

    Added some code to utils.urls.url() to output remaining explicit url names together with the matching dotted-path to the view function.  - Legacy-Id: 12921

--- ietf/utils/urls.py
+++ ietf/utils/urls.py
@@ -5,8 +5,16 @@ import debug                            # pyflakes:ignore
 from inspect import isclass
 
 from django.conf.urls import url as django_url
+from django.views.generic import View
 
 def url(regex, view, kwargs=None, name=None):
+    if callable(view) and hasattr(view, '__name__'):
+        view_name = "%s.%s" % (view.__module__, view.__name__)
+    elif callable(view) and hasattr(view, '__class__'):
+        view_name = "%s.%s" % (view.__module__, view.__class__.__name__)
+    else:
+        view_name = regex
+
     if name:
         branch = 'name'
     elif isinstance(view, (list, tuple)):
@@ -16,12 +24,18 @@ def url(regex, view, kwargs=None, name=None):
         name = view
     elif callable(view) and hasattr(view, '__name__'):
         branch = 'callable'
-        name = "%s.%s" % (view.__module__, view.__name__)
+        name = view_name
+    elif isinstance(view, View):
+        branch = 'view'
     elif isclass(view) or hasattr(view, '__class__'):
         branch = 'class'
     else:
         branch = 'notimpl'
         raise NotImplementedError("Auto-named url from view of type %s: %s" % (type(view), view))
+    if branch == 'name':
+        # List explicit url names with accompanying view dotted-path:
+        #debug.say("%s %s" % (view_name, name))
+        pass
     if name:
         branch = branch                 # silent pyflakes
         #debug.show('branch')
