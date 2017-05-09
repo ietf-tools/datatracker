8030fb39654f1d1a9163a2a7f8bee38243fe3d38
Robert Sparks <rjsparks@nostrum.com>
2017-05-09 20:16:11 +0000

    Created an EditedAuthorsDocEvent to capture the basis or reason for changing a documentauthor set. Mapped a large number of email addresses found from the RFC Editor database and from parsing RFCs to the right Person objects, and creating Person objects as necessary. Updated the documentauthors set for RFCs based on reconciling what the tracker already knew with the RFC Editor database and the parsing the RFCs. Manually inspected the Updates to ensure no author was inappropriately removed from the set. Commit ready for merge.  - Legacy-Id: 13289

--- ietf/doc/models.py
+++ ietf/doc/models.py
@@ -832,6 +832,7 @@ EVENT_TYPES = [
     ("changed_document", "Changed document metadata"),
     ("added_comment", "Added comment"),
     ("added_message", "Added message"),
+    ("edited_authors", "Edited the documents author list"),
 
     ("deleted", "Deleted document"),
 
@@ -1060,3 +1061,10 @@ class DeletedEvent(models.Model):
 
     def __unicode__(self):
         return u"%s by %s %s" % (self.content_type, self.by, self.time)
+
+class EditedAuthorsDocEvent(DocEvent):
+    """ Capture the reasoning or authority for changing a document author list.
+        Allows programs to recognize and not change lists that have been manually verified and corrected.
+        Example 'basis' values might be from ['manually adjusted','recomputed by parsing document', etc.]
+    """
+    basis = models.CharField(help_text="What is the source or reasoning for the changes to the author list",max_length=255)
