b5319c576ac0bd133aea66a40734f49aa12d55cc
Henrik Levkowetz <henrik@levkowetz.com>
2017-05-12 15:27:23 +0000

    Updated the utils.DumpInfo model to match the database.  - Legacy-Id: 13330

--- ietf/utils/models.py
+++ ietf/utils/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class DumpInfo(models.Model):
     date = models.DateTimeField()
     host = models.CharField(max_length=128)
+    tz   = models.CharField(max_length=32, default='UTC')
     
