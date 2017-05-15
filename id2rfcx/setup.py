d90d6c3f56784a7881b1c8214d9d50153632cba2
Henrik Levkowetz <henrik@levkowetz.com>
2017-05-15 22:17:02 +0000

    Changed the placement of the manpage in setup.py.  Added the manpage file to the listed datafiles.  - Legacy-Id: 13365

--- id2rfcx/setup.py
+++ id2rfcx/setup.py
@@ -95,14 +95,14 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'id2xml': ['data/*',],
+        'id2xml': ['data/*', 'id2xml.1.gz', ],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('share/man/man1', ['id2xml/id2xml.1.gz',])],
+    data_files=[('/usr/local/share/man/man1', ['id2xml/id2xml.1.gz',])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
