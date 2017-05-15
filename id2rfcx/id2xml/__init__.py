076db71e78843ab7f50bb0f7102d17cf79b6b147
Henrik Levkowetz <henrik@levkowetz.com>
2017-05-15 13:20:08 +0000

    Added some information to __init__.py  - Legacy-Id: 13358

--- id2rfcx/id2xml/__init__.py
+++ id2rfcx/id2xml/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.9.3"
+__version__ = "0.9.3.dev0"
+
+__date__    = "$Date: 2017-05-14 03:01:04 -0700 (Sun, 14 May 2017) $"
+
+__rev__     = "$Rev:$"
+
+__id__      = "$Id: __init__.py 13345 2017-05-14 10:01:04Z henrik@levkowetz.com $"
