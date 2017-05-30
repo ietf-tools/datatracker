e4d510f68150b219774477609ae1092ab5c09c9b
Henrik Levkowetz <henrik@levkowetz.com>
2017-05-30 17:14:36 +0000

    Creating new tag 'tags/id2xml/dev/1.0.1.dev0' from branch/elft/id2xml  - Legacy-Id: 13470

---
+++ 1.0.1.dev0/id2xml/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.0.1.dev0"
+
+__date__    = "$Date: 2017-05-14 03:01:04 -0700 (Sun, 14 May 2017) $"
+
+__rev__     = "$Rev:$"
+
+__id__      = "$Id: __init__.py 13345 2017-05-14 10:01:04Z henrik@levkowetz.com $"
