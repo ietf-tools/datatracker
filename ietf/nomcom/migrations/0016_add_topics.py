f2b0881dae01a4be776675b568212d6ce8ad99f2
Henrik Levkowetz <henrik@levkowetz.com>
2017-06-05 12:27:45 +0000

    Corrected the add_topics migration  - Legacy-Id: 13525

--- ietf/nomcom/migrations/0016_add_topics.py
+++ ietf/nomcom/migrations/0016_add_topics.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
             name='Topic',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('subject', models.CharField(help_text=b'This short description will appear on the Feedback pages.', max_length=255, verbose_name=b'Subject')),
+                ('subject', models.CharField(help_text=b'This short description will appear on the Feedback pages.', max_length=255, verbose_name=b'Name')),
                 ('accepting_feedback', models.BooleanField(default=False, verbose_name=b'Is accepting feedback')),
                 ('audience', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='name.TopicAudienceName')),
                 ('description', models.ForeignKey(editable=False, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='description', to='dbtemplate.DBTemplate')),
