dda271bd7e0ffe48207bc7af7853307817bb7d38
Henrik Levkowetz <henrik@levkowetz.com>
2017-09-06 14:46:49 +0000

    Fixed another issue with the meeting materials urls, related to the href() normalisation work.  - Legacy-Id: 14101

--- ietf/meeting/urls.py
+++ ietf/meeting/urls.py
@@ -80,7 +80,7 @@ type_ietf_only_patterns_id_optional = [
     url(r'^week-view(?:.html)?/?$', views.week_view),
     url(r'^room-view(?:.html)?/?$', views.room_view),
     url(r'^materials(?:.html)?/?$', views.materials),
-    url(r'^materials/%(document)s$' % settings.URL_REGEXPS, views.materials_document),
+    url(r'^materials/%(document)s/?$' % settings.URL_REGEXPS, views.materials_document),
     url(r'^session/?$', views.materials_editable_groups),
     url(r'^proceedings(?:.html)?/?$', views.proceedings),
     url(r'^proceedings(?:.html)?/finalize/?$', views.finalize_proceedings),
