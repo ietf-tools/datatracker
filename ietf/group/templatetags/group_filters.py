dfaf5a56ac40b2d51987c174d3722497eb63a26a
Henrik Levkowetz <henrik@levkowetz.com>
2017-10-13 11:58:42 +0000

    Renamed groupfilters to group_filters  - Legacy-Id: 14206

--- ietf/group/templatetags/group_filters.py
+++ ietf/group/templatetags/group_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 def has_sessions(group,num):
     return group.session_set.filter(meeting__number=num).exists()
 
+@register.filter
+def active_roles(queryset):
+    return queryset.filter(state_id='active').exclude(group__acronym='secretariat')
+    
