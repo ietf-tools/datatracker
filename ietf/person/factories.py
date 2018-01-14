87f4997bf561a844f2375721e1d44df3cdc56fdb
Henrik Levkowetz <henrik@levkowetz.com>
2018-01-14 19:20:24 +0000

    Fixed unbalanced parentheses.  - Legacy-Id: 14520

--- ietf/person/factories.py
+++ ietf/person/factories.py
@@ -24,7 +24,7 @@ def random_faker():
     # extraction code, and also don't seem to match the way people with arabic
     # names romanize arabic names.  Exlude those locales from name generation
     # in order to avoid test failures.
-    locales = set( [ l for l in faker.config.AVAILABLE_LOCALES if not (l.startswith('ar_') or l.startswith('sg_') ] )
+    locales = set( [ l for l in faker.config.AVAILABLE_LOCALES if not (l.startswith('ar_') or l.startswith('sg_')) ] )
     return faker.Faker(random.sample(locales, 1)[0])
 
 class UserFactory(factory.DjangoModelFactory):
