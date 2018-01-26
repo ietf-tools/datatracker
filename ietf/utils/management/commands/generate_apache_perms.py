a3d1d5595d19275f641af3e0b110af7f40a00cfe
Henrik Levkowetz <henrik@levkowetz.com>
2018-01-26 22:40:48 +0000

    Added an option to list the slugs that can be used with the generate_apache_perms management command.  - Legacy-Id: 14576

--- ietf/utils/management/commands/generate_apache_perms.py
+++ ietf/utils/management/commands/generate_apache_perms.py
@@ -1,27 +1,41 @@
 # Copyright 2016 IETF Trust
 
+import sys
+
 #from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 import debug                            # pyflakes:ignore
 
 from ietf.group.models import Role
+from ietf.name.models import GroupTypeName, RoleName
 
 class Command(BaseCommand):
     help = "Create apache permission stanzas for given roles."
 
     def add_arguments(self, parser):
-        parser.add_argument('roles', nargs='+',
+        parser.add_argument('roles', nargs='*',
             help="One or more group:type:role specifications.  Use '*' as wildcard. Use group acronyms "
             "for groups and group type and role name slugs for type and role. "
             "Examples:  all ADs: *:*:ad,  core chairs: core:*:chair,  Directorate secretaries: *:dir:secr."
             )
+        parser.add_argument('-l', '--list-slugs', action='store_true', default=False,
+            help="List the group type and role slugs")
 
     # --------------------------------------------------------------------
 
     def handle(self, *filenames, **options):
         self.verbosity = options['verbosity']
         self.errors = []
+
+        if options['list_slugs']:
+            self.stdout.write("Group types:\n\n")
+            for t in GroupTypeName.objects.all().order_by('slug'):
+                self.stdout.write("   %-16s  %s\n" % (t.slug, t.name))
+            self.stdout.write("\nRoles:\n\n")
+            for r in RoleName.objects.all().order_by('slug'):
+                self.stdout.write("   %-16s  %s\n" % (r.slug, r.name))
+            sys.exit(0)
 
         for role_spec in options["roles"]:
             try:
