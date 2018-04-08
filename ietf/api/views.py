33e7be35e3a01ad0666a0833a9ace0584579428f
Henrik Levkowetz <henrik@levkowetz.com>
2018-04-08 13:24:14 +0000

    Added more information about personal API keys to the API help page.  - Legacy-Id: 15031

--- ietf/api/views.py
+++ ietf/api/views.py
@@ -48,5 +48,5 @@ def api_help(request):
     key = JWK()
     # import just public part here, for display in info page
     key.import_from_pem(settings.API_PUBLIC_KEY_PEM)
-    return render(request, "api/index.html", {'key': key, })
+    return render(request, "api/index.html", {'key': key, 'settings':settings, })
     
