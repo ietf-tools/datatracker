151b5bc39a41bd09430476f4ad75f7a84dd39b75
Robert Sparks <rjsparks@nostrum.com>
2018-07-12 20:33:39 +0000

    The cookie tests were all calling make_test_data, but not using anything it created. Removed the calls. Commit ready for merge.  - Legacy-Id: 15320

--- ietf/cookies/tests.py
+++ ietf/cookies/tests.py
@@ -5,13 +5,11 @@ from django.urls import reverse as urlreverse
 
 import debug                            # pyflakes:ignore
 
-from ietf.utils.test_data import make_test_data
 from ietf.utils.test_utils import TestCase
 
 
 class CookieTests(TestCase):
     def test_settings_defaults(self):
-        make_test_data()
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
         self.assertListEqual([], r.cookies.keys())
@@ -23,7 +21,6 @@ class CookieTests(TestCase):
 
         
     def test_settings_defaults_from_cookies(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '7', 'expires_soon' : 7, 'left_menu': 'on', })
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -35,7 +32,6 @@ class CookieTests(TestCase):
         self.assertEqual(q('div a.active[href="/accounts/settings/left_menu/on"]').contents(),    ['On'])
         
     def test_settings_values_from_cookies_garbage(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'foo', 'new_enough' : 'foo', 'expires_soon' : 'foo', 'left_menu': 'foo', })
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -46,7 +42,6 @@ class CookieTests(TestCase):
         self.assertEqual(q('div a.active[href="/accounts/settings/left_menu/off"]').contents(),   ['Off'])
 
     def test_settings_values_from_cookies_random(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'zappa', 'new_enough' : '365', 'expires_soon' : '5', 'left_menu': 'zappa', })
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -62,7 +57,6 @@ class CookieTests(TestCase):
 #         self.assertNotRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon')
 
     def test_settings_values_from_cookies_1(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'new_enough' : '90', 'expires_soon' : 7, 'left_menu': 'off', })
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -77,7 +71,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*7 days')
 
     def test_settings_values_from_cookies_2(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '60', 'expires_soon' : 14, 'left_menu': 'on', })
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -92,7 +85,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*14 days')
        
     def test_settings_values_from_cookies_3(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'new_enough' : '30', 'expires_soon' : 21, 'left_menu': 'off'})
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -107,7 +99,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*21 days')
 
     def test_settings_values_from_cookies_4(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '21', 'expires_soon' : 30, 'left_menu': 'on', })
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -122,7 +113,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*30 days')
 
     def test_settings_values_from_cookies_5(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'new_enough' : '14', 'expires_soon' : 60, 'left_menu': 'off', })
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -137,7 +127,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*60 days')
 
     def test_settings_values_from_cookies_6(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '7', 'expires_soon' : 90, 'left_menu': 'on', })
         r = self.client.get(urlreverse("ietf.cookies.views.preferences"))
         self.assertEqual(r.status_code, 200)
@@ -152,7 +141,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*90 days')
 
     def test_full_draft(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14})
         r = self.client.get(urlreverse("ietf.cookies.views.full_draft")) # no value: reset
         self.assertEqual(r.status_code, 200)
@@ -167,7 +155,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*14 days')
 
     def test_full_draft_on(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14})
         r = self.client.get(urlreverse("ietf.cookies.views.full_draft", kwargs=dict(enabled="on")))
         self.assertEqual(r.status_code, 200)
@@ -178,7 +165,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*full_draft.*on')
 
     def test_full_draft_off(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14})
         r = self.client.get(urlreverse("ietf.cookies.views.full_draft", kwargs=dict(enabled="off")))
         self.assertEqual(r.status_code, 200)
@@ -191,7 +177,6 @@ class CookieTests(TestCase):
 #        self.assertRegexpMatches(r.content, r'ietf-highlight-y.*full_draft.*off')
 
     def test_full_draft_foo(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14})
         r = self.client.get(urlreverse("ietf.cookies.views.full_draft", kwargs=dict(enabled="foo")))
         self.assertEqual(r.status_code, 200)
@@ -203,7 +188,6 @@ class CookieTests(TestCase):
 #        self.assertRegexpMatches(r.content, r'ietf-highlight-y.*full_draft.*off')
 
     def test_left_menu(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14, 'left_menu': 'on', })
         r = self.client.get(urlreverse("ietf.cookies.views.left_menu")) # no value: reset
         self.assertEqual(r.status_code, 200)
@@ -216,7 +200,6 @@ class CookieTests(TestCase):
         self.assertEqual(q('div a.active[href="/accounts/settings/expires_soon/14"]').contents(), ['14 days'])
 
     def test_left_menu_on(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14, 'left_menu': 'off', })
         r = self.client.get(urlreverse("ietf.cookies.views.left_menu", kwargs=dict(enabled="on")))
         self.assertEqual(r.status_code, 200)
@@ -226,7 +209,6 @@ class CookieTests(TestCase):
         self.assertEqual(q('div a.active[href="/accounts/settings/left_menu/on"]').contents(),  ['On'])
 
     def test_left_menu_off(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14, 'left_menu': 'off', })
         r = self.client.get(urlreverse("ietf.cookies.views.left_menu", kwargs=dict(enabled="off")))
         self.assertEqual(r.status_code, 200)
@@ -236,7 +218,6 @@ class CookieTests(TestCase):
         self.assertEqual(q('div a.active[href="/accounts/settings/left_menu/off"]').contents(),  ['Off'])
 
     def test_left_menu_foo(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14, 'left_menu': 'off', })
         r = self.client.get(urlreverse("ietf.cookies.views.left_menu", kwargs=dict(enabled="foo")))
         self.assertEqual(r.status_code, 200)
@@ -245,7 +226,6 @@ class CookieTests(TestCase):
         self.assertEqual(q('div a.active[href="/accounts/settings/left_menu/off"]').contents(),  ['Off'])
 
     def test_new_enough(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14})
         r = self.client.get(urlreverse("ietf.cookies.views.new_enough")) # no value: reset
         self.assertEqual(r.status_code, 200)
@@ -260,7 +240,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*14 days')
 
     def test_new_enough_7(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'new_enough' : '14', 'expires_soon' : 21})
         r = self.client.get(urlreverse("ietf.cookies.views.new_enough", kwargs=dict(days="7")))
         self.assertEqual(r.status_code, 200)
@@ -275,7 +254,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*21 days')
 
     def test_new_enough_14(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'new_enough' : '7', 'expires_soon' : 99})
         r = self.client.get(urlreverse("ietf.cookies.views.new_enough", kwargs=dict(days="14")))
         self.assertEqual(r.status_code, 200)
@@ -290,7 +268,6 @@ class CookieTests(TestCase):
 #         self.assertNotRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon')
 
     def test_new_enough_21(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'new_enough' : '14', 'expires_soon' : 90})
         r = self.client.get(urlreverse("ietf.cookies.views.new_enough", kwargs=dict(days="21")))
         self.assertEqual(r.status_code, 200)
@@ -305,7 +282,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*90 days')
 
     def test_new_enough_30(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 7})
         r = self.client.get(urlreverse("ietf.cookies.views.new_enough", kwargs=dict(days="30")))
         self.assertEqual(r.status_code, 200)
@@ -320,7 +296,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*7 days')
 
     def test_new_enough_60(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '14', 'expires_soon' : 14})
         r = self.client.get(urlreverse("ietf.cookies.views.new_enough", kwargs=dict(days="60")))
         self.assertEqual(r.status_code, 200)
@@ -335,7 +310,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*14 days')
 
     def test_new_enough_90(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'new_enough' : '22', 'expires_soon' : 60})
         r = self.client.get(urlreverse("ietf.cookies.views.new_enough", kwargs=dict(days="90")))
         self.assertEqual(r.status_code, 200)
@@ -350,7 +324,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*expires_soon.*60 days')
 
     def test_expires_soon(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'expires_soon' : '14', 'new_enough' : 14})
         r = self.client.get(urlreverse("ietf.cookies.views.expires_soon")) # no value: reset
         self.assertEqual(r.status_code, 200)
@@ -365,7 +338,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*new_enough.*14 days')
 
     def test_expires_soon_7(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'expires_soon' : '14', 'new_enough' : 21})
         r = self.client.get(urlreverse("ietf.cookies.views.expires_soon", kwargs=dict(days="7")))
         self.assertEqual(r.status_code, 200)
@@ -380,7 +352,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*new_enough.*21 days')
 
     def test_expires_soon_14(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'expires_soon' : '7', 'new_enough' : 99})
         r = self.client.get(urlreverse("ietf.cookies.views.expires_soon", kwargs=dict(days="14")))
         self.assertEqual(r.status_code, 200)
@@ -395,7 +366,6 @@ class CookieTests(TestCase):
 #         self.assertNotRegexpMatches(r.content, r'ietf-highlight-y.*new_enough')
 
     def test_expires_soon_21(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'on', 'expires_soon' : '14', 'new_enough' : 90})
         r = self.client.get(urlreverse("ietf.cookies.views.expires_soon", kwargs=dict(days="21")))
         self.assertEqual(r.status_code, 200)
@@ -410,7 +380,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*new_enough.*90 days')
 
     def test_expires_soon_30(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'expires_soon' : '14', 'new_enough' : 7})
         r = self.client.get(urlreverse("ietf.cookies.views.expires_soon", kwargs=dict(days="30")))
         self.assertEqual(r.status_code, 200)
@@ -425,7 +394,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*new_enough.*7 days')
 
     def test_expires_soon_60(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'expires_soon' : '14', 'new_enough' : 14})
         r = self.client.get(urlreverse("ietf.cookies.views.expires_soon", kwargs=dict(days="60")))
         self.assertEqual(r.status_code, 200)
@@ -440,7 +408,6 @@ class CookieTests(TestCase):
 #         self.assertRegexpMatches(r.content, r'ietf-highlight-y.*new_enough.*14 days')
 
     def test_expires_soon_90(self):
-        make_test_data()
         self.client.cookies = SimpleCookie({'full_draft': 'off', 'expires_soon' : '22', 'new_enough' : 60})
         r = self.client.get(urlreverse("ietf.cookies.views.expires_soon", kwargs=dict(days="90")))
         self.assertEqual(r.status_code, 200)
