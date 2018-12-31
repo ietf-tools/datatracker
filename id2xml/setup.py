b61a879b475bd8abc6a87971758b51f06a0b573c
Henrik Levkowetz <henrik@levkowetz.com>
2018-12-31 14:41:06 +0000

    Removed installation of a man file in a fixed location from setup.py, as it does not work in a virtualenv.  - Legacy-Id: 15847

--- id2xml/setup.py
+++ id2xml/setup.py
@@ -150,7 +150,7 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('/usr/local/share/man/man1', ['id2xml/id2xml.1.gz',])],
+    # data_files=[('/usr/local/share/man/man1', ['id2xml/id2xml.1.gz',])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
