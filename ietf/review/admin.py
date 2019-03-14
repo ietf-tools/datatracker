334767fa6fe354138bb016d3a124ea4a00445b33
Robert Sparks <rjsparks@nostrum.com>
2019-03-14 19:14:33 +0000

    Some admin tweaks and removal of a dead template. Tests all pass now. Functionality is still not right.  - Legacy-Id: 16032

--- ietf/review/admin.py
+++ ietf/review/admin.py
@@ -3,7 +3,7 @@ import simple_history
 from django.contrib import admin
 
 from ietf.review.models import (ReviewerSettings, ReviewSecretarySettings, UnavailablePeriod,
-    ReviewWish, NextReviewerInTeam, ReviewRequest, ReviewTeamSettings )
+    ReviewWish, NextReviewerInTeam, ReviewRequest, ReviewAssignment, ReviewTeamSettings )
 
 class ReviewerSettingsAdmin(simple_history.admin.SimpleHistoryAdmin):
     def acronym(self, obj):
@@ -60,6 +60,15 @@ class ReviewRequestAdmin(admin.ModelAdmin):
     search_fields = ["doc__name"]
 
 admin.site.register(ReviewRequest, ReviewRequestAdmin)
+
+class ReviewAssignmentAdmin(admin.ModelAdmin):
+    list_display = ["review_request", "reviewer", "assigned_on", "result"]
+    list_filter = ["result", "state"]
+    ordering = ["-id"]
+    raw_id_fields = ["reviewer", "result"]
+    search_fields = ["review_request__doc__name"]
+
+admin.site.register(ReviewAssignment, ReviewAssignmentAdmin)
 
 class ReviewTeamSettingsAdmin(admin.ModelAdmin):
     list_display = ["group", ] 
