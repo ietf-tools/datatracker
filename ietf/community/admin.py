c9a7571e0ae6bf88cd73cc12127a186f8b6d6352
Henrik Levkowetz <henrik@levkowetz.com>
2019-06-17 12:12:46 +0000

    Changed a field to use raw_id to avoid choice fields with 90000 entries  - Legacy-Id: 16271

--- ietf/community/admin.py
+++ ietf/community/admin.py
@@ -1,5 +1,7 @@
-
+# Copyright The IETF Trust 2017-2019, All Rights Reserved
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.contrib import admin
 
 from ietf.community.models import CommunityList, SearchRule, EmailSubscription
@@ -11,7 +13,7 @@ admin.site.register(CommunityList, CommunityListAdmin)
 
 class SearchRuleAdmin(admin.ModelAdmin):
     list_display = [u'id', 'community_list', 'rule_type', 'state', 'group', 'person', 'text']
-    raw_id_fields = ['community_list', 'state', 'group', 'person']
+    raw_id_fields = ['community_list', 'state', 'group', 'person', 'name_contains_index']
 admin.site.register(SearchRule, SearchRuleAdmin)
 
 class EmailSubscriptionAdmin(admin.ModelAdmin):
