e9d38090999a8ce6b4e2be6a5bc69bca0a28f0dd
Henrik Levkowetz <henrik@levkowetz.com>
2020-01-17 00:49:01 +0000

    Refined the links from document stats pages to author pages so as to not link to non-existent pages.  Fixes 404 errors in the test crawler logs.  - Legacy-Id: 17237

--- ietf/person/templatetags/person_filters.py
+++ ietf/person/templatetags/person_filters.py
@@ -1,4 +1,4 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
+# Copyright The IETF Trust 2017-2020, All Rights Reserved
 
 import datetime
 
@@ -7,6 +7,7 @@ from django import template
 import debug                            # pyflakes:ignore
 
 from ietf.meeting.utils import is_nomcom_eligible as util_is_nomcom_eligible
+from ietf.person.models import Alias
 
 register = template.Library()
 
@@ -14,3 +15,11 @@ register = template.Library()
 def is_nomcom_eligible(person, date=datetime.date.today()):
     return util_is_nomcom_eligible(person,date)
 
+@register.filter
+def person_by_name(name):
+    "Look up a person record from name"
+    if not isinstance(name, (type(b''), type(u''))):
+        return None
+    alias = Alias.objects.filter(name=name).first()
+    return alias.person if alias else None
+    
