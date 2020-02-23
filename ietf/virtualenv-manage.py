d9b11f31d9d366029a52d702a185cd4209d372f7
Henrik Levkowetz <henrik@levkowetz.com>
2020-02-23 16:27:30 +0000

    More Py2/3 adaptations fetched from production.  - Legacy-Id: 17333

--- ietf/virtualenv-manage.py
+++ ietf/virtualenv-manage.py
@@ -1,26 +1,11 @@
-#!/usr/bin/env python
-# Copyright The IETF Trust 2016-2019, All Rights Reserved
+#!/a/www/ietf-datatracker/web/env/bin/python
+# Copyright The IETF Trust 2016-2020, All Rights Reserved
 # -*- coding: utf-8 -*-
 
-
-from __future__ import absolute_import, print_function, unicode_literals
-
-import io
 import os
 import sys
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# change directory so patches etc. will be picked up as expected
-os.chdir(path)
-
-# Virtualenv support
-virtualenv_activation = os.path.join(path, "env", "bin", "activate_this.py")
-if os.path.exists(virtualenv_activation):
-    exec(compile(io.open(virtualenv_activation, "rb").read(), virtualenv_activation, 'exec'), dict(__file__=virtualenv_activation))
-else:
-    raise RuntimeError("Could not find the expected virtual python environment.")
-
 if not path in sys.path:
     sys.path.insert(0, path)
 
