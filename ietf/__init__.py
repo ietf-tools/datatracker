6f4372f54b6d4451a8270b38ae877e38f6f16f0b
Henrik Levkowetz <henrik@levkowetz.com>
2020-04-08 16:52:23 +0000

    Set version info and settings back to development mode  - Legacy-Id: 17602

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -7,13 +7,13 @@ from __future__ import absolute_import, print_function, unicode_literals
 from . import checks                           # pyflakes:ignore
 
 # Don't add patch number here:
-__version__ = "6.125.0"
+__version__ = "6.125.1.dev0"
 
 # set this to ".p1", ".p2", etc. after patching
 __patch__   = ""
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$"
+__rev__     = "$Rev$ (dev) Latest release: Rev. 17600 "
 
 __id__      = "$Id$"
