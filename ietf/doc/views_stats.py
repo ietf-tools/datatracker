3e6a1cfb1196b5dc0438fdc673592293890db527
Henrik Levkowetz <henrik@levkowetz.com>
2020-05-01 11:37:32 +0000

    Limited the data for 'New draft revisions over time' on the personal profile page to type='draft'.  - Legacy-Id: 17720

--- ietf/doc/views_stats.py
+++ ietf/doc/views_stats.py
@@ -174,7 +174,8 @@ def chart_data_person_drafts(request, id):
     if not person:
         data = []
     else:
-        data = model_to_timeline_data(DocEvent, doc__documentauthor__person=person, type='new_revision')
+        data = model_to_timeline_data(DocEvent, doc__documentauthor__person=person, type='new_revision', doc__type_id='draft')
+    debug.show('len(data)')
     return JsonResponse(data, safe=False)
     
 
