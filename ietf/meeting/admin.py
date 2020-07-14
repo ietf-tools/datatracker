0d0db4b1544fbc18f8323fb88adef1fd761cff4f
Henrik Levkowetz <henrik@levkowetz.com>
2020-07-14 20:39:48 +0000

    Admin page tweak for the UrlResource admin page.  - Legacy-Id: 18173

--- ietf/meeting/admin.py
+++ ietf/meeting/admin.py
@@ -11,7 +11,7 @@ from ietf.meeting.models import (Meeting, Room, Session, TimeSlot, Constraint, S
 
 class UrlResourceAdmin(admin.ModelAdmin):
     list_display = ['name', 'room', 'url', ]
-    list_filter = ['room__meeting', ]
+    list_filter = ['name', 'room__meeting', ]
     raw_id_fields = ['room', ]
 admin.site.register(UrlResource, UrlResourceAdmin)
 
