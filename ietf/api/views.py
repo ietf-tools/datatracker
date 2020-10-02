6cd3e6c83e71aa6cb82007ab31ec14bb3c5b5528
Henrik Levkowetz <henrik@levkowetz.com>
2020-10-02 12:10:33 +0000

    Added Person.personextresource_set to the information returned in the personal-information JSON blob.  - Legacy-Id: 18543

--- ietf/api/views.py
+++ ietf/api/views.py
@@ -83,7 +83,9 @@ class PersonalInformationExportView(DetailView, JsonExportMixin):
             'sendqueue', 'nominee', 'topicfeedbacklastseen', 'alias', 'email', 'apikeys', 'personevent',
             'reviewersettings', 'reviewsecretarysettings', 'unavailableperiod', 'reviewwish',
             'nextreviewerinteam', 'reviewrequest', 'meetingregistration', 'submissionevent', 'preapproval',
-            'user', 'user__communitylist', ]
+            'user', 'user__communitylist', 'personextresource_set', ]
+
+
         return self.json_view(request, filter={'id':person.id}, expand=expand)
 
 
