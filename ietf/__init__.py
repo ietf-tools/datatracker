f2a894068429f6e62c5eadf10824dff5a017acaf
Robert Sparks <rjsparks@nostrum.com>
2021-06-06 14:57:09 +0000

    Set version info to release version 7.31.0 before branching.  - Legacy-Id: 19090

--- ietf/__init__.py
+++ ietf/__init__.py
@@ -5,13 +5,13 @@
 from . import checks                           # pyflakes:ignore
 
 # Don't add patch number here:
-__version__ = "7.30.1.dev0"
+__version__ = "7.31.0"
 
 # set this to ".p1", ".p2", etc. after patching
 __patch__   = ""
 
 __date__    = "$Date$"
 
-__rev__     = "$Rev$ (dev) Latest release: Rev. 19015 "
+__rev__     = "$Rev$"
 
 __id__      = "$Id$"
