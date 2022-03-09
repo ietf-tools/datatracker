0acfacaf2d6c77239717e83ba2101c20c7a3e920
Jennifer Richards <jennifer@painless-security.com>
2022-03-09 15:36:45 -0400

    ci: remove bs3 asset build step

--- dev/deploy/build.sh
+++ dev/deploy/build.sh
@@ -2,11 +2,3 @@
 
 echo "Installing NPM dependencies..."
 npm install
-
-echo "Building bootstrap 3 assets..."
-cd bootstrap
-npm install -g grunt-cli
-npm install
-grunt dist
-cp -r dist/. ../ietf/static/ietf/bootstrap/
-cd ..
