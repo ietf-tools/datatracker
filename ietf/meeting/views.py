300f717c3ddffcd6ad2cbf84fb0e622387f7b8d5
Jennifer Richards <jennifer@painless-security.com>
2022-03-30 18:38:47 -0300

    fix: avoid needlessly blocking import of session minutes (#3761)
    Fixes #3558

--- ietf/meeting/views.py
+++ ietf/meeting/views.py
@@ -4169,11 +4169,11 @@ def import_session_minutes(request, session_id, num):
     if current_minutes:
         try:
             with open(current_minutes.get_file_name()) as f:
-                if import_contents == Note.preprocess_source(f.read()):
+                if import_contents == f.read():
                     contents_changed = False
                     messages.warning(request, 'This document is identical to the current revision, no need to import.')
-        except FileNotFoundError:
-            pass  # allow import if the file is missing
+        except Exception:
+            pass  # Do not let a failure here prevent minutes from being updated.
 
     return render(
         request,
