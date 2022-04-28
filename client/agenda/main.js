730a242393702074e7f114693beef9830baa62dc
Nicolas Giard <github@ngpixel.com>
2022-04-28 02:05:21 +0000

    fix: download ics + move agenda into own component

--- client/agenda/main.js
+++ client/agenda/main.js
@@ -1,15 +1,6 @@
 import { createApp } from 'vue'
-// import PrimeVue from 'primevue/config'
 import App from './App.vue'
 
-// import 'primevue/resources/themes/saga-blue/theme.css'
-// import 'primevue/resources/primevue.min.css'
-// import 'primeicons/primeicons.css'
-
-const agendaData = JSON.parse(document.getElementById('agenda-data').textContent)
-
-const app = createApp(App, agendaData)
-
-// app.use(PrimeVue, { ripple: true })
+const app = createApp(App, {})
 
 app.mount('#app-agenda')
