ietf-tools/datatracker

commit 337d2ad38751b9c6ff5b6184b6343a13896214ce

feat: Switch to SVG logo; use it (and new bitmaps) in more places (#4035)
* feat: Switch to SVG logo; use it (and new bitmaps) in more places

* Run images through image_optim

* Copy icons from base template

* Tint icons red when in dev mode

ietf/static/css/ietf.scss
@@ -56,11 +56,6 @@ $popover-max-width: 100%;
 // Utilities
 @import "bootstrap/scss/utilities/api";
 
-// Make the bootstrap icons available.
-$bootstrap-icons-font-src: url("npm:bootstrap-icons/font/fonts/bootstrap-icons.woff2?a97b3594ad416896e15824f6787370e0") format("woff2"),
-url("npm:bootstrap-icons/font/fonts/bootstrap-icons.woff?a97b3594ad416896e15824f6787370e0") format("woff") !default;
-@import "bootstrap-icons/font/bootstrap-icons";
-
 // Leave room for fixed-top navbar...
 body {
     padding-top: 60px;
@@ -82,9 +77,18 @@ pre {
     margin-bottom: 0;
 }
 
-// Make the font size choice cascade down into group menu dropdowns.
-.leftmenu li>ul {
-    font-size: inherit;
+// Size the navbar brand image.
+.navbar-brand img {
+    height: auto;
+    width: 60px;
+}
+
+.leftmenu {
+    width: 13em;
+
+    li>ul {
+        font-size: inherit; // Make the font size choice cascade down into group menu dropdowns.
+    }
 }
 
 // Use fixed lengths for the first three columns of "meta" tables.
@@ -100,7 +104,7 @@ table tbody.meta {
         width: 7em;
     }
 
-    // See https://getbootstrap.com/docs/5.0/layout/breakpoints/#media-queries
+    // See https://getbootstrap.com/docs/5.1/layout/breakpoints/#media-queries
     @include media-breakpoint-up(md) {
 
         th:first-child,
@@ -251,7 +255,7 @@ table tbody.meta {
     border: 2 * $table-border-width solid #000;
 }
 
-// See https://getbootstrap.com/docs/5.0/customize/color/#all-colors
+// See https://getbootstrap.com/docs/5.1/customize/color/#all-colors
 $color-discuss: $red-500;
 $color-yes: $green-500;
 $color-noobj: $green-100;

ietf/static/js/ietf.js
@@ -16,6 +16,9 @@ import "bootstrap/js/dist/tooltip";
 
 import jquery from "jquery";
 
+// Make the bootstrap icons available. Yes, we require CSS in JS, parcel wants this.
+require("bootstrap-icons/font/bootstrap-icons.css");
+
 window.$ = window.jQuery = jquery;
 if (!process.env.BUILD_DEPLOY) {
     // get warnings for using deprecated jquery features

commit 54859a1b24477a4a232e6672dd6a95535fb851f8

fix: Fix include of bootstrap-icons
This got broken in #4035, which included some things it wasn't meant to.

ietf/static/css/ietf.scss
@@ -56,6 +56,11 @@ $popover-max-width: 100%;
 // Utilities
 @import "bootstrap/scss/utilities/api";
 
+// Make the bootstrap icons available.
+$bootstrap-icons-font-src: url("npm:bootstrap-icons/font/fonts/bootstrap-icons.woff2") format("woff2"),
+url("npm:bootstrap-icons/font/fonts/bootstrap-icons.woff") format("woff");
+@import "bootstrap-icons/font/bootstrap-icons";
+
 // Leave room for fixed-top navbar...
 body {
     padding-top: 60px;

ietf/static/js/ietf.js
@@ -16,9 +16,6 @@ import "bootstrap/js/dist/tooltip";
 
 import jquery from "jquery";
 
-// Make the bootstrap icons available. Yes, we require CSS in JS, parcel wants this.
-require("bootstrap-icons/font/bootstrap-icons.css");
-
 window.$ = window.jQuery = jquery;
 if (!process.env.BUILD_DEPLOY) {
     // get warnings for using deprecated jquery features