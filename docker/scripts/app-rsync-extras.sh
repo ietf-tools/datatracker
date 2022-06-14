17af088f7a7f21679c5878a6dd8cb55b9da450ae
Lars Eggert <lars@eggert.org>
2022-06-14 17:18:25 +0200

    Roll back erroneous commit

--- docker/scripts/app-rsync-extras.sh
+++ docker/scripts/app-rsync-extras.sh
@@ -33,9 +33,7 @@ done
 
 echo "Using destination $DEST_ROOT"
 
-EXCLUDE="$(mktemp)"
 cat << EOF > "$EXCLUDE"
-<<<<<<< HEAD
 *#
 *%
 *.1
@@ -76,42 +74,32 @@ rfc[0-9][0-9][0-9][0-9]
 EOF
 
 OPTS="-asz --no-owner --no-group --partial ${PROGRESS:+--info=progress2} --exclude-from=$EXCLUDE --del --delete-excluded"
-=======
-*.html
-*.xml
-*.pdf
-*.json
-*.p7s
-EOF
-
-OPTS="-as --no-owner --no-group --partial ${PROGRESS:+--info=progress2} --exclude-from=$EXCLUDE --del --delete-excluded"
->>>>>>> f64d7c67c (Interim commit)
 
 for dir in bofreq; do
     dest="$DEST_ROOT/ietf-ftp/$dir"
     mkdir -p "$dest"
     echo "Fetching $dest ..."
-    rsync $OPTS rsync.ietf.org::$dir/ $dest/
+    rsync -auz ${PROGRESS:+--info=progress2} rsync.ietf.org::$dir/ $dest/
 done
 
-for dir in internet-drafts charter conflict-reviews internet-drafts review rfc slides status-changes yang; do
+for dir in charter conflict-reviews internet-drafts review rfc slides status-changes yang; do
     dest="$DEST_ROOT/ietf-ftp/$dir"
     mkdir -p "$dest"
     echo "Fetching $dest ..."
-    rsync $OPTS rsync.ietf.org::everything-ftp/$dir/ $dest/
+    rsync -auz ${PROGRESS:+--info=progress2} rsync.ietf.org::everything-ftp/$dir/ $dest/
 done
 
 for dir in floor photo; do
     dest="$DEST_ROOT/media/$dir"
     mkdir -p "$dest"
     echo "Fetching $dest ..."
-    rsync $OPTS rsync.ietf.org::dev.media/$dir/ $dest/
+    rsync -auz ${PROGRESS:+--info=progress2} rsync.ietf.org::dev.media/$dir/ $dest/
 done
 
 dest="$DEST_ROOT/archive/id"
 mkdir -p "$dest"
 echo "Fetching $dest ..."
-rsync $OPTS rsync.ietf.org::id-archive/ $dest/
+rsync -auz ${PROGRESS:+--info=progress2} rsync.ietf.org::id-archive/ $dest/
 
 dest="$DEST_ROOT/www6s/proceedings"
 mkdir -p "$dest"
