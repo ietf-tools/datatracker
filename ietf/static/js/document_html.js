d27bc893a3556e860d48d4291556216a55164e85
Lars Eggert <lars@eggert.org>
2022-06-14 16:09:04 +0200

    Progress

---
+++ ietf/static/js/document_html.js
@@ -0,0 +1,15 @@
+// Only import what we need:
+// https://getbootstrap.com/docs/5.1/customize/optimize/
+
+// import "bootstrap/js/dist/alert";
+import "bootstrap/js/dist/button";
+// import "bootstrap/js/dist/carousel";
+import "bootstrap/js/dist/collapse";
+// import "bootstrap/js/dist/dropdown";
+// import "bootstrap/js/dist/modal";
+// import "bootstrap/js/dist/offcanvas";
+// import "bootstrap/js/dist/popover";
+// import "bootstrap/js/dist/scrollspy";
+// import "bootstrap/js/dist/tab";
+// import "bootstrap/js/dist/toast";
+import "bootstrap/js/dist/tooltip";
