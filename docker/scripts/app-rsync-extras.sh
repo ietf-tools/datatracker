01229dce505c2f2ea980e88c7c83db088c7e41c8
Lars Eggert <lars@eggert.org>
2022-06-17 15:14:04 +0300

    Fix

--- docker/scripts/app-rsync-extras.sh
+++ docker/scripts/app-rsync-extras.sh
@@ -33,6 +33,7 @@ done
 
 echo "Using destination $DEST_ROOT"
 
+EXCLUDE="$(mktemp)"
 cat << EOF > "$EXCLUDE"
 *#
 *%
@@ -79,27 +80,27 @@ for dir in bofreq; do
     dest="$DEST_ROOT/ietf-ftp/$dir"
     mkdir -p "$dest"
     echo "Fetching $dest ..."
-    rsync -auz ${PROGRESS:+--info=progress2} rsync.ietf.org::$dir/ $dest/
+    rsync $OPTS rsync.ietf.org::$dir/ $dest/
 done
 
 for dir in charter conflict-reviews internet-drafts review rfc slides status-changes yang; do
     dest="$DEST_ROOT/ietf-ftp/$dir"
     mkdir -p "$dest"
     echo "Fetching $dest ..."
-    rsync -auz ${PROGRESS:+--info=progress2} rsync.ietf.org::everything-ftp/$dir/ $dest/
+    rsync $OPTS rsync.ietf.org::everything-ftp/$dir/ $dest/
 done
 
 for dir in floor photo; do
     dest="$DEST_ROOT/media/$dir"
     mkdir -p "$dest"
     echo "Fetching $dest ..."
-    rsync -auz ${PROGRESS:+--info=progress2} rsync.ietf.org::dev.media/$dir/ $dest/
+    rsync $OPTS rsync.ietf.org::dev.media/$dir/ $dest/
 done
 
 dest="$DEST_ROOT/archive/id"
 mkdir -p "$dest"
 echo "Fetching $dest ..."
-rsync -auz ${PROGRESS:+--info=progress2} rsync.ietf.org::id-archive/ $dest/
+rsync $OPTS rsync.ietf.org::id-archive/ $dest/
 
 dest="$DEST_ROOT/www6s/proceedings"
 mkdir -p "$dest"
