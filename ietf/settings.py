3040752218e4920414ed77e707152adb353c7586
Robert Sparks <rjsparks@nostrum.com>
2022-06-20 17:34:10 -0500

    feat: use author-tools for idnits.

--- ietf/settings.py
+++ ietf/settings.py
@@ -538,7 +538,7 @@ INTERNAL_IPS = (
 # no slash at end
 IDTRACKER_BASE_URL = "https://datatracker.ietf.org"
 RFCDIFF_BASE_URL = "https://www.ietf.org/rfcdiff"
-IDNITS_BASE_URL = "https://www.ietf.org/tools/idnits"
+IDNITS_BASE_URL = "https://author-tools.ietf.org/api/idnits"
 XML2RFC_BASE_URL = "https://xml2rfc.tools.ietf.org/experimental.html"
 
 # The name of the method to use to invoke the test suite
