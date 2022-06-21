8b90ecd4aac78098e822ad3c90418dd33de40870
Robert Sparks <rjsparks@nostrum.com>
2022-06-21 15:02:02 -0500

    feat: add pronouns to Person and oidc claims. Fixes #4043. (#4059)
    * feat: add pronouns
    
    * fix: include migrations
    
    * fix: correct daggers on person form.
    
    * fix: clean pronouns
    
    * feat: add choices to pronouns
    
    * feat: show pronouns on public profile
    
    * feat: add pronouns to oidc userinfo
    
    * fix: move pronouns to new claim. Add tests.
    
    * fix: improve html generated by new widget
    
    * feat: use a MultiWidget for pronouns
    
    * refactor: use two fields on Person for the two types of pronoun entry.
    
    * chore: update copyrights

--- ietf/ietfauth/views.py
+++ ietf/ietfauth/views.py
@@ -1,4 +1,4 @@
-# Copyright The IETF Trust 2007-2020, All Rights Reserved
+# Copyright The IETF Trust 2007-2022, All Rights Reserved
 # -*- coding: utf-8 -*-
 #
 # Portions Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
