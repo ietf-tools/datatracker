cfa088634378a5f23db0ef949385b78066aafa1b
Lars Eggert <lars@eggert.org>
2022-06-22 19:59:32 +0300

    fix: Make the IPR search form work when enter is pressed (#4114)

--- ietf/static/js/ipr-search.js
+++ ietf/static/js/ipr-search.js
@@ -1,8 +1,8 @@
 $(document)
     .ready(function () {
         // hack the "All States" check box
-        $("#id_state")
-            .addClass("list-inline");
+        $("#id_state .form-check")
+            .addClass("form-check-inline");
 
         $("#id_state input[value!=all]")
             .on("change", function (e) {
@@ -20,18 +20,24 @@ $(document)
                 }
             });
 
+        $("form.ipr-search button[type=submit]")
+            .on("click", function (e) {
+                const value = $(e.target)
+                    .attr("value");
+                $("form.ipr-search input[name=submit]")
+                    .attr("value", value);
+            });
+
         // make enter presses submit through the nearby button
-        // FIXME: this seems to be broken
-        $("form.ipr-search input,select")
-            .on("keyup", function (e) {
-                var submitButton = $(this)
-                    .closest(".mb-3")
-                    .find('button[type=submit]');
-                if (e.which == 13 && submitButton.length > 0) {
-                    submitButton.trigger("click");
-                    return false;
-                } else {
-                    return true;
+        $("form.ipr-search input")
+            .on("keydown", function (e) {
+                if (e.key != "Enter") {
+                    return;
                 }
+                e.preventDefault();
+                $(this)
+                    .closest(".input-group")
+                    .find('button[type=submit]')
+                    .trigger("click");
             });
     });
