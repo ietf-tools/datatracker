621941217be52c22ab6affbbf56900a464837c24
Jennifer Richards <jennifer@painless-security.com>
2022-07-05 17:22:44 -0300

    feat: trap TERM to gracefully stop celery container

--- dev/celery/docker-init.sh
+++ dev/celery/docker-init.sh
@@ -9,4 +9,18 @@ if [[ -n "${UPDATE_REQUIREMENTS}" && -r requirements.txt ]]; then
   pip install --upgrade -r requirements.txt
 fi
 
-celery --app="${CELERY_APP:-ietf}" worker "$@"
+celery_pid=0
+cleanup () {
+  # Cleanly terminate the celery app by sending it a TERM, then waiting for it to exit.
+  if [[ "${celery_pid}" != 0 ]]; then
+    echo "Gracefully terminating celery worker. This may take a few minutes if tasks are in progress..."
+    kill -TERM "${celery_pid}"
+    wait "${celery_pid}"
+  fi
+}
+
+trap cleanup TERM
+# start celery in the background so we can trap the TERM signal
+celery --app="${CELERY_APP:-ietf}" worker "$@" &
+celery_pid=$!
+wait "${celery_pid}"
