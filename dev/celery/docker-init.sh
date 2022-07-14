a048fff4099b1347c7c449803bd7976df0028134
Jennifer Richards <jennifer@painless-security.com>
2022-07-14 18:04:14 -0300

    ci: pass filename to pip update in celery container

--- dev/celery/docker-init.sh
+++ dev/celery/docker-init.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-
+#
+# Environment parameters:
+#
+#   CELERY_APP - name of application to pass to celery (defaults to ietf)
+#
+#   UPDATES_REQUIREMENTS_FROM - path, relative to /workspace mount, to a pip requirements
+#       file that should be installed at container startup. Default is no package install/update.
+#
 WORKSPACEDIR="/workspace"
 
 cd "$WORKSPACEDIR" || exit 255
 
-if [[ -n "${UPDATE_REQUIREMENTS}" && -r requirements.txt ]]; then
-  echo "Updating requirements..."
-  pip install --upgrade -r requirements.txt
+if [[ -n "${UPDATE_REQUIREMENTS_FROM}" ]]; then
+  reqs_file="${WORKSPACEDIR}/${UPDATE_REQUIREMENTS_FROM}"
+  echo "Updating requirements from ${reqs_file}..."
+  pip install --upgrade -r "${reqs_file}"
 fi
 
 echo "Running initial checks..."
