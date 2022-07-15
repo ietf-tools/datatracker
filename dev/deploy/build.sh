adcca27a7453ce7a4f042b6bbdf7a1dcaa66e00b
Robert Sparks <rjsparks@nostrum.com>
2022-07-15 09:12:12 -0500

    ci: more portable sh/bash test construction

--- dev/deploy/build.sh
+++ dev/deploy/build.sh
@@ -3,7 +3,7 @@
 echo "Compiling native node packages..."
 yarn rebuild
 echo "Packaging static assets..."
-if [ "${SHOULD_DEPLOY}" == "true" ]; then
+if [ "${SHOULD_DEPLOY}" = "true" ]; then
     yarn build --base=https://www.ietf.org/lib/dt/$PKG_VERSION/
 else
     yarn build
