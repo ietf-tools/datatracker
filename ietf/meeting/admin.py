65448ac60a31bd1bb653e1e3fc49a89e63b0476e
Robert Sparks <rjsparks@nostrum.com>
2022-07-20 11:45:05 -0500

    fix: further tweaks to the attended admin

--- ietf/meeting/admin.py
+++ ietf/meeting/admin.py
@@ -208,6 +208,6 @@ admin.site.register(MeetingHost, MeetingHostAdmin)
 class AttendedAdmin(admin.ModelAdmin):
     model = Attended
     list_display= ["id", "person", "session"]
-    search_fields = ["person__name", "session__group__acronym", "session__meeting__number"]
+    search_fields = ["person__name", "session__group__acronym", "session__meeting__number", "session__name", "session__purpose__name"]
     raw_id_fields= ["person", "session"]
 admin.site.register(Attended, AttendedAdmin)
