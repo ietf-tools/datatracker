dc6b409d373e15cc3f1cef4d6947f8729336e0f5
Robert Sparks <rjsparks@nostrum.com>
2022-07-27 14:28:41 -0500

    fix: add new search rule to correct group community lists

--- ietf/community/migrations/0009_add_group_exp_rule_to_groups.py
+++ ietf/community/migrations/0009_add_group_exp_rule_to_groups.py
@@ -6,12 +6,13 @@ from django.db import migrations
 
 def forward(apps, schema_editor):
     SearchRule = apps.get_model('community', 'SearchRule')
+    CommunityList = apps.get_model('community', 'CommunityList')
     Group = apps.get_model('group', 'Group')
     State = apps.get_model('doc', 'State')
     for group in Group.objects.filter(type_id__in=['wg','rg'], state_id='active'):
-        Rule = SearchRule.objects.filter(group = group)
-        if Rule.exists():
-            SearchRule.objects.create(community_list=Rule[0].community_list, rule_type="group_exp", group=group, state=State.objects.get(slug="expired", type="draft"),)
+        com_list = CommunityList.objects.filter(group=group).first()
+        if com_list is not None:
+            SearchRule.objects.create(community_list=com_list, rule_type="group_exp", group=group, state=State.objects.get(slug="expired", type="draft"),)
 
 
 def reverse(apps, schema_editor):
