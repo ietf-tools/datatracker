e7949d0513df5d7a96a030dc4b8ddafbfe27e728
Jennifer Richards <jennifer@painless-security.com>
2022-08-05 14:01:15 -0300

    fix: clear password when CELERY_PASSWORD is empty
    Setting to an empty password is really not a good plan!

--- dev/mq/ietf-rabbitmq-server.bash
+++ dev/mq/ietf-rabbitmq-server.bash
@@ -9,9 +9,13 @@ export RABBITMQ_PID_FILE=/tmp/rabbitmq.pid
 update_celery_password () {
   rabbitmqctl wait "${RABBITMQ_PID_FILE}" --timeout 300
   rabbitmqctl await_startup --timeout 300
-  rabbitmqctl change_password datatracker <<END
+  if [[ -n "${CELERY_PASSWORD}" ]]; then
+    rabbitmqctl change_password datatracker <<END
 ${CELERY_PASSWORD}
 END
+  else
+    rabbitmqctl clear_password datatracker
+  fi
 }
 
 update_celery_password &
