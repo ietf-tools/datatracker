0c6462bd13fe1e580e5c4d9a7b30cb61bb41c7d6
NGPixel <github@ngpixel.com>
2022-08-09 16:21:06 -0400

    feat: diff docker tool - start app containers + execute prepare script

--- dev/diff/cli.js
+++ dev/diff/cli.js
@@ -14,7 +14,7 @@ import extract from 'extract-zip'
 import tar from 'tar'
 import { kebabCase } from 'lodash-es'
 
-const cliStatus = {}
+let cliStatus = null
 const config = {
   source: {
     path: process.cwd(),
@@ -74,35 +74,35 @@ async function downloadExtractZip ({ msg, url, ext = 'zip', branch }) {
   await fs.emptyDir(config.target.path)
   // Download zip
   try {
-    cliStatus.downloadZip = ora(msg).start()
+    cliStatus = ora(msg).start()
     const downloadBranchStream = got.stream(url)
     downloadBranchStream.on('downloadProgress', progress => {
-      cliStatus.downloadZip.text = `${msg} ${prettyBytes(progress.transferred)}`
+      cliStatus.text = `${msg} ${prettyBytes(progress.transferred)}`
     })
     await pipeline(
       downloadBranchStream,
       fs.createWriteStream(archivePath)
     )
-    cliStatus.downloadZip.succeed(`Downloaded ${ext} archive successfully.`)
+    cliStatus.succeed(`Downloaded ${ext} archive successfully.`)
   } catch (err) {
-    cliStatus.downloadZip.fail(`Failed to download ${ext} archive from GitHub.`)
+    cliStatus.fail(`Failed to download ${ext} archive from GitHub.`)
     console.error(chalk.redBright(err.message))
     process.exit(1)
   }
 
   // Extract zip
   try {
-    cliStatus.extractZip = ora(`Extracting ${ext} archive contents...`).start()
+    cliStatus = ora(`Extracting ${ext} archive contents...`).start()
     if (ext === 'zip') {
       const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'dt-'))
       await fs.ensureDir(tmpDir)
       await extract(archivePath, {
         dir: tmpDir,
         onEntry (entry) {
-          cliStatus.extractZip.text = `Extracting ${ext} archive contents... ${entry.fileName}`
+          cliStatus.text = `Extracting ${ext} archive contents... ${entry.fileName}`
         }
       })
-      cliStatus.extractZip.text = 'Moving extracted files to final location...'
+      cliStatus.text = 'Moving extracted files to final location...'
       await fs.move(path.join(tmpDir, kebabCase(`datatracker-${branch}`)), config.target.path, { overwrite: true })
       await fs.remove(tmpDir)
     } else if (ext === 'tgz') {
@@ -111,18 +111,39 @@ async function downloadExtractZip ({ msg, url, ext = 'zip', branch }) {
         file: archivePath,
         cwd: config.target.path,
         filter (path) {
-          cliStatus.extractZip.text = `Extracting ${ext} archive contents... ${path}`
+          cliStatus.text = `Extracting ${ext} archive contents... ${path}`
           return true
         }
       })
     }
-    cliStatus.extractZip.succeed(`Extracted ${ext} archive successfully.`)
+    cliStatus.succeed(`Extracted ${ext} archive successfully.`)
     await fs.remove(archivePath)
   } catch (err) {
-    cliStatus.extractZip.fail(`Failed to extract ${ext} archive contents.`)
+    cliStatus.fail(`Failed to extract ${ext} archive contents.`)
     console.error(chalk.redBright(err.message))
     process.exit(1)
   }
+}
+
+/**
+ * Run a command on a running container
+ * 
+ * @param {Docker.Container} container Docker container instance
+ * @param {Array<String>} cmd Command to execute
+ */
+async function executeCommand (container, cmd) {
+  await new Promise(async (resolve, reject) => {
+    const execChmod = await container.exec({
+      Cmd: cmd,
+      AttachStdout: true,
+      AttachStderr: true
+    })
+    const execChmodStream = await execChmod.start()
+    execChmodStream.on('close', () => {
+      resolve()
+    })
+    container.modem.demuxStream(execChmodStream, process.stdout, process.stderr)
+  })
 }
 
 async function main () {
@@ -131,22 +152,26 @@ async function main () {
   console.info('║ IETF DATATRACKER DIFF TOOL ║')
   console.info('╚════════════════════════════╝\n')
 
+  // ----------------------------
   // Connect to Docker Engine API
+  // ----------------------------
   let dock = null
   try {
-    cliStatus.dockerConnect = ora('Connecting to Docker Engine API...').start()
+    cliStatus = ora('Connecting to Docker Engine API...').start()
     dock = new Docker()
     await dock.ping()
-    cliStatus.dockerConnect.succeed('Connected to Docker Engine API.')
+    cliStatus.succeed('Connected to Docker Engine API.')
   } catch (err) {
-    cliStatus.dockerConnect.fail('Failed to connect to Docker Engine API!')
+    cliStatus.fail('Failed to connect to Docker Engine API!')
     console.error(chalk.redBright(err.message))
     return process.exit(1)
   }
 
+  // ---------------------------------------------------------------
   // Find base path so that it works from both / and /dev/diff paths
+  // ---------------------------------------------------------------
   try {
-    cliStatus.findBasePath = ora('Finding base datatracker instance base path...').start()
+    cliStatus = ora('Finding base datatracker instance base path...').start()
     let parentIdx = 0
     while(!(await fs.pathExists(path.join(config.source.path, 'requirements.txt')))) {
       config.source.path = path.resolve(config.source.path, '..')
@@ -155,21 +180,23 @@ async function main () {
         throw new Error('Start the CLI from a valid datatracker project path.')
       }
     }
-    cliStatus.findBasePath.info(`Using path ${config.source.path} for base datatracker instance. [SOURCE]`)
+    cliStatus.info(`Using path ${config.source.path} for base datatracker instance. [SOURCE]`)
   } catch (err) {
-    cliStatus.dockerConnect.fail('Could not find base path of the datatracker project!')
+    cliStatus.fail('Could not find base path of the datatracker project!')
     console.error(chalk.redBright(err.message))
     return process.exit(1)
   }
 
+  // --------------------------------------
   // Select comparison datatracker instance
+  // --------------------------------------
   const compareAgainstPrompt = await inquirer.prompt([
     {
       type: 'list',
       name: 'ans',
       message: 'What do you want to compare against?',
       choices: [
-        { value: 'local', name: 'Local path...' },
+        { value: 'local', name: 'Local folder path...' },
         { value: 'remote', name: 'Remote GitHub branch...' },
         { value: 'release', name: 'Latest release' },
       ]
@@ -187,15 +214,15 @@ async function main () {
       const branches = []
       let branch = 'main'
       try {
-        cliStatus.fetchBranches = ora('Fetching available remote branches...').start()
+        cliStatus = ora('Fetching available remote branches...').start()
         const branchesResp = await got('https://api.github.com/repos/ietf-tools/datatracker/branches').json()
         if (branchesResp?.length < 1) {
           throw new Error('No remote branches available.')
         }
         branches.push(...branchesResp.map(b => b.name))
-        cliStatus.fetchBranches.succeed(`Fetched ${branches.length} remote branches.`)
+        cliStatus.succeed(`Fetched ${branches.length} remote branches.`)
       } catch (err) {
-        cliStatus.fetchBranches.fail('Failed to fetch branches!')
+        cliStatus.fail('Failed to fetch branches!')
         console.error(chalk.redBright(err.message))
         return process.exit(1)
       }
@@ -244,9 +271,10 @@ async function main () {
   }
 
   try {
-
+    // ------------------
     // Pull latest images
-    cliStatus.fetchImages = ora('Pulling latest docker images... [0/2]').start()
+    // ------------------
+    cliStatus = ora('Pulling latest docker images... [0/2]').start()
     let fetchImagesIdx = 0
     await Promise.all([
       (async () => {
@@ -255,7 +283,7 @@ async function main () {
           dock.modem.followProgress(dbImagePullStream, (err, res) => err ? reject(err) : resolve(res))
         })
         fetchImagesIdx++
-        cliStatus.fetchImages.text = `Pulling latest docker images... [${fetchImagesIdx}/2]`
+        cliStatus.text = `Pulling latest docker images... [${fetchImagesIdx}/2]`
       })(),
       (async () => {
         const appImagePullStream = await dock.pull('ghcr.io/ietf-tools/datatracker-app-base:latest')
@@ -263,21 +291,25 @@ async function main () {
           dock.modem.followProgress(appImagePullStream, (err, res) => err ? reject(err) : resolve(res))
         })
         fetchImagesIdx++
-        cliStatus.fetchImages.text = `Pulling latest docker images... [${fetchImagesIdx}/2]`
+        cliStatus.text = `Pulling latest docker images... [${fetchImagesIdx}/2]`
       })()
     ])
-    cliStatus.fetchImages.succeed('Pulled latest docker images successfully. [2/2]')
+    cliStatus.succeed('Pulled latest docker images successfully. [2/2]')
 
+    // --------------
     // Create network
-    cliStatus.createNet = ora('Creating network...').start()
+    // --------------
+    cliStatus = ora('Creating docker network...').start()
     containers.net = await dock.createNetwork({
       Name: 'dt-diff-net',
       CheckDuplicate: true
     })
-    cliStatus.createNet.succeed('Network created successfully.')
+    cliStatus.succeed('Docker network created successfully.')
 
+    // ----------------------------
     // Create + Start DB containers
-    cliStatus.createDbs = ora('Creating DB docker containers... [0/2]').start()
+    // ----------------------------
+    cliStatus = ora('Creating DB docker containers... [0/2]').start()
     let createDbsIdx = 0
     await Promise.all([
       (async () => {
@@ -291,7 +323,7 @@ async function main () {
         })
         await containers.dbSource.start()
         createDbsIdx++
-        cliStatus.createDbs.text = `Creating DB docker containers... [${createDbsIdx}/2]`
+        cliStatus.text = `Creating DB docker containers... [${createDbsIdx}/2]`
       })(),
       (async () => {
         containers.dbTarget = await dock.createContainer({
@@ -304,29 +336,98 @@ async function main () {
         })
         await containers.dbTarget.start()
         createDbsIdx++
-        cliStatus.createDbs.text = `Creating DB docker containers... [${createDbsIdx}/2]`
+        cliStatus.text = `Creating DB docker containers... [${createDbsIdx}/2]`
       })()
     ])
-    cliStatus.createDbs.succeed('Created and started DB containers successfully. [2/2]')
+    cliStatus.succeed('Created and started DB containers successfully. [2/2]')
 
-    // Stop + Remove Containers
-    cliStatus.stopContainers = ora('Stopping containers...').start()
+    // -------------------------------------
+    // Create + Start Datatracker containers
+    // -------------------------------------
+    cliStatus = ora('Creating datatracker docker containers... [0/2]').start()
+    let createAppsIdx = 0
     await Promise.all([
-      containers.dbSource.stop(),
-      containers.dbTarget.stop()
+      (async () => {
+        containers.appSource = await dock.createContainer({
+          Image: 'ghcr.io/ietf-tools/datatracker-app-base:latest',
+          name: 'dt-diff-app-source',
+          Tty: true,
+          Hostname: 'appsource',
+          HostConfig: {
+            Binds: [
+              `${config.source.path}:/workspace`
+            ],
+            NetworkMode: 'dt-diff-net'
+          }
+        })
+        await containers.appSource.start()
+        createAppsIdx++
+        cliStatus.text = `Creating datatracker docker containers... [${createAppsIdx}/2]`
+      })(),
+      (async () => {
+        containers.appTarget = await dock.createContainer({
+          Image: 'ghcr.io/ietf-tools/datatracker-app-base:latest',
+          name: 'dt-diff-app-target',
+          Tty: true,
+          Hostname: 'apptarget',
+          HostConfig: {
+            Binds: [
+              `${config.target.path}:/workspace`
+            ],
+            NetworkMode: 'dt-diff-net'
+          }
+        })
+        await containers.appTarget.start()
+        createAppsIdx++
+        cliStatus.text = `Creating datatracker docker containers... [${createAppsIdx}/2]`
+      })()
     ])
-    cliStatus.stopContainers.text = 'Removing containers...'
+    cliStatus.succeed('Created and started datatracker containers successfully. [2/2]')
+
+    // ------------------------
+    // Run
+    // ------------------------
     await Promise.all([
-      containers.dbSource.remove(),
-      containers.dbTarget.remove()
+      async () => {
+        await executeCommand (containers.appSource, ['bash', '-c', 'chmod +x ./dev/diff/prepare.sh'])
+        await executeCommand (containers.appSource, ['bash', './dev/diff/prepare.sh'])
+      },
+      async () => {
+        await executeCommand (containers.appTarget, ['bash', '-c', 'chmod +x ./dev/diff/prepare.sh'])
+        await executeCommand (containers.appTarget, ['bash', './dev/diff/prepare.sh'])
+      },
     ])
-    cliStatus.stopContainers.text = 'Removing network...'
-    containers.net.remove()
-    cliStatus.stopContainers.succeed('Removed docker resources.')
   } catch (err) {
+    cliStatus.fail()
     console.error(chalk.redBright(err.message))
-    return process.exit(1)
   }
+
+  // ------------------------
+  // Stop + Remove Containers
+  // ------------------------
+  cliStatus = ora('Stopping containers...').start()
+  try {
+    await Promise.allSettled([
+      containers.dbSource && containers.dbSource.stop(),
+      containers.dbTarget && containers.dbTarget.stop(),
+      containers.appSource && containers.appSource.stop(),
+      containers.appTarget && containers.appTarget.stop()
+    ])
+  } catch (err) { }
+  cliStatus.text = 'Removing resources...'
+  try {
+    await Promise.allSettled([
+      containers.dbSource && containers.dbSource.remove(),
+      containers.dbTarget && containers.dbTarget.remove(),
+      containers.appSource && containers.appSource.remove(),
+      containers.appTarget && containers.appTarget.remove()
+    ])
+  } catch (err) { }
+  cliStatus.text = 'Removing network...'
+  try {
+    await containers.net.remove()
+  } catch (err) {}
+  cliStatus.succeed('Removed docker resources.')
 }
 
 main()
