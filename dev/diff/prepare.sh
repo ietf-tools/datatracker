822a5725891314a08493ab6f688ef34d8424f29f
Nicolas Giard <github@ngpixel.com>
2022-08-18 13:28:47 -0400

    feat: diff crawl docker tool (#4310)
    * feat: diff docker tool (wip)
    
    * feat: diff docker tool - download + extract from github
    
    * feat: diff docker tool - pull + create + shutdown containers
    
    * feat: diff docker tool - start app containers + execute prepare script
    
    * fix: many fixes + run target instance server
    
    * feat: diff docker tool - run crawl + options
    
    * fix: diff docker tool - various fixes
    
    * feat: diff docker tool - add tag + commit fetch options
    
    * feat: diff docker tool - handle log save + more options
    
    * feat: diff docker tool - F10 quit option + better exec display + checklist fixes
    
    * fix: diff docker tool - remove latest release option

---
+++ dev/diff/prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Fixing permissions..."
+chmod -R 777 ./
+echo "Ensure all requirements.txt packages are installed..."
+pip --disable-pip-version-check --no-cache-dir install -r requirements.txt
+echo "Compiling native node packages..."
+yarn rebuild
+echo "Building static assets..."
+yarn build
+yarn legacy:build
+echo "Creating data directories..."
+chmod +x ./docker/scripts/app-create-dirs.sh
+./docker/scripts/app-create-dirs.sh
