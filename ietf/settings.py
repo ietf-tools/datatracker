5c8545eecbe4f7d2084b7a5c93bb449f63d56675
Robert Sparks <rjsparks@nostrum.com>
2022-09-20 12:09:11 -0500

    feat: use hmac, sha256, and a better secret for published nomcom hashes. (#4475)
    * feat: use hmac, sha256, and a better secret for published nomcom hashes.
    
    * fix: avoid using django secret key for a different secret context
    
    * fix: Only strip the newline
    
    * fix: improve readability by using rstrip

--- ietf/settings.py
+++ ietf/settings.py
@@ -1272,6 +1272,8 @@ if SERVER_MODE != 'production':
 
     if 'SECRET_KEY' not in locals():
         SECRET_KEY = 'PDwXboUq!=hPjnrtG2=ge#N$Dwy+wn@uivrugwpic8mxyPfHka'
+    if 'NOMCOM_APP_SECRET' not in locals():
+        NOMCOM_APP_SECRET = b'\x9b\xdas1\xec\xd5\xa0SI~\xcb\xd4\xf5t\x99\xc4i\xd7\x9f\x0b\xa9\xe8\xfeY\x80$\x1e\x12tN:\x84'
 
     ALLOWED_HOSTS = ['*',]
     
