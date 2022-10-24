2d39d727c21050fe43d221039cb725367ba2df46
Jennifer Richards <jennifer@painless-security.com>
2022-10-24 18:56:58 -0300

    test: avoid local times near midnight for test_past_swap_days_buttons()

--- ietf/utils/timezone.py
+++ ietf/utils/timezone.py
@@ -1,8 +1,11 @@
 import pytz
 import email.utils
 import datetime
+import random
 
 from django.conf import settings
+from django.utils import timezone
+
 
 def local_timezone_to_utc(d):
     """Takes a naive datetime in the local timezone and returns a
@@ -37,3 +40,16 @@ def email_time_to_local_timezone(date_string):
 def date2datetime(date, tz=pytz.utc):
     return datetime.datetime(*(date.timetuple()[:6]), tzinfo=tz)
     
+
+def timezone_not_near_midnight():
+    """Get the name of a random timezone where it's not close to midnight
+
+    Avoids midnight +/- 1 hour.
+    """
+    timezone_options = pytz.common_timezones
+    tzname = random.choice(timezone_options)
+    right_now = timezone.now().astimezone(pytz.timezone(tzname))
+    while right_now.hour < 1 or right_now.hour >= 23:
+        tzname = random.choice(pytz.common_timezones)
+        right_now = right_now.astimezone(pytz.timezone(tzname))
+    return tzname
