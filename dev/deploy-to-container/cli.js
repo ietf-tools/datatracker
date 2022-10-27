417bf3da1c24a8e1f9bb543382715c6620d4638c
NGPixel <github@ngpixel.com>
2022-10-26 23:13:09 -0400

    ci: deploy to sandbox - fix settings replace

--- dev/deploy-to-container/cli.js
+++ dev/deploy-to-container/cli.js
@@ -65,8 +65,9 @@ async function main () {
   await fs.outputFile(settingsPath,
     cfgRaw
       .replace('__DBHOST__', `dt-db-${branch}`)
-      .replace('__SECRETKEY__', nanoid(36)))
+      .replace('__SECRETKEY__', nanoid(36))
       .replace('__MQCONNSTR__', `amqp://datatracker:${mqKey}@dt-mq-${branch}/dt`)
+  )
   await fs.copy(path.join(basePath, 'docker/scripts/app-create-dirs.sh'), path.join(releasePath, 'app-create-dirs.sh'))
   await fs.copy(path.join(basePath, 'dev/deploy-to-container/start.sh'), path.join(releasePath, 'start.sh'))
   await fs.copy(path.join(basePath, 'test/data'), path.join(releasePath, 'test/data'))
