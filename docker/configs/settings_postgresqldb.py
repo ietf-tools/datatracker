27fccc6ba05cec8d581b0d7afe3a1d7a982916e3
Robert Sparks <rjsparks@nostrum.com>
2022-11-11 11:01:01 +0000

    feat: move to postgresql (#4744)
    * feat: move to postgresql
    
    * fix: repair fractional replace statement
    
    * fix: use pathlib to manipulate settings_local
    
    Co-authored-by: Jennifer Richards <jennifer@painless-security.com>
    
    * fix: do two string replacements, not one followed by another that throws away the first.
    
    Co-authored-by: Jennifer Richards <jennifer@painless-security.com>
    
    * fix: use pathlib again to manipulate settings_local
    
    Co-authored-by: Jennifer Richards <jennifer@painless-security.com>
    
    * fix: properly use assert (1/2)
    
    Co-authored-by: Jennifer Richards <jennifer@painless-security.com>
    
    * fix: properly use assert (2/2)
    
    Co-authored-by: Jennifer Richards <jennifer@painless-security.com>
    
    Co-authored-by: Jennifer Richards <jennifer@painless-security.com>

---
+++ docker/configs/settings_postgresqldb.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'HOST': 'pgdb',
+        'PORT': 5432,
+        'NAME': 'ietf',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'django',
+        'PASSWORD': 'RkTkDPFnKpko',
+    },
+}
