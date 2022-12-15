94d367000b82650e36ad2d14066bcb6a0575887e
Nicolas Giard <github@ngpixel.com>
2022-12-15 15:43:25 -0500

    ci: add search path modif to db dump import

--- docker/scripts/db-pg-import.sh
+++ docker/scripts/db-pg-import.sh
@@ -4,3 +4,4 @@ set -e
 dropdb -U django --if-exists ietf
 createdb -U django ietf
 pg_restore -U django -d ietf ietf.dump
+psql -U django -d ietf -v ON_ERROR_STOP=1 -c '\x' -c 'ALTER USER django set search_path=ietf_utf8,django,public;'
