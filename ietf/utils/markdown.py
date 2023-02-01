8d6f88b67cbbd5672d6d6a6ac5733c9c6871562f
Lars Eggert <lars@eggert.org>
2023-02-01 20:45:03 +0200

    fix: Need to linkify during Markdown rendering

--- ietf/utils/markdown.py
+++ ietf/utils/markdown.py
@@ -10,17 +10,41 @@ import markdown as python_markdown
 from django.utils.safestring import mark_safe
 
 from ietf.doc.templatetags.ietf_filters import urlize_ietf_docs
-from ietf.utils.text import bleach_cleaner, bleach_linker
+from ietf.utils.text import (
+    bleach_cleaner,
+    check_url_validity,
+    bleach,
+    tlds_sorted,
+    protocols,
+)
+from mdx_linkify.mdx_linkify import LinkifyExtension  # type: ignore
 
 
 def markdown(text):
     return mark_safe(
-        bleach_linker.linkify(
-            urlize_ietf_docs(
-                bleach_cleaner.clean(
-                    python_markdown.markdown(
-                        text, extensions=["extra", "nl2br", "sane_lists", "toc"]
-                    )
+        urlize_ietf_docs(
+            bleach_cleaner.clean(
+                python_markdown.markdown(
+                    text,
+                    extensions=[
+                        "extra",
+                        "nl2br",
+                        "sane_lists",
+                        "toc",
+                        LinkifyExtension(
+                            # keep these in sync with the bleach_linker initialization
+                            linker_options={
+                                "callbacks": [check_url_validity],
+                                "url_re": bleach.linkifier.build_url_re(
+                                    tlds=tlds_sorted, protocols=protocols
+                                ),
+                                "email_re": bleach.linkifier.build_email_re(
+                                    tlds=tlds_sorted
+                                ),
+                                "parse_email": True,
+                            }
+                        ),
+                    ],
                 )
             )
         )
