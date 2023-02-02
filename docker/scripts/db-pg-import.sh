117bb2a5b8ca18086ec8626e6a83f35ed9627dc5
Robert Sparks <rjsparks@nostrum.com>
2023-02-02 13:19:37 -0600

    ci: clean up after recent exploration.

--- docker/scripts/db-pg-import.sh
+++ docker/scripts/db-pg-import.sh
@@ -4,19 +4,8 @@ set -e
 echo "Drop dummy ietf DB if it exists..."
 dropdb -U django --if-exists ietf
 
-#echo "Create new ietf DB..."
-#createdb -U django ietf
-#
-#echo "Enable citext extension..."
-#psql -U django -d ietf -v ON_ERROR_STOP=1 -c '\x' -c 'CREATE EXTENSION IF NOT EXISTS citext;'
-#
-#echo "Set schema search path for user django..."
-#psql -U django -d ietf -v ON_ERROR_STOP=1 -c '\x' -c 'ALTER USER django set search_path=ietf_utf8,django,public;'
-
+# Extensions and search paths will be loaded from the dump
 echo "Import DB dump into ietf..."
 pg_restore --clean --if-exists --create -U django -d postgres ietf.dump
-
-#echo "Set schema search path for user django..."
-#psql -U django -d ietf -v ON_ERROR_STOP=1 -c '\x' -c 'ALTER USER django set search_path=ietf_utf8,django,public;'
 
 echo "Done!"
