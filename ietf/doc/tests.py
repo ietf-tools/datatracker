6a6a3db3b978f052a4fa47a89a5c296173836312
Lars Eggert <lars@eggert.org>
2023-02-10 22:33:54 +0200

    fix: Don't try to further linkify rfc2html output (#5112)
    * fix: Don't try to further linkify rfc2html output
    
    Fixes #5111
    
    * Fix test

--- ietf/doc/tests.py
+++ ietf/doc/tests.py
@@ -740,7 +740,7 @@ Man                    Expires September 22, 2015               [Page 3]
         self.assertEqual(q('title').text(), 'draft-ietf-mars-test-01')
         self.assertEqual(len(q('.rfcmarkup pre')), 3)
         self.assertEqual(len(q('.rfcmarkup span.h1, .rfcmarkup h1')), 2)
-        self.assertEqual(len(q('.rfcmarkup a[href]')), 28)
+        self.assertEqual(len(q('.rfcmarkup a[href]')), 27)
 
         r = self.client.get(urlreverse("ietf.doc.views_doc.document_html", kwargs=dict(name=draft.name, rev=draft.rev)))
         self.assertEqual(r.status_code, 200)
