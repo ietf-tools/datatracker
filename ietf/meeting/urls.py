219dbb5e24c578bdd1a0af544121654088ba159d
Jennifer Richards <jennifer@painless-security.com>
2023-02-27 17:41:49 -0400

    feat: Dispatch agenda-utc.txt URL

--- ietf/meeting/urls.py
+++ ietf/meeting/urls.py
@@ -69,6 +69,7 @@ type_interim_patterns = [
 
 type_ietf_only_patterns_id_optional = [
     url(r'^agenda(?P<utc>-utc)?(?P<ext>\.html)?/?$', views.agenda, name='agenda'),
+    url(r'^agenda(?P<utc>-utc)(?P<ext>\.txt)$', views.agenda_plain),
     url(r'^agenda(?P<ext>\.txt)$', views.agenda_plain),
     url(r'^agenda(?P<ext>\.csv)$', views.agenda_plain),
     url(r'^agenda/edit$',
