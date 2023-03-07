7470a4907b67742db61f5e3ed62ead63608e657d
Jennifer Richards <jennifer@painless-security.com>
2023-03-07 16:56:25 -0400

    feat: Add has_onsite_flag to Session

--- ietf/meeting/models.py
+++ ietf/meeting/models.py
@@ -1071,6 +1071,7 @@ class Session(models.Model):
     modified = models.DateTimeField(auto_now=True)
     remote_instructions = models.CharField(blank=True,max_length=1024)
     on_agenda = models.BooleanField(default=True, help_text='Is this session visible on the meeting agenda?')
+    has_onsite_tool = models.BooleanField(default=False, help_text="Does this session use the officially supported onsite and remote tooling?")
 
     tombstone_for = models.ForeignKey('Session', blank=True, null=True, help_text="This session is the tombstone for a session that was rescheduled", on_delete=models.CASCADE)
 
