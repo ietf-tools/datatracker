5bb776017d9a051a40ae62b55918708f0a8224bb
Lars Eggert <lars@eggert.org>
2023-03-09 15:26:19 +0200

    Attempt to address further review comments

--- ietf/ietfauth/views.py
+++ ietf/ietfauth/views.py
@@ -126,8 +126,12 @@ def create_account(request):
             user = User.objects.filter(username__iexact=new_account_email)
             email = Email.objects.filter(address__iexact=new_account_email)
             if user.exists() or email.exists():
-                to_email = user.first().person.email_address() if user else new_account_email
-                send_account_creation_exists_email(request, new_account_email, to_email)
+                person_to_contact = user.first().person if user else email.first().person
+                to_email = person_to_contact.email_address()
+                if to_email:
+                    send_account_creation_exists_email(request, new_account_email, to_email)
+                else:
+                    raise ValidationError(f"Account for {{new_account_email}} exists, but cannot email it")
             else:
                 # For the IETF 113 Registration period (at least) we are lowering the
                 # barriers for account creation to the simple email round-trip check
