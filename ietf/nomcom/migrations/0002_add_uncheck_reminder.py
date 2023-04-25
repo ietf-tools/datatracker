5f36905515c81d19f7afd2710304f5f0226da6ce
Robert Sparks <rjsparks@nostrum.com>
2023-04-25 11:59:41 -0500

    fix: reorder new nomcom migration (#5534)

--- ietf/nomcom/migrations/0002_add_uncheck_reminder.py
+++ ietf/nomcom/migrations/0002_add_uncheck_reminder.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('nomcom', '0013_update_accepting_volunteers_helptext'),
+        ('nomcom', '0001_initial'),
     ]
 
     operations = [
