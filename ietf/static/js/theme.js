342e6f58bd825fcce25d64bbbd293189245070d0
Lars Eggert <lars@eggert.org>
2023-05-26 16:16:00 +0200

    Color mode fixes

--- ietf/static/js/theme.js
+++ ietf/static/js/theme.js
@@ -38,15 +38,16 @@
     if (!themeSwitcher) {
       return;
     }
-
-    const themeSwitcherText = document.querySelector("#bd-theme-text");
-    const activeThemeIcon = document.querySelector(".theme-icon-active use");
+    // Commented-out lines are from the original bs5 js, which uses a more complicated pref dropdown.
+    // Kept them here for easier future diffing.
+    // const themeSwitcherText = document.querySelector("#bd-theme-text");
+    // const activeThemeIcon = document.querySelector(".theme-icon-active use");
     const btnToActive = document.querySelector(
       `[data-bs-theme-value="${theme}"]`
     );
-    const svgOfActiveBtn = btnToActive
-      .querySelector("svg use")
-      .getAttribute("href");
+    // const svgOfActiveBtn = btnToActive
+    //   .querySelector("svg use")
+    //   .getAttribute("href");
 
     document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
       element.classList.remove("active");
@@ -55,13 +56,13 @@
 
     btnToActive.classList.add("active");
     btnToActive.setAttribute("aria-pressed", "true");
-    activeThemeIcon.setAttribute("href", svgOfActiveBtn);
-    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
-    themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
+    // activeThemeIcon.setAttribute("href", svgOfActiveBtn);
+    // const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+    // themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
 
-    if (focus) {
-      themeSwitcher.focus();
-    }
+    // if (focus) {
+    //   themeSwitcher.focus();
+    // }
   };
 
   window
