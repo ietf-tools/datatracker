9c17db6c39649235cf1c8f3e7be25e6ab410a998
Jennifer Richards <jennifer@staff.ietf.org>
2023-05-26 13:11:06 -0300

    chore: Renumber migration

--- ietf/review/migrations/0002_reviewteamsettings_allow_reviewer_to_reject_after_deadline.py
+++ ietf/review/migrations/0002_reviewteamsettings_allow_reviewer_to_reject_after_deadline.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('review', '0031_id_term'),
+        ('review', '0001_initial'),
     ]
 
     operations = [
