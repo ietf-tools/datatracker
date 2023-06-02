a6d33dc76e63404da9d06082027a02ad6c0d29ea
Robert Sparks <rjsparks@nostrum.com>
2023-06-02 13:28:03 -0500

    fix: fetch kesara's repo and move files into place.

--- ietf/meeting/management/commands/import_iab_minutes.py
+++ ietf/meeting/management/commands/import_iab_minutes.py
@@ -2,10 +2,13 @@
 
 import datetime
 import json
-import zoneinfo
+import os
+import shutil
+import subprocess
+import tempfile
 
-from pathlib import PurePath
-
+from pathlib import PurePath, Path
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from ietf.doc.models import Document, DocAlias
@@ -20,6 +23,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        tmpdir = tempfile.mkdtemp()
+        process = subprocess.Popen(["git","clone","https://github.com/kesara/iab-scraper.git",tmpdir],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if not Path(tmpdir).joinpath("iab_minutes","2022-12-14.md").exists():
+            print("Git clone of the iab-scraper directory did not go as expected")
+            print("stdout:", stdout)
+            print("stderr:", stderr)
+            print(f"Clean up {tmpdir} manually")
+            exit(-1)
+        
+
         ntby = nametimes_by_year()
         for year in ntby:
             counter = 1
@@ -27,7 +41,7 @@ class Command(BaseCommand):
                 start = datetime.datetime(int(year), month, day, start_hour, start_minute, tzinfo=datetime.timezone.utc)
                 meeting_name = f"interim-{year}-iab-{counter:02d}"
                 minutes_docname = f"minutes-interim-{year}-iab-{counter:02d}-{start:%Y%m%d}" # Note violating the convention of having the start time...
-                minutes_filename = f"{minutes_docname}-00.{ext}"  # I plan to use a management command to put the files in place after the migration is run.
+                minutes_filename = f"{minutes_docname}-00.{ext}"
                 # Create Document
                 doc = Document.objects.create(
                     name = minutes_docname,
@@ -41,7 +55,7 @@ class Command(BaseCommand):
                 # Create Meeting - Add a note about noon utc fake meeting times
                 meeting = Meeting.objects.create(
                     number=meeting_name,
-                    type_id='interim',
+                    type_id="interim",
                     date=start.date(),
                     days=1,
                     time_zone=start.tzname())
@@ -81,4 +95,15 @@ class Command(BaseCommand):
                 # Add Document to Session
                 session.sessionpresentation_set.create(document=doc,rev=doc.rev)
 
+                # Put file in place
+                source = Path(tmpdir).joinpath("iab_minutes",f"{year}-{month:02d}-{day:02d}.{ext}")
+                dest = Path(settings.AGENDA_PATH).joinpath(meeting_name, "minutes", minutes_filename)
+                if dest.exists():
+                    print(f"WARNING: {dest} already exists - not overwriting it.")
+                else:
+                    os.makedirs(dest.parent, exist_ok=True)
+                    shutil.copy(source, dest)
+
                 counter += 1
+
+        shutil.rmtree(tmpdir)
