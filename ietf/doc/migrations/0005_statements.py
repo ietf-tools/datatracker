97220a50da7efbcd090a6833da1fb981904e3ad9
Robert Sparks <rjsparks@nostrum.com>
2023-06-22 12:53:35 -0500

    feat: infrastructure for statements doctype

---
+++ ietf/doc/migrations/0005_statements.py
@@ -0,0 +1,30 @@
+# Copyright The IETF Trust 2023, All Rights Reserved
+
+from django.db import migrations
+
+def forward(apps, schema_editor):
+    StateType = apps.get_model("doc", "StateType")
+    State = apps.get_model("doc", "State")
+
+    StateType.objects.create(slug="statement", label="Statement State")
+    State.objects.create(slug="active", type_id="statement", name="Active", order=0, desc="The statement is active")
+    State.objects.create(slug="replaced", type_id="statement", name="Active", order=0, desc="The statement has been replaced")
+
+
+def reverse(apps, schema_editor):
+    StateType = apps.get_model("doc", "StateType")
+    State = apps.get_model("doc", "State")
+
+    State.objects.filter(type_id="statement").delete()
+    StateType.objects.filter(slug="statement").delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("doc", "0004_alter_dochistory_ad_alter_dochistory_shepherd_and_more"),
+        ("name", "0004_statements"),
+    ]
+
+    operations = [
+        migrations.RunPython(forward, reverse),
+    ]
