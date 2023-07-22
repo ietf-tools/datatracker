e69a3bb4d49859f179e4ba4aecf54d303879a115
Jim Fenton <fenton@bluepopcorn.net>
2023-07-22 15:57:44 -0700

    Intermediate checkpoint

--- ietf/doc/views_help.py
+++ ietf/doc/views_help.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 from ietf.doc.models import State, StateType, IESG_SUBSTATE_TAGS
-from ietf.name.models import DocRelationshipName,  DocTagName
+from ietf.name.models import DocRelationshipName,  DocTagName, StreamName
 from ietf.doc.utils import get_tags_for_stream_id
 
 def state_help(request, type):
@@ -57,3 +57,15 @@ def relationship_help(request,subset=None):
     if subset:
        rels = rels.filter(slug__in=subsets[subset]) 
     return render(request, "doc/relationship_help.html", { "relations": rels } )
+
+def state(request, doc, type=None):
+    if type:
+        streams = [ s.slug for s in StreamName.objects.all() ]
+        if type in streams:
+            type = "stream-%s" % type
+    slug = "%s-%s" % (doc,type) if type else doc
+    statetype = get_object_or_404(StateType, slug=slug)
+    states = State.objects.filter(used=True, type=statetype).order_by('order')
+    return render(request, 'help/states.html', {"doc": doc, "type": statetype, "states":states} )
+
+
