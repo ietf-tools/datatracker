ietf-tools/datatracker

commit 842e7305e5d1bace775b3b41d3c9fa0b605efa02

refactor: When invoking reclassification from a view-feedback page, load the corresponding reclassify-feedback page

ietf/nomcom/views.py
@@ -770,6 +770,11 @@ def process_nomination_status(request, year, nominee_position_id, state, date, h
 @role_required("Nomcom Chair", "Nomcom Advisor")
 @nomcom_private_key_required
 def reclassify_feedback(request, year):
+    referer = request.META.get('HTTP_REFERER', None)
+    if 'nominee' in referer or 'topic' in referer or 'unrelated' in referer:
+        url = referer.replace('view', 'reclassify')
+        return HttpResponseRedirect(url)
+
     return view_or_reclassify_feedback(request, year, reclassify=True)
 
 @role_required("Nomcom")

commit 3ef8a6996c61a55720f98278364ce71c815054b6

fix: 842e730 broke the Back button

ietf/nomcom/views.py
@@ -771,7 +771,7 @@ def process_nomination_status(request, year, nominee_position_id, state, date, h
 @nomcom_private_key_required
 def reclassify_feedback(request, year):
     referer = request.META.get('HTTP_REFERER', None)
-    if 'nominee' in referer or 'topic' in referer or 'unrelated' in referer:
+    if 'view' in referer and ('nominee' in referer or 'topic' in referer or 'unrelated' in referer):
         url = referer.replace('view', 'reclassify')
         return HttpResponseRedirect(url)
 