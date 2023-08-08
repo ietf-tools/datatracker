06c9f06d5504f0fd75dca1609fa1960e7b286444
Paul Selkirk <paul@painless-security.com>
2023-08-08 13:33:17 -0400

    feat: Reclassify nomcom feedback (#6002)
    * fix: Clean up view_feedback_pending
    
    - Remove "Unclassified" column header, which caused misalignment in the table body.
    
    - Show the message author - previously displayed as `(None)`.
    
    * feat: Reclassify nomcom feedback (#4669)
    
    - There's a new `Chair/Advisor Tasks` menu item `Reclassify feedback`.
    
    - I overloaded `view_feedback*` URLs with a `?reclassify` parameter.
    
    - This adds a checkbox to each feedback message, and a `Reclassify` button
    at the bottom of each feedback page.
    
    - "Reclassifying" basically de-classifies the feedback, and punts it back
    to the "Pending emails" view for reclassification.
    
    - If a feedback has been applied to multiple nominees, declassifying it
    from one nominee removes it from all.
    
    * fix: Remove unused local variables
    
    * fix: Fix some missing and mis-nested html
    
    * test: Add tests for reclassifying feedback
    
    * refactor: Substantial redesign of feedback reclassification
    
    - Break out reclassify_feedback* as their own URLs and views,
      and revert changes to view_feedback*.html.
    
    - Replace checkboxes with a Reclassify button on each message.
    
    * fix: Remember to clear the feedback associations when reclassifying
    
    * feat: Add an 'Overcome by events' feedback type
    
    * refactor: When invoking reclassification from a view-feedback page, load the corresponding reclassify-feedback page
    
    * fix: De-conflict migration with 0004_statements
    
    Also change the coding style to match, and add a reverse migration.
    
    * fix: Fix a test case to account for new feedback type
    
    * fix: 842e730 broke the Back button
    
    * refactor: Reclassify feedback directly instead of putting it back in the work queue
    
    * fix: Adjust tests to new workflow
    
    * refactor: Further refine reclassification to avoid redirects
    
    * refactor: Impose a FeedbackTypeName ordering
    
    Also add FeedbackTypeName.legend field, rather than synthesizing it every
    time we classify or reclassify feedback.
    
    In the reclassification forms, only show the relevant feedback types.
    
    * refactor: Merge reclassify_feedback_* back into view_feedback_*
    
    This means the "Reclassify" button is always present, but eliminates some
    complexity.
    
    * refactor: Add filter(used=True) on FeedbackTypeName querysets
    
    * refactor: Add the new FeedbackTypeName to the reclassification success message
    
    * fix: Secure reclassification against rogue nomcom members

--- ietf/nomcom/tests.py
+++ ietf/nomcom/tests.py
@@ -1450,7 +1450,7 @@ class FeedbackIndexTests(TestCase):
         self.assertEqual(response.status_code,200)
         q = PyQuery(response.content)
         r = q('tfoot').eq(0).find('td').contents()
-        self.assertEqual([a.strip() for a in r], ['1', '1', '1'])
+        self.assertEqual([a.strip() for a in r], ['1', '1', '1', '0'])
 
 class FeedbackLastSeenTests(TestCase):
 
@@ -2863,3 +2863,92 @@ class VolunteerDecoratorUnitTests(TestCase):
                 self.assertEqual(v.qualifications,'path_2')
             if v.person == author_person:
                 self.assertEqual(v.qualifications,'path_3')
+
+class ReclassifyFeedbackTests(TestCase):
+    """Tests for feedback reclassification"""
+
+    def setUp(self):
+        super().setUp()
+        setup_test_public_keys_dir(self)
+        nomcom_test_data()
+        self.nc = NomComFactory.create(**nomcom_kwargs_for_year())
+        self.chair = self.nc.group.role_set.filter(name='chair').first().person
+        self.member = self.nc.group.role_set.filter(name='member').first().person
+        self.nominee = self.nc.nominee_set.order_by('pk').first()
+        self.position = self.nc.position_set.first()
+        self.topic = self.nc.topic_set.first()
+
+    def tearDown(self):
+        teardown_test_public_keys_dir(self)
+        super().tearDown()
+
+    def test_reclassify_feedback_nominee(self):
+        fb = FeedbackFactory.create(nomcom=self.nc,type_id='comment')
+        fb.positions.add(self.position)
+        fb.nominees.add(self.nominee)
+        fb.save()
+        self.assertEqual(Feedback.objects.comments().count(), 1)
+
+        url = reverse('ietf.nomcom.views.view_feedback_nominee', kwargs={'year':self.nc.year(), 'nominee_id':self.nominee.id})
+        login_testing_unauthorized(self,self.member.user.username,url)
+        provide_private_key_to_test_client(self)
+        response = self.client.post(url, {'feedback_id': fb.id, 'type': 'obe'})
+        self.assertEqual(response.status_code, 403)
+
+        self.client.logout()
+        self.client.login(username=self.chair.user.username, password=self.chair.user.username + "+password")
+        provide_private_key_to_test_client(self)
+
+        response = self.client.post(url, {'feedback_id': fb.id, 'type': 'obe'})
+        self.assertEqual(response.status_code, 200)
+
+        fb = Feedback.objects.get(id=fb.id)
+        self.assertEqual(fb.type_id,'obe')
+        self.assertEqual(Feedback.objects.comments().count(), 0)
+        self.assertEqual(Feedback.objects.filter(type='obe').count(), 1)
+
+    def test_reclassify_feedback_topic(self):
+        fb = FeedbackFactory.create(nomcom=self.nc,type_id='comment')
+        fb.topics.add(self.topic)
+        fb.save()
+        self.assertEqual(Feedback.objects.comments().count(), 1)
+
+        url = reverse('ietf.nomcom.views.view_feedback_topic', kwargs={'year':self.nc.year(), 'topic_id':self.topic.id})
+        login_testing_unauthorized(self,self.member.user.username,url)
+        provide_private_key_to_test_client(self)
+        response = self.client.post(url, {'feedback_id': fb.id, 'type': 'unclassified'})
+        self.assertEqual(response.status_code, 403)
+
+        self.client.logout()
+        self.client.login(username=self.chair.user.username, password=self.chair.user.username + "+password")
+        provide_private_key_to_test_client(self)
+
+        response = self.client.post(url, {'feedback_id': fb.id, 'type': 'unclassified'})
+        self.assertEqual(response.status_code, 200)
+
+        fb = Feedback.objects.get(id=fb.id)
+        self.assertEqual(fb.type_id,None)
+        self.assertEqual(Feedback.objects.comments().count(), 0)
+        self.assertEqual(Feedback.objects.filter(type=None).count(), 1)
+
+    def test_reclassify_feedback_unrelated(self):
+        fb = FeedbackFactory(nomcom=self.nc, type_id='read')
+        self.assertEqual(Feedback.objects.filter(type='read').count(), 1)
+
+        url = reverse('ietf.nomcom.views.view_feedback_unrelated', kwargs={'year':self.nc.year()})
+        login_testing_unauthorized(self,self.member.user.username,url)
+        provide_private_key_to_test_client(self)
+        response = self.client.post(url, {'feedback_id': fb.id, 'type': 'junk'})
+        self.assertEqual(response.status_code, 403)
+
+        self.client.logout()
+        self.client.login(username=self.chair.user.username, password=self.chair.user.username + "+password")
+        provide_private_key_to_test_client(self)
+
+        response = self.client.post(url, {'feedback_id': fb.id, 'type': 'junk'})
+        self.assertEqual(response.status_code, 200)
+
+        fb = Feedback.objects.get(id=fb.id)
+        self.assertEqual(fb.type_id, 'junk')
+        self.assertEqual(Feedback.objects.filter(type='read').count(), 0)
+        self.assertEqual(Feedback.objects.filter(type='junk').count(), 1)
