73137364cc6ca822120c70aad731f33ad232c7e8
Robert Sparks <rjsparks@nostrum.com>
2023-08-11 11:21:38 -0500

    chore: rename test settings and shorten runserver instructions (#6131)

--- docker/scripts/app-init.sh
+++ docker/scripts/app-init.sh
@@ -106,11 +106,11 @@ if [ -z "$EDITOR_VSCODE" ]; then
         echo
         echo "You can execute arbitrary commands now, e.g.,"
         echo
-        echo "    ietf/manage.py runserver 0.0.0.0:8001"
+        echo "    ietf/manage.py runserver 8001"
         echo
         echo "to start a development instance of the Datatracker."
         echo
-        echo "    ietf/manage.py test --settings=settings_postgrestest"
+        echo "    ietf/manage.py test --settings=settings_test"
         echo
         echo "to run all the python tests."
         echo
