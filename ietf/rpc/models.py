77c800be2dbebf34f5598b9fbe6d089cbb7175a9
Jennifer Richards <jennifer@staff.ietf.org>
2023-08-15 17:06:09 -0300

    style: Apply Black

--- ietf/rpc/models.py
+++ ietf/rpc/models.py
@@ -2,33 +2,56 @@ from django.db import models
 from django.utils import timezone
 
 from ietf.doc.models import Document
-from ietf.name.models import DocRelationshipName, StdLevelName, StreamName, SourceFormatName, TlpBoilerplateChoiceName
+from ietf.name.models import (
+    DocRelationshipName,
+    StdLevelName,
+    StreamName,
+    SourceFormatName,
+    TlpBoilerplateChoiceName,
+)
 from ietf.person.models import Person
 
 
 class RfcToBe(models.Model):
     """
-    
+
     Notes:
      * not in_progress and not published = abandoned without publication
     """
+
     in_progress = models.BooleanField(default=True)
-    published = models.DateTimeField(null=True)  # should match a DocEvent on the rfc Document
+    published = models.DateTimeField(
+        null=True
+    )  # should match a DocEvent on the rfc Document
     is_april_first_rfc = models.BooleanField(default=False)
-    draft = models.ForeignKey(Document, null=True, on_delete=models.PROTECT)  # only null if is_april_first_rfc is True
+    draft = models.ForeignKey(
+        Document, null=True, on_delete=models.PROTECT
+    )  # only null if is_april_first_rfc is True
     rfc_number = models.PositiveIntegerField(null=True)
 
     cluster = models.ForeignKey("Cluster", null=True, on_delete=models.SET_NULL)
     order_in_cluster = models.PositiveSmallIntegerField(default=1)
 
     submitted_format = models.ForeignKey(SourceFormatName, on_delete=models.PROTECT)
-    submitted_std_level = models.ForeignKey(StdLevelName, on_delete=models.PROTECT, related_name="+")
-    submitted_boilerplate = models.ForeignKey(TlpBoilerplateChoiceName, on_delete=models.PROTECT, related_name="+")
-    submitted_stream = models.ForeignKey(StreamName, on_delete=models.PROTECT, related_name="+")
+    submitted_std_level = models.ForeignKey(
+        StdLevelName, on_delete=models.PROTECT, related_name="+"
+    )
+    submitted_boilerplate = models.ForeignKey(
+        TlpBoilerplateChoiceName, on_delete=models.PROTECT, related_name="+"
+    )
+    submitted_stream = models.ForeignKey(
+        StreamName, on_delete=models.PROTECT, related_name="+"
+    )
 
-    intended_std_level = models.ForeignKey(StdLevelName, on_delete=models.PROTECT, related_name="+")
-    intended_boilerplate = models.ForeignKey(TlpBoilerplateChoiceName, on_delete=models.PROTECT, related_name="+")
-    intended_stream = models.ForeignKey(StreamName, on_delete=models.PROTECT, related_name="+")
+    intended_std_level = models.ForeignKey(
+        StdLevelName, on_delete=models.PROTECT, related_name="+"
+    )
+    intended_boilerplate = models.ForeignKey(
+        TlpBoilerplateChoiceName, on_delete=models.PROTECT, related_name="+"
+    )
+    intended_stream = models.ForeignKey(
+        StreamName, on_delete=models.PROTECT, related_name="+"
+    )
 
     external_deadline = models.DateTimeField(null=True)
     internal_goal = models.DateTimeField(null=True)
@@ -45,10 +68,10 @@ class RfcToBe(models.Model):
                 name="rfctobe_unique_order_in_cluster",
                 violation_error_message="order in cluster must be unique",
                 deferrable=models.Deferrable.DEFERRED,
-            )
+            ),
         ]
 
-    
+
 class Cluster(models.Model):
     number = models.PositiveIntegerField(unique=True)
 
@@ -74,9 +97,12 @@ ASSIGNMENT_STATE_CHOICES = (
 
 class Assignment(models.Model):
     """Assignment of an RpcPerson to an RfcToBe"""
+
     rfc_to_be = models.ForeignKey(RfcToBe, on_delete=models.PROTECT)
     person = models.ForeignKey(RpcPerson, on_delete=models.PROTECT)
-    state = models.CharField(max_length=32, choices=ASSIGNMENT_STATE_CHOICES, default="unassigned")
+    state = models.CharField(
+        max_length=32, choices=ASSIGNMENT_STATE_CHOICES, default="unassigned"
+    )
     time_spent = models.DurationField()  # tbd
 
 
@@ -140,14 +166,17 @@ class RpcRelatedDocument(models.Model):
     class Meta:
         constraints = [
             models.CheckConstraint(
-                check=models.Q(target_document__isnull=True) ^ models.Q(target_rfctobe__isnull=True),
+                check=models.Q(target_document__isnull=True)
+                ^ models.Q(target_rfctobe__isnull=True),
                 name="rpcrelateddocument_exactly_one_target",
                 violation_error_message="exactly one target field must be set",
             )
         ]
 
+
 class RpcDocumentComment(models.Model):
     """Private RPC comment about a draft, RFC or RFC-to-be"""
+
     document = models.ForeignKey(Document, null=True, on_delete=models.PROTECT)
     rfc_to_be = models.ForeignKey(RfcToBe, null=True, on_delete=models.PROTECT)
     comment = models.TextField()
@@ -157,7 +186,8 @@ class RpcDocumentComment(models.Model):
     class Meta:
         constraints = [
             models.CheckConstraint(
-                check=models.Q(document__isnull=True) ^ models.Q(rfc_to_be__isnull=True),
+                check=models.Q(document__isnull=True)
+                ^ models.Q(rfc_to_be__isnull=True),
                 name="rpcdocumentcomment_exactly_one_target",
                 violation_error_message="exactly one of document or rfc_to_be must be set",
             )
@@ -166,13 +196,16 @@ class RpcDocumentComment(models.Model):
 
 class RpcAuthorComment(models.Model):
     """Private RPC comment about an author
-    
+
     Notes:
         rjs = Person(...)
         rjs.rpcauthorcomments_by.all()  # comments by
         rjs.rpcauthorcomment_set.all()  # comments about
     """
+
     person = models.ForeignKey(Person, on_delete=models.PROTECT)
     comment = models.TextField()
-    by = models.ForeignKey(Person, on_delete=models.PROTECT, related_name="rpcauthorcomments_by")
+    by = models.ForeignKey(
+        Person, on_delete=models.PROTECT, related_name="rpcauthorcomments_by"
+    )
     time = models.DateTimeField(default=timezone.now)
