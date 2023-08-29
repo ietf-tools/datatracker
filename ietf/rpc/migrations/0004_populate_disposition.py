019af0281f2781ea27874d4a9b152d6380fc2de2
Robert Sparks <rjsparks@nostrum.com>
2023-08-29 13:06:31 -0500

    chore: rerun black

--- ietf/rpc/migrations/0004_populate_disposition.py
+++ ietf/rpc/migrations/0004_populate_disposition.py
@@ -2,23 +2,18 @@
 
 from django.db import migrations
 
+
 def forward(apps, schema_editor):
     Disposition = apps.get_model("rpc", "Disposition")
 
     Disposition.objects.create(
-        slug = "in_progress",
-        name = "In Progress",
-        desc = "RfcToBe is a work in progress"
+        slug="in_progress", name="In Progress", desc="RfcToBe is a work in progress"
     )
     Disposition.objects.create(
-        slug = "published",
-        name = "Published",
-        desc = "RfcToBe has been published as an RFC"
+        slug="published", name="Published", desc="RfcToBe has been published as an RFC"
     )
     Disposition.objects.create(
-        slug = "withdrawn",
-        name = "Withdrawn",
-        desc = "RfcToBe has been withdrawn"
+        slug="withdrawn", name="Withdrawn", desc="RfcToBe has been withdrawn"
     )
 
 
@@ -27,11 +22,10 @@ def reverse(apps, schema_editor):
 
     Disposition.objects.all().delete()
 
+
 class Migration(migrations.Migration):
     dependencies = [
         ("rpc", "0003_populate_capability"),
     ]
 
-    operations = [
-        migrations.RunPython(forward, reverse)
-    ]
+    operations = [migrations.RunPython(forward, reverse)]
