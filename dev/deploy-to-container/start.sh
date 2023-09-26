1c9494d43a05ae435d7987b71f80e9df4d3cd3ce
Robert Sparks <rjsparks@nostrum.com>
2023-09-26 17:28:55 -0500

    ci: rfc-index sync on deploy

--- dev/deploy-to-container/start.sh
+++ dev/deploy-to-container/start.sh
@@ -36,7 +36,10 @@ echo "Running Datatracker checks..."
 # Migrate, adjusting to what the current state of the underlying database might be:
 
 echo "Running Datatracker migrations..."
-/usr/local/bin/python ./ietf/manage.py migrate --fake-initial --settings=settings_local
+/usr/local/bin/python ./ietf/manage.py migrate --settings=settings_local
+
+echo "Syncing with the rfc-index
+./ietf/bin/rfc-editor-updates -d 1969-01-01
 
 echo "Starting Datatracker..."
 ./ietf/manage.py runserver 0.0.0.0:8000 --settings=settings_local
