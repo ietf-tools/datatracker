ietf-tools/datatracker

commit 57030e4303aa42423335366f6d025b379b25f6ef

fix: Pre-sort WG doc table on date (#5361)
Fixes #5341.

Also remove some js code that was only used on the old agenda
page while I'm here.

ietf/static/js/list.js
@@ -266,43 +266,21 @@ $(document)
                                 });
                         }
                     });
-
-                    e.on("searchComplete", function () {
-                        var last_show_with_children = {};
-                        e.items.forEach((item) => {
-                            if ($(item.elm)
-                                .hasClass("show-with-children")) {
-                                var kind = $(item.elm)
-                                    .attr("class")
-                                    .split(/\s+/)
-                                    .join();
-                                last_show_with_children[kind] = item;
-                            }
-
-                            if (item.found) {
-                                Object.entries(last_show_with_children)
-                                    .forEach(([key, val]) => {
-                                        val.found = true;
-                                        val.show();
-                                        delete last_show_with_children[key];
-                                    });
-                            }
-
-                            if ($(item.elm)
-                                .hasClass("show-always")) {
-                                item.found = true;
-                                item.show();
-                            }
-                        });
-
-                        e.update();
-                        replace_with_internal(table, internal_table, i);
-                    });
                 });
+
                 $(table.addClass("tablesorter-done"));
                 n++;
                 $(table)[0]
                     .dispatchEvent(new Event("tablesorter:done"));
+
+                // if there is a data-default-sort attribute on a column, pre-sort the table on that
+                const presort_col = $(header_row).children("[data-default-sort]:first");
+                if (presort_col) {
+                    const order = presort_col.attr("data-default-sort");
+                    $.each(list_instance, (i, e) => {
+                        e.sort(presort_col.attr("data-sort"), { order: order, sortFunction: text_sort });
+                    });
+                }
             });
 
         // if the URL contains a #, scroll to it again, since we modified the DOM

ietf/templates/doc/search/search_results.html
@@ -20,6 +20,8 @@
                         <th scope="col" data-sort="{{ h.key }}"
                             {% if h.title|slugify == "ipr" or h.title|slugify == "ad-shepherd" %}
                               class="d-none d-sm-table-cell"
+                            {% elif h.title|slugify == 'date' %}
+                              data-default-sort="desc"
                             {% endif %}>
                             {% if "sort_url" in h %}
                                 <a href="{{ h.sort_url }}">

commit b1eb38c18bb04d826ec42631d18b537710d46b92

fix: Restore the ability to search in tables
This was broken by 57030e4303aa42423335366f6d025b379b25f6ef

ietf/static/js/list.js
@@ -267,6 +267,9 @@ $(document)
                                 });
                         }
                     });
+                    e.on("searchComplete", function () {
+                        replace_with_internal(table, internal_table, i);
+                    });
                 });
 
                 $(table.addClass("tablesorter-done"));