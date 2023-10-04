a7995065f05f929310b9f567421929203bada972
Jennifer Richards <jennifer@staff.ietf.org>
2023-10-04 14:53:23 -0300

    feat: Add @requires_api_token decorator

--- ietf/api/ietf_utils.py
+++ ietf/api/ietf_utils.py
@@ -3,7 +3,11 @@
 # This is not utils.py because Tastypie implicitly consumes ietf.api.utils.
 # See ietf.api.__init__.py for details.
 
+from functools import wraps
+
 from django.conf import settings
+from django.http import HttpResponseForbidden
+
 
 def is_valid_token(endpoint, token):
     # This is where we would consider integration with vault
@@ -13,3 +17,14 @@ def is_valid_token(endpoint, token):
         if endpoint in token_store and token in token_store[endpoint]:
             return True
     return False
+
+def requires_api_token(endpoint):
+    def decorate(f):
+        @wraps(f)
+        def wrapped(request, *args, **kwargs):
+            authtoken = request.META.get("HTTP_X_API_KEY", None)
+            if authtoken is None or not is_valid_token(endpoint, authtoken):
+                return HttpResponseForbidden()
+            return f(request, *args, **kwargs)
+        return wrapped
+    return decorate
