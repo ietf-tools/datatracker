dec99e5872a7576491461e12d999de4c6bbfeae9
Robert Sparks <rjsparks@nostrum.com>
2023-12-18 15:56:50 -0600

    fix: safer create

--- ietf/api/views.py
+++ ietf/api/views.py
@@ -209,12 +209,12 @@ def api_new_meeting_registration(request):
                     nomcom = NomCom.objects.get(is_accepting_volunteers=True)
                 except (NomCom.DoesNotExist, NomCom.MultipleObjectsReturned):
                     nomcom = None
-                if nomcom and not Volunteer.objects.filter(nomcom=nomcom,person=object.person).exists():
-                    Volunteer.objects.create(
-                        nomcom=nomcom,
-                        person=object.person,
-                        affiliation=data['affiliation'],
-                        origin='registration')
+                if nomcom:
+                    v, created =  Volunteer.objects.get_or_create(nomcom=nomcom,person=object.person)
+                    if created:
+                        v.affiliation=data['affiliation']
+                        v.origin='registration'
+                        v.save()
             return HttpResponse(response, status=202, content_type='text/plain')
     else:
         return HttpResponse(status=405)
