763b72592a22df9f9ad459cd0dfd89c24a5dd57a
Nicolas Giard <github@ngpixel.com>
2024-01-15 18:41:18 -0500

    ci: fix collectstatics build step

--- dev/deploy/collectstatics.sh
+++ dev/deploy/collectstatics.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp dev/deploy/settings_local_collectstatics.py ietf/settings_local.py
+
 # Install Python dependencies
 pip --disable-pip-version-check --no-cache-dir install -r requirements.txt
 
---
+++ dev/deploy/settings_local_collectstatics.py
@@ -0,0 +1,7 @@
+# Copyright The IETF Trust 2007-2019, All Rights Reserved
+# -*- coding: utf-8 -*-
+
+from ietf.settings import *                                          # pyflakes:ignore
+
+STATIC_URL = "https://static.ietf.org/dt/%s/"%__version__
+STATIC_ROOT = os.path.abspath(BASE_DIR + "/../static/")
