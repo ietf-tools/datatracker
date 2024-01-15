71cf00ddc63d31c7ee00866136b7803aecfa5b5b
Nicolas Giard <github@ngpixel.com>
2024-01-15 18:49:12 -0500

    ci: fix collectstatics from build workflow

--- dev/deploy/settings_local_collectstatics.py
+++ dev/deploy/settings_local_collectstatics.py
@@ -1,6 +1,7 @@
 # Copyright The IETF Trust 2007-2019, All Rights Reserved
 # -*- coding: utf-8 -*-
 
+from ietf import __version__
 from ietf.settings import *                                          # pyflakes:ignore
 
 STATIC_URL = "https://static.ietf.org/dt/%s/"%__version__
