1894404d0764e715ef0ebe9d6c74f1f9bd8d6c65
Jennifer Richards <jennifer@staff.ietf.org>
2024-01-15 23:21:45 -0400

    refactor: Make bin/rfc-editor-index-updates into a task

--- ietf/utils/tasks.py
+++ ietf/utils/tasks.py
@@ -11,6 +11,7 @@ from ietf.message.utils import send_scheduled_message_from_send_queue
 from ietf.message.models import SendQueue
 from ietf.review.tasks import send_review_reminders_task
 from ietf.stats.tasks import fetch_meeting_attendance_task
+from ietf.sync.tasks import rfc_editor_index_update_task
 from ietf.utils import log
 from ietf.utils.mail import log_smtp_exception, send_error_email
 
@@ -20,14 +21,19 @@ def every_15m_task():
     """Queue four-times-hourly tasks for execution"""
     # todo decide whether we want this to be a meta-task or to individually schedule the tasks
     send_scheduled_mail_task.delay()
+    # Parse the last year of RFC index data to get new RFCs. Needed until
+    # https://github.com/ietf-tools/datatracker/issues/3734 is addressed.
+    rfc_editor_index_update_task.delay(full_index=False)
 
 
 @shared_task
 def daily_task():
     """Queue daily tasks for execution"""
-    # todo - decide whether to queue the every_15m_task here or to make them independent
     fetch_meeting_attendance_task.delay()
     send_review_reminders_task.delay()
+    # Run an extended version of the rfc editor update to catch changes
+    # with backdated timestamps
+    rfc_editor_index_update_task.delay(full_index=True)
 
 
 @shared_task
