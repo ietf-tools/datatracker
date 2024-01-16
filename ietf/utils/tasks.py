291cc29d413b92783aeeb248cc9fd585c30c7567
Jennifer Richards <jennifer@staff.ietf.org>
2024-01-15 22:08:07 -0400

    refactor: Make bin/send-review-reminders into a task

--- ietf/utils/tasks.py
+++ ietf/utils/tasks.py
@@ -4,10 +4,12 @@
 #
 from celery import shared_task
 
+from ietf.review.tasks import send_review_reminders_task
 from ietf.stats.tasks import fetch_meeting_attendance_task
 
 
 @shared_task
 def daily_task():
     fetch_meeting_attendance_task.delay()
+    send_review_reminders_task.delay()
 
