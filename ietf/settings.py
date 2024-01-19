b38e8c38aeb352263a9b8893018e5c1d203010ed
Robert Sparks <rjsparks@nostrum.com>
2024-01-19 14:21:09 -0600

    chore: shadowbox with mypy

--- ietf/settings.py
+++ ietf/settings.py
@@ -170,7 +170,7 @@ if SERVER_MODE != 'production' and SERVE_CDN_FILES_LOCALLY_IN_DEV_MODE:
     STATIC_ROOT = os.path.abspath(BASE_DIR + "/../static/")
 else:
     STATIC_URL = "https://static.ietf.org/lib/%s/"%__version__
-    STATIC_ROOT = None
+    # Intentionally not setting STATIC_ROOT - see django/django (the default is None)
 
 # List of finder classes that know how to find static files in
 # various locations.
