8d12071bf5d022e241918551ad8887eb0a4659ea
Jennifer Richards <jennifer@staff.ietf.org>
2024-01-22 14:04:16 -0400

    refactor: Move cron jobs to celery tasks (#6926)
    * refactor: Factor out helper from fetch_meeting_attendance.py
    
    * feat: Define fetch_meeting_attendance_task task
    
    Equivalent to the fetch_meeting_attendance management command
    
    * chore: Disable fetch_meeting_attendance in bin/daily
    
    * feat: Log errors in fetch_meeting_attendance_task
    
    * feat: Enable a result backend for celery
    
    Ignore results by default, but enable the backend so we
    can manage tasks
    
    * feat: Define daily task in ietf.utils.tasks
    
    * refactor: Make bin/send-review-reminders into a task
    
    * refactor: Make bin/send-scheduled-mail into a task
    
    * chore: Update copyright years
    
    * refactor: Make bin/rfc-editor-index-updates into a task
    
    * refactor: Accept date type in rfc index update fn
    
    * chore: Update comment
    
    * fix: Annotate param as Optional
    
    * fix: Revert treating skip_older_than_date as str
    
    Misunderstood the comment, "fixed" a non-bug. Oops.
    
    * feat: mgmt command to create periodic tasks
    
    * feat: add summary of tasks to mgmt cmd
    
    * style: black
    
    * fix: Remove debug statements
    
    * feat: Enable/disable tasks
    
    * chore: Disable periodic tasks by default
    
    * chore: Revert changes to daily and every15m
    
    * fix: Call intended function
    
    * chore: Add task descriptions

---
+++ ietf/review/tasks.py
@@ -0,0 +1,43 @@
+# Copyright The IETF Trust 2024, All Rights Reserved
+#
+# Celery task definitions
+#
+from celery import shared_task
+
+from ietf.review.utils import (
+    review_assignments_needing_reviewer_reminder, email_reviewer_reminder,
+    review_assignments_needing_secretary_reminder, email_secretary_reminder,
+    send_unavailability_period_ending_reminder, send_reminder_all_open_reviews,
+    send_review_reminder_overdue_assignment, send_reminder_unconfirmed_assignments)
+from ietf.utils.log import log
+from ietf.utils.timezone import date_today, DEADLINE_TZINFO
+
+
+@shared_task
+def send_review_reminders_task():
+    today = date_today(DEADLINE_TZINFO)
+    
+    for assignment in review_assignments_needing_reviewer_reminder(today):
+        email_reviewer_reminder(assignment)
+        log("Emailed reminder to {} for review of {} in {} (req. id {})".format(assignment.reviewer.address, assignment.review_request.doc_id, assignment.review_request.team.acronym, assignment.review_request.pk))
+    
+    for assignment, secretary_role in review_assignments_needing_secretary_reminder(today):
+        email_secretary_reminder(assignment, secretary_role)
+        review_req = assignment.review_request
+        log("Emailed reminder to {} for review of {} in {} (req. id {})".format(secretary_role.email.address, review_req.doc_id, review_req.team.acronym, review_req.pk))
+    
+    period_end_reminders_sent = send_unavailability_period_ending_reminder(today)
+    for msg in period_end_reminders_sent:
+        log(msg)
+    
+    overdue_reviews_reminders_sent = send_review_reminder_overdue_assignment(today)
+    for msg in overdue_reviews_reminders_sent:
+        log(msg)
+    
+    open_reviews_reminders_sent = send_reminder_all_open_reviews(today)
+    for msg in open_reviews_reminders_sent:
+        log(msg)
+    
+    unconfirmed_assignment_reminders_sent = send_reminder_unconfirmed_assignments(today)
+    for msg in unconfirmed_assignment_reminders_sent:
+        log(msg)
---
+++ ietf/utils/tasks.py
@@ -0,0 +1,52 @@
+# Copyright The IETF Trust 2024 All Rights Reserved
+#
+# Celery task definitions
+#
+from django.utils import timezone
+
+from celery import shared_task
+from smtplib import SMTPException
+
+from ietf.message.utils import send_scheduled_message_from_send_queue
+from ietf.message.models import SendQueue
+from ietf.review.tasks import send_review_reminders_task
+from ietf.stats.tasks import fetch_meeting_attendance_task
+from ietf.sync.tasks import rfc_editor_index_update_task
+from ietf.utils import log
+from ietf.utils.mail import log_smtp_exception, send_error_email
+
+
+@shared_task
+def every_15m_task():
+    """Queue four-times-hourly tasks for execution"""
+    # todo decide whether we want this to be a meta-task or to individually schedule the tasks
+    send_scheduled_mail_task.delay()
+    # Parse the last year of RFC index data to get new RFCs. Needed until
+    # https://github.com/ietf-tools/datatracker/issues/3734 is addressed.
+    rfc_editor_index_update_task.delay(full_index=False)
+
+
+@shared_task
+def daily_task():
+    """Queue daily tasks for execution"""
+    fetch_meeting_attendance_task.delay()
+    send_review_reminders_task.delay()
+    # Run an extended version of the rfc editor update to catch changes
+    # with backdated timestamps
+    rfc_editor_index_update_task.delay(full_index=True)
+
+
+@shared_task
+def send_scheduled_mail_task():
+    """Send scheduled email
+    
+    This is equivalent to `ietf/bin/send-scheduled-mail all`, which was the only form used in the cron job.
+    """
+    needs_sending = SendQueue.objects.filter(sent_at=None).select_related("message")
+    for s in needs_sending:
+        try:
+            send_scheduled_message_from_send_queue(s)
+            log.log('Sent scheduled message %s "%s"' % (s.id, s.message.subject))
+        except SMTPException as e:
+            log_smtp_exception(e)
+            send_error_email(e)
