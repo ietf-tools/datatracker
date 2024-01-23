dee90914f09363a1a3b7a1ffb4e0c1bbf273b353
Jennifer Richards <jennifer@staff.ietf.org>
2024-01-23 12:20:23 -0400

    test: Cannot force-reset draftless RFC

--- ietf/doc/management/commands/tests.py
+++ ietf/doc/management/commands/tests.py
@@ -47,6 +47,9 @@ class CommandTests(TestCase):
         with self.assertRaises(CommandError, msg="Cannot reset an RFC with no draft"):
             self._call_command(command_name, rfc.rfc_number)
 
+        with self.assertRaises(CommandError, msg="Cannot force-reset an RFC with no draft"):
+            self._call_command(command_name, rfc.rfc_number, "--force")
+
         # Link the draft to the rfc
         rfc.targets_related.create(relationship_id="became_rfc", source=draft)
 
