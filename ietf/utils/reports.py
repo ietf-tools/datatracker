7b3c35f70f858b9778aef75dbd0d67f2f4529d45
Robert Sparks <rjsparks@nostrum.com>
2024-01-25 11:42:07 -0600

    fix: filter to posted submissions and produce report

--- ietf/utils/reports.py
+++ ietf/utils/reports.py
@@ -1,5 +1,6 @@
 # Copyright The IETF Trust 2023, All Rights Reserved
 
+import csv
 from typing import List, Set, Tuple
 from django.db.models import QuerySet
 
@@ -12,7 +13,7 @@ from ietf.submit.models import Submission
 def authors_by_year(year: int) -> Set[str]:
     """Email addresses provided by I-D authors for drafts that were submitted in the given year."""
     addresses = set()
-    for submission in Submission.objects.filter(submission_date__year=year):
+    for submission in Submission.objects.filter(submission_date__year=year,state="posted"):
         addresses.update([a["email"] for a in submission.authors])
     return addresses
 
@@ -23,7 +24,7 @@ def submitters_by_year(year: int) -> Set[str]:
         [
             parseaddr(a)[1]
             for a in Submission.objects.filter(
-                submitter__contains="@", submission_date__year=year
+                submitter__contains="@", submission_date__year=year, state="posted"
             ).values_list("submitter", flat=True)
         ]
     )
@@ -43,3 +44,19 @@ def unique_people(addresses: List[str]) -> Tuple["QuerySet[Person]", Set]:
     persons = Person.objects.filter(email__address__in=addresses).distinct()
     known_email = set(persons.values_list("email__address", flat=True))
     return (persons, set(addresses) - set(known_email))
+
+def write_reports(year: int) -> None :
+    authors = authors_by_year(year)
+    submitters = submitters_by_year(year)
+    print(f"authors: {len(authors)}")
+    print(f"submitters: {len(submitters)}")
+    persons, nopersons = unique_people(authors)
+    print(f"authors: unique persons: {len(persons)}, no person found: {len(nopersons)}")
+    persons, nopersons = unique_people(submitters)
+    print(f"submitters: unique persons: {len(persons)}, no person found: {len(nopersons)}")
+    with open("authors.csv","w",newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(authors)
+    with open("submitters.csv","w",newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(submitters)
