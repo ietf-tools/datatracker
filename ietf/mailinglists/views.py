442e873ff1fc97023d0d8dd54dacd3b580dae94c
Robert Sparks <rjsparks@nostrum.com>
2024-02-02 17:31:03 -0600

    chore: black

--- ietf/mailinglists/views.py
+++ ietf/mailinglists/views.py
@@ -1,19 +1,25 @@
 # Copyright The IETF Trust 2007-2022, All Rights Reserved
 
-import re
-
 from django.shortcuts import render
 
-import debug                            # pyflakes:ignore
+import debug  # pyflakes:ignore
 
 from ietf.group.models import Group
 from ietf.mailinglists.models import NonWgMailingList
 
-def groups(request):
-    groups = Group.objects.filter(type__features__acts_like_wg=True, list_archive__startswith='http').exclude(state__in=('bof', 'conclude')).order_by("acronym")
 
-    return render(request, "mailinglists/group_archives.html", { "groups": groups } )
+def groups(request):
+    groups = (
+        Group.objects.filter(
+            type__features__acts_like_wg=True, list_archive__startswith="http"
+        )
+        .exclude(state__in=("bof", "conclude"))
+        .order_by("acronym")
+    )
+
+    return render(request, "mailinglists/group_archives.html", {"groups": groups})
+
 
 def nonwg(request):
     lists = NonWgMailingList.objects.order_by("name")
-    return render(request, "mailinglists/nonwg.html", { "lists": lists } )
+    return render(request, "mailinglists/nonwg.html", {"lists": lists})
