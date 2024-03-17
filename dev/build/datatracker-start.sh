d119ca470f7d96730227c4e995e17691cadc13b0
Robert Sparks <rjsparks@nostrum.com>
2024-03-16 23:00:50 -0500

    feat: use gunicorn

--- dev/build/datatracker-start.sh
+++ dev/build/datatracker-start.sh
@@ -7,4 +7,14 @@ echo "Running Datatracker migrations..."
 ./ietf/manage.py migrate --settings=settings_local
 
 echo "Starting Datatracker..."
-./ietf/manage.py runserver 0.0.0.0:8000 --settings=settings_local
+
+gunicorn \
+          --workers 53 \
+          --max-requests 32768 \
+          --timeout 180 \
+          --bind :8000 \
+          --error-logfile gunicorn_error.log \
+          --log-level info \
+          ietf.wsgi:application
+
+          #--env SCOUT_NAME=Datatracker \
