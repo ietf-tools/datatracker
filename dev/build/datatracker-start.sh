328ddc4ec57af61378a4668e1b2eacf1ec00da65
Jennifer Richards <jennifer@staff.ietf.org>
2024-03-21 14:08:13 +1000

    fix: typo

--- dev/build/datatracker-start.sh
+++ dev/build/datatracker-start.sh
@@ -14,7 +14,7 @@ echo "Starting Datatracker..."
 gunicorn \
           --workers ${DATATRACKER_GUNICORN_WORKERS:-9} \
           --max-requests ${DATATRACKER_GUNICORN_MAX_REQUESTS:-32768} \
-          --timeout ${DATATRACKER_GUNICORN_TIMEOUT-180} \
+          --timeout ${DATATRACKER_GUNICORN_TIMEOUT:-180} \
           --bind :8000 \
           --log-level ${DATATRACKER_GUNICORN_LOG_LEVEL:-info} \
           ietf.wsgi:application
