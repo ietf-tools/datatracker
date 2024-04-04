8cfb54ff47124f1d8ad7b4191ecefb2266d7363f
muhzinx <118731257+muhzinx@users.noreply.github.com>
2024-04-04 12:02:31 +0300

    Delete dev/diff directory

--- dev/diff/cleanup.sh
+++
@@ -1,7 +0,0 @@
-# Force remove docker resources created by this tool
-# in case it doesn't clean up properly
-
-docker rm dt-diff-app-source dt-diff-app-target dt-diff-db-source dt-diff-db-target -f
-docker network rm dt-diff-net
-
-echo "Docker resources cleaned successfully."
