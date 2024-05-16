d9bac3b0dca199f62289ba4e185f65445ea5e3a7
Jennifer Richards <jennifer@staff.ietf.org>
2024-05-16 11:13:32 -0300

    fix: fix bug uncovered by tests

--- ietf/group/tasks.py
+++ ietf/group/tasks.py
@@ -15,18 +15,19 @@ from .utils import fill_in_charter_info, fill_in_wg_drafts, fill_in_wg_roles
 @shared_task
 def generate_wg_charters_files_task():
     areas = Group.objects.filter(type="area", state="active").order_by("name")
+    groups = Group.objects.filter(type="wg", state="active").exclude(parent=None).order_by("acronym")
+    for group in groups:
+        fill_in_charter_info(group)
+        fill_in_wg_roles(group)
+        fill_in_wg_drafts(group)
     for area in areas:
-        area.groups = Group.objects.filter(parent=area, type="wg", state="active").order_by("name")
-        for group in area.groups:
-            fill_in_charter_info(group)
-            fill_in_wg_roles(group)
-            fill_in_wg_drafts(group)
+        area.groups = [g for g in groups if g.parent_id == area.pk]
     charter_path = Path(settings.CHARTER_PATH)
     (charter_path / "1wg-charters.txt").write_text(
         render_to_string("group/1wg-charters.txt", {"areas": areas}),
         encoding="utf8",
     )
     (charter_path / "1wg-charters-by-acronym.txt").write_text(
-        render_to_string("group/1wg-charters-by-acronym.txt", {"areas": areas}),
+        render_to_string("group/1wg-charters-by-acronym.txt", {"groups": groups}),
         encoding="utf8",
     )
