ce24d70936bacc23131a2a1039c68d52406faad6
Jennifer Richards <jennifer@staff.ietf.org>
2024-05-29 13:21:40 -0300

    chore: create daily PeriodicTask

--- ietf/utils/management/commands/periodic_tasks.py
+++ ietf/utils/management/commands/periodic_tasks.py
@@ -241,6 +241,17 @@ class Command(BaseCommand):
             ),
         )
 
+        PeriodicTask.objects.get_or_create(
+            name="Purge old personal API key events",
+            task="ietf.person.tasks.purge_personal_api_key_events_task",
+            kwargs=json.dumps(dict(keep_days=14)),
+            defaults=dict(
+                enabled=False,
+                crontab=self.crontabs["daily"],
+                description="Purge PersonApiKeyEvent instances older than 14 days",
+            ),
+        )
+
     def show_tasks(self):
         for label, crontab in self.crontabs.items():
             tasks = PeriodicTask.objects.filter(crontab=crontab).order_by(
