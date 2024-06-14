c1941df7e767e729f4b874631faf21921280b5ae
Jennifer Richards <jennifer@staff.ietf.org>
2024-06-14 11:28:14 -0300

    chore: adjustments for k8s deployment (#7531)
    * chore: handle TERM in datatracker-start.sh
    
    * chore: delay celery start if migration needed
    
    * chore: skip-checks when migrating
    
    * chore: label beat/celery as deleteBeforeUpgrade
    
    Used by the infra-k8s deployment process to flag
    these as needing to be shut down before a new
    release rolls out.
    
    * chore: increase termination grace periods

--- dev/build/datatracker-start.sh
+++ dev/build/datatracker-start.sh
@@ -4,14 +4,29 @@ echo "Running Datatracker checks..."
 ./ietf/manage.py check
 
 echo "Running Datatracker migrations..."
-./ietf/manage.py migrate --settings=settings_local
+./ietf/manage.py migrate --skip-checks --settings=settings_local
 
 echo "Starting Datatracker..."
 
+# trap TERM and shut down gunicorn
+cleanup () {
+    if [[ -n "${gunicorn_pid}" ]]; then
+        echo "Terminating gunicorn..."
+        kill -TERM "${gunicorn_pid}"
+        wait "${gunicorn_pid}"
+    fi
+}
+
+trap 'trap "" TERM; cleanup' TERM
+
+# start gunicorn in the background so we can trap the TERM signal
 gunicorn \
           --workers "${DATATRACKER_GUNICORN_WORKERS:-9}" \
           --max-requests "${DATATRACKER_GUNICORN_MAX_REQUESTS:-32768}" \
           --timeout "${DATATRACKER_GUNICORN_TIMEOUT:-180}" \
           --bind :8000 \
           --log-level "${DATATRACKER_GUNICORN_LOG_LEVEL:-info}" \
-          ietf.wsgi:application
+          ${DATATRACKER_GUNICORN_EXTRA_ARGS} \
+          ietf.wsgi:application &
+gunicorn_pid=$!
+wait "${gunicorn_pid}"
