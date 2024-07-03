396c99abb8156ecf2287fae38f7d0dce52ccac71
Jennifer Richards <jennifer@staff.ietf.org>
2024-07-03 20:05:08 -0300

    refactor: eliminate base_site.html

--- ietf/admin/sites.py
+++ ietf/admin/sites.py
@@ -1,7 +1,13 @@
 # Copyright The IETF Trust 2024, All Rights Reserved
 from django.contrib.admin import AdminSite as _AdminSite
-
+from django.conf import settings
+from django.utils.html import mark_safe
 
 class AdminSite(_AdminSite):
     site_title = "Datatracker admin"
-    site_header = "Datatracker administration"
+    
+    def site_header(self):
+        if settings.SERVER_MODE == "production":
+            return "Datatracker administration"
+        else:
+            return mark_safe('Datatracker administration <span class="text-danger">&delta;</span>')
