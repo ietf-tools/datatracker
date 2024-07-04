a5cbec2983f22be371b96b52029ecdff083f9e26
Jennifer Richards <jennifer@staff.ietf.org>
2024-07-04 13:30:01 -0300

    refactor: make method static; fix tests

--- ietf/admin/sites.py
+++ ietf/admin/sites.py
@@ -3,10 +3,12 @@ from django.contrib.admin import AdminSite as _AdminSite
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
+
 class AdminSite(_AdminSite):
     site_title = "Datatracker admin"
     
-    def site_header(self):
+    @staticmethod
+    def site_header():
         if settings.SERVER_MODE == "production":
             return "Datatracker administration"
         else:
