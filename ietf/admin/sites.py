6e6b350d70d9075d6c80b1c4ce292a84c2018903
Jennifer Richards <jennifer@staff.ietf.org>
2024-07-04 11:37:22 -0300

    fix: commit missing __init__.py

--- ietf/admin/sites.py
+++ ietf/admin/sites.py
@@ -1,7 +1,7 @@
 # Copyright The IETF Trust 2024, All Rights Reserved
 from django.contrib.admin import AdminSite as _AdminSite
 from django.conf import settings
-from django.utils.html import mark_safe
+from django.utils.safestring import mark_safe
 
 class AdminSite(_AdminSite):
     site_title = "Datatracker admin"
