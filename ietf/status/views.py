933f0bed272a353ce0271b6571592e8cc10efbd6
Matthew Holloway <Matthew Holloway>
2024-07-09 20:57:08 +1200

    feat: Status

--- ietf/status/views.py
+++ ietf/status/views.py
@@ -1,12 +1,47 @@
 # Copyright The IETF Trust 2016-2020, All Rights Reserved
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from ietf.utils import markdown
+from django.core.exceptions import FieldError
+from django.shortcuts import render, get_object_or_404
+from ietf.status.models import Status
 
 import debug                            # pyflakes:ignore
 
-def status_index(request):
-    return render(request, "status/index.html")
+def get_context_data():
+    status = Status.objects.order_by("-date").first()
+    if status is None or status.active == False:
+        return { "hasMessage": False }
 
-def status_api(request):
-    return HttpResponse(response, status=200, content_type='application/json')
+    if status.slug is None:
+        raise FieldError(f"No slug generated. This shouldn't happen.")
+
+    context = {
+        "hasMessage": True,
+        "id": status.id,
+        "slug": status.slug,
+        "title": status.title,
+        "body": status.body,
+        "url": f"/status/{status.slug}",
+        "date": status.date.isoformat(),
+        "by": status.by.name,
+    }
+    return context
+
+def status_index(request):
+    return render(request, "status/index.html", context=get_context_data())
+
+def status_latest_html(request):
+    return render(request, "status/latest.html", context=get_context_data())
+
+def status_page(request, slug):
+    status = get_object_or_404(Status, slug=slug)
+    return render(request, "status/status.html", context={
+        'status': status,
+        'status_page_html': markdown.markdown(status.page),
+    })
+
+def status_latest_json(request):
+    return HttpResponse(json.dumps(get_context_data()), status=200, content_type='application/json')
