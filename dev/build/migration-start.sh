9a96fcb5b97a0aaa055c178d1f38b7b1623f8937
Jennifer Richards <jennifer@staff.ietf.org>
2024-07-10 13:41:03 -0300

    chore: no checks before migration

--- dev/build/migration-start.sh
+++ dev/build/migration-start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Running Datatracker checks..."
-./ietf/manage.py check
-
 echo "Running Datatracker migrations..."
 ./ietf/manage.py migrate --skip-checks --settings=settings_local
 
