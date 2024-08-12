5d64c2846b055f0590003e12e6e9afc7de1a87de
Matthew Holloway <matthew@staff.ietf.org>
2024-08-13 08:48:32 +1200

    chore: PR feedback (naming)

--- ietf/doc/templatetags/document_type_badge.py
+++ ietf/doc/templatetags/document_type_badge.py
@@ -1,7 +1,9 @@
 # Copyright The IETF Trust 2015-2020, All Rights Reserved
 from django import template
+from django.conf import settings
 from django.template.loader import render_to_string
-# from django.urls import reverse
+
+from warnings import warn
 
 # from ietf.group.models import Group
 # from ietf.name.models import GroupTypeName
@@ -16,8 +18,15 @@ def document_type_badge(doc, snapshot):
             "doc/badge/doc-badge-rfc.html",
             {"doc": doc, "snapshot": snapshot},
         )
-
-    return render_to_string(
-        "doc/badge/doc-badge-other.html",
-        {"doc": doc, "snapshot": snapshot},
-    )
+    elif doc.type == "draft":
+        return render_to_string(
+            "doc/badge/doc-badge-draft.html",
+            {"doc": doc, "snapshot": snapshot},
+        )
+    else:
+        error_message = f"Unsupported document type {doc.type}."
+        if settings.SERVER_MODE != 'production':
+            raise ValueError(error_message)
+        else:
+            warn(error_message)
+        return ""
