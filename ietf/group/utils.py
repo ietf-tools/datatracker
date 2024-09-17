74598983a273004b40472ac9b09adfa3b89647aa
Robert Sparks <rjsparks@nostrum.com>
2024-09-17 09:12:55 -0500

    fix: restrict groupman_role matches to active states

--- ietf/group/utils.py
+++ ietf/group/utils.py
@@ -161,7 +161,7 @@ def can_manage_some_groups(user):
     authroles = set(list(chain.from_iterable(GroupFeatures.objects.values_list("groupman_authroles",flat=True))))
     extra_role_qs = dict()
     for gf in GroupFeatures.objects.all():
-        extra_role_qs[f"{gf.type_id} groupman roles"] = Q(name__in=gf.groupman_roles, group__type_id=gf.type_id)
+        extra_role_qs[f"{gf.type_id} groupman roles"] = Q(name__in=gf.groupman_roles, group__type_id=gf.type_id, group__state__in=["active","bof", "proposed"])
     return has_role(user, authroles, extra_role_qs = extra_role_qs)
 
     # for gf in GroupFeatures.objects.all():
