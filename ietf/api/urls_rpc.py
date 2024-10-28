9cf8f1015cc692b252ee3761f043df21e9d08afd
Robert Sparks <rjsparks@nostrum.com>
2024-10-28 14:23:33 -0500

    feat: support for building rfc authors (#7983)
    * feat: support for building rfc authors
    
    * chore: copyrights

--- ietf/api/urls_rpc.py
+++ ietf/api/urls_rpc.py
@@ -1,4 +1,4 @@
-# Copyright The IETF Trust 2023, All Rights Reserved
+# Copyright The IETF Trust 2023-2024, All Rights Reserved
 
 from ietf.api import views_rpc
 
@@ -10,7 +10,9 @@ urlpatterns = [
     url(r"^doc/drafts_by_names/", views_rpc.drafts_by_names),
     url(r"^doc/submitted_to_rpc/$", views_rpc.submitted_to_rpc),
     url(r"^doc/rfc/original_stream/$", views_rpc.rfc_original_stream),
+    url(r"^doc/rfc/authors/$", views_rpc.rfc_authors),
     url(r"^person/create_demo_person/$", views_rpc.create_demo_person),
+    url(r"^person/persons_by_email/$", views_rpc.persons_by_email),
     url(r"^person/(?P<person_id>[0-9]+)$", views_rpc.rpc_person),
     url(r"^persons/$", views_rpc.rpc_persons),
     url(r"^subject/(?P<subject_id>[0-9]+)/person/$", views_rpc.rpc_subject_person),
