00b64dfe5a0eaded5d462dc86de593dbd36d05cb
Jennifer Richards <jennifer@staff.ietf.org>
2024-10-30 17:11:02 -0300

    fix: actually working demo endpoint

--- ietf/person/api.py
+++ ietf/person/api.py
@@ -1,10 +1,10 @@
 # Copyright The IETF Trust 2024, All Rights Reserved
 """DRF API Views"""
 
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, permissions, viewsets
 
 from .models import Person
-
+from .serializers import PersonSerializer
 
 class PersonViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """Person viewset
@@ -12,4 +12,7 @@ class PersonViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     Mostly demo for now. Only allows retrieving single instances. Think hard about permissions before
     allowing write or list access.
     """
+    permission_classes = [permissions.AllowAny]
+
     queryset = Person.objects.all()
+    serializer_class = PersonSerializer
