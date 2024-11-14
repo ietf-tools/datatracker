b7c90129630f29769ebe054d997fe189c529e568
NGPixel <github@ngpixel.com>
2024-11-14 18:32:06 +0000

    ci: fix playwright floor-plan middleware

--- dev/vite-plugins/serve-preview-assets.js
+++ dev/vite-plugins/serve-preview-assets.js
@@ -4,10 +4,9 @@ import path from 'node:path'
 export default function servePreviewAssets () {
   return {
     name: 'serve-preview-assets',
-    configurePreviewServer(server) {
+    configurePreviewServer (server) {
       server.middlewares.use('/media/floor', (req, res, next) => {
-        const reqUrl = new URL(req.url)
-        send(req, reqUrl.pathname, { root: path.join(process.cwd(), 'playwright/data/floor-plan-images') }).pipe(res)
+        send(req, req.url, { root: path.join(process.cwd(), 'playwright/data/floor-plan-images') }).pipe(res)
       })
     }
   }
