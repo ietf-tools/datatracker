0c702bbace7786c34f4f34f057fb45e7d8fcd8b2
Jennifer Richards <jennifer@staff.ietf.org>
2024-11-14 08:53:24 -0400

    chore: better schema_path_prefix

--- ietf/settings.py
+++ ietf/settings.py
@@ -576,7 +576,7 @@ SPECTACULAR_SETTINGS = {
     "TITLE": "Datatracker API",
     "DESCRIPTION": "Datatracker API",
     "VERSION": "1.0.0",
-    "SCHEMA_PATH_PREFIX": "/api/",
+    "SCHEMA_PATH_PREFIX": "/api/v3/",
     "COMPONENT_SPLIT_REQUEST": True,
     "COMPONENT_NO_READ_ONLY_REQUIRED": True,
     "SERVERS": [
