c474310c17325f6b81fdc01f7d8126758d4970bf
Kesara Rathnayake <kesara@fq.nz>
2024-11-19 11:10:25 +1300

    fix: Expose libraries that are important for document submission

--- ietf/settings.py
+++ ietf/settings.py
@@ -1279,7 +1279,7 @@ if "CACHES" not in locals():
 
 PUBLISH_IPR_STATES = ['posted', 'removed', 'removed_objfalse']
 
-IMPORTANT_LIBRARIES = ["django", "celery", "lxml", "markdown", "psycopg2", "pyang", "rfc2html", "weasyprint", "xml2rfc"]
+IMPORTANT_LIBRARIES = ["markdown", "pyang", "rfc2html", "xml2rfc"]
 
 # We provide a secret key only for test and development modes.  It's
 # absolutely vital that django fails to start in production mode unless a
