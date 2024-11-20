f61dd6ec40ce32e0f1ab1c1c67948f17a96c97c6
Jennifer Richards <jennifer@staff.ietf.org>
2024-11-20 17:04:38 -0400

    test: test person-detail view

---
+++ ietf/api/tests_core.py
@@ -0,0 +1,41 @@
+# Copyright The IETF Trust 2024, All Rights Reserved
+"""Core API tests"""
+from django.urls import reverse as urlreverse
+from rest_framework.test import APIClient
+
+from ietf.person.factories import PersonFactory
+from ietf.utils.test_utils import TestCase
+
+
+class CoreApiTestCase(TestCase):
+    client_class = APIClient
+
+
+class PersonTests(CoreApiTestCase):
+    def test_person_detail(self):
+        person = PersonFactory()
+        url = urlreverse("ietf.api.core_api.person-detail", kwargs={"pk": person.pk})
+        r = self.client.get(url, format="json")
+        self.assertEqual(r.status_code, 403)
+        self.client.login(
+            username=person.user.username, password=person.user.username + "+password"
+        )
+        r = self.client.get(url, format="json")
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(
+            r.data,
+            {
+                "id": person.pk,
+                "name": person.name,
+                "emails": [
+                    {
+                        "person": person.pk,
+                        "address": email.address,
+                        "primary": email.primary,
+                        "active": email.active,
+                        "origin": email.origin,
+                    }
+                    for email in person.email_set.all()
+                ],
+            },
+        )
