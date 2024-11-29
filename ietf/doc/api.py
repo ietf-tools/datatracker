513423681568fa817f3706cb3c2c2cf43bff5bcd
Jennifer Richards <jennifer@staff.ietf.org>
2024-11-29 13:21:21 -0400

    feat: pagination + ordered RFC index

--- ietf/doc/api.py
+++ ietf/doc/api.py
@@ -1,13 +1,20 @@
 # Copyright The IETF Trust 2024, All Rights Reserved
 """Doc API implementations"""
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Document
 from .serializers import RfcMetadataSerializer
 
 
+class RfcLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 500
+
+
 class RfcViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     permission_classes = []
-    queryset = Document.objects.filter(type_id="rfc")
+    pagination_class = RfcLimitOffsetPagination
+    queryset = Document.objects.filter(type_id="rfc").order_by("-rfc_number")
     serializer_class = RfcMetadataSerializer
