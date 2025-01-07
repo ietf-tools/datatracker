0a2070d8d222b323157dea3f8f583b526454da4d
Robert Sparks <rjsparks@nostrum.com>
2025-01-07 13:14:32 -0600

    fix: log exception description

--- ietf/doc/utils_charter.py
+++ ietf/doc/utils_charter.py
@@ -112,10 +112,10 @@ def fix_charter_revision_after_approval(charter, by):
     )
     try:
         os.link(new, ftp_filepath)
-    except IOError:
+    except IOError as e:
         log(
-            "There was an error creating a hardlink at %s pointing to %s"
-            % (ftp_filepath, new)
+            "There was an error creating a hardlink at %s pointing to %s: %s"
+            % (ftp_filepath, new, e)
         )
 
     events = []
