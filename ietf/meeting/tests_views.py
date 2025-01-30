f2e14caf1f9f19e617cf28fa7ca46f55caf72907
Jennifer Richards <jennifer@staff.ietf.org>
2025-01-30 11:21:34 -0400

    chore: remove accidental import
    *shakes fist at pycharm*

--- ietf/meeting/tests_views.py
+++ ietf/meeting/tests_views.py
@@ -52,7 +52,6 @@ from ietf.meeting.utils import create_recording, delete_recording, get_next_sequ
 from ietf.meeting.views import session_draft_list, parse_agenda_filter_params, sessions_post_save, agenda_extract_schedule
 from ietf.meeting.views import get_summary_by_area, get_summary_by_type, get_summary_by_purpose, generate_agenda_data
 from ietf.name.models import SessionStatusName, ImportantDateName, RoleName, ProceedingsMaterialTypeName
-from ietf.settings import YOUTUBE_BASE_URL
 from ietf.utils.decorators import skip_coverage
 from ietf.utils.mail import outbox, empty_outbox, get_payload_text
 from ietf.utils.test_utils import TestCase, login_testing_unauthorized, unicontent
