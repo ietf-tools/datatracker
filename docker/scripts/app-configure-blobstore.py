a10cb6d03f518e3f5aa14ff6a91e4d619e7c3c92
Jennifer Richards <jennifer@staff.ietf.org>
2025-02-13 17:56:59 -0400

    chore: parameters in app-configure-blobstore
    Allows use with other blob stores.

--- docker/scripts/app-configure-blobstore.py
+++ docker/scripts/app-configure-blobstore.py
@@ -2,6 +2,7 @@
 # Copyright The IETF Trust 2024, All Rights Reserved
 
 import boto3
+import os
 import sys
 
 from ietf.settings import MORE_STORAGE_NAMES
@@ -10,15 +11,17 @@ from ietf.settings import MORE_STORAGE_NAMES
 def init_blobstore():
     blobstore = boto3.resource(
         "s3",
-        endpoint_url="http://blobstore:9000",
-        aws_access_key_id="minio_root",
-        aws_secret_access_key="minio_pass",
+        endpoint_url=os.environ.get("BLOB_STORE_ENDPOINT_URL", "http://blobstore:9000"),
+        aws_access_key_id=os.environ.get("BLOB_STORE_ACCESS_KEY", "minio_root"),
+        aws_secret_access_key=os.environ.get("BLOB_STORE_SECRET_KEY", "minio_pass"),
         aws_session_token=None,
         config=boto3.session.Config(signature_version="s3v4"),
         verify=False,
     )
     for bucketname in MORE_STORAGE_NAMES:
-        blobstore.create_bucket(Bucket=bucketname)
+        blobstore.create_bucket(
+            Bucket=f"{os.environ.get('BLOB_STORE_BUCKET_PREFIX', '')}{bucketname}".strip()
+        )
 
 
 if __name__ == "__main__":
