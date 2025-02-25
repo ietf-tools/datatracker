ed6fb8ccef90787bccab119196b88ca083690793
Jennifer Richards <jennifer@staff.ietf.org>
2025-02-25 16:25:04 -0400

    chore: flower support to prod Docker image

--- dev/build/start.sh
+++ dev/build/start.sh
@@ -2,7 +2,7 @@
 #
 # Environment config:
 #
-#  CONTAINER_ROLE - datatracker, celery, or beat (defaults to datatracker)
+#  CONTAINER_ROLE - datatracker, celery, beat, or flower (defaults to datatracker)
 #
 case "${CONTAINER_ROLE:-datatracker}" in
     auth)
@@ -16,6 +16,9 @@ case "${CONTAINER_ROLE:-datatracker}" in
         ;;
     datatracker)
         exec ./datatracker-start.sh
+        ;;
+    flower)
+        exec ./celery-start.sh --app=ietf flower
         ;;
     migrations)
         exec ./migration-start.sh
