239b52d0aca7f27862c9d614a5502696a795a406
Jennifer Richards <jennifer@staff.ietf.org>
2025-03-05 18:48:46 -0400

    feat: admin for blob model

--- ietf/blobdb/admin.py
+++ ietf/blobdb/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.db.models.functions import Length
 
-# Register your models here.
+from .models import Blob
+
+
+@admin.register(Blob)
+class BlobAdmin(admin.ModelAdmin):
+    list_display = ["bucket", "name", "object_size", "checksum", "modified"]
+    list_display_links = ["name"]
+
+    def get_queryset(self, request):
+        return (
+            super().get_queryset(request)
+            .defer("content")  # don't load this unless we want it
+            .annotate(object_size=Length("content"))  # accessed via object_size()
+        )
+
+    def object_size(self, instance):
+        """Get the size of the object"""
+        return instance.object_size  # annotation added in get_queryset()
