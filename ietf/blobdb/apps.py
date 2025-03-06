a448c6c4ee830e394acb7e568775be572d34d4be
Jennifer Richards <jennifer@staff.ietf.org>
2025-03-05 21:46:02 -0400

    feat: database router

--- ietf/blobdb/apps.py
+++ ietf/blobdb/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class BlobdbConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ietf.blobdb"
+
+    def ready(self):
+        from django.conf import settings  # settings should be ready now
+        db = getattr(settings, "BLOBDB_DATABASE", None)
+        if db is not None and db not in settings.DATABASES:
+            raise RuntimeError(f"settings.BLOBDB_DATABASE is '{db}' but that is not present in settings.DATABASES")
