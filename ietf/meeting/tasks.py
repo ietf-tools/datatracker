8ba40bbc73ac4088ac0422a5858c469bc965a6a6
Ryan Cross <rcross@amsl.com>
2025-03-06 15:48:49 -0800

    fix: remove management command. add task

--- ietf/meeting/tasks.py
+++ ietf/meeting/tasks.py
@@ -5,8 +5,18 @@
 from celery import shared_task
 
 from .views import generate_agenda_data
+from .utils import migrate_registrations
 
 
 @shared_task
 def agenda_data_refresh():
     generate_agenda_data(force_refresh=True)
+
+
+@shared_task
+def migrate_registrations_task(initial=False):
+    """ Migrate ietf.stats.MeetingRegistration to ietf.meeting.Registration
+        If initial is True, migrate all meetings otherwise only future meetings.
+        This function is idempotent. It can be run regularly from cron.
+    """
+    migrate_registrations(initial=initial)
