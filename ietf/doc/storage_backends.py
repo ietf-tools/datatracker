27fb76481508748a5d96c05e172e471c87c3e39a
Jennifer Richards <jennifer@staff.ietf.org>
2025-03-06 21:32:55 -0400

    refactor: better separation of StorageObject and Storage
    Partial revert of 5d6bd49afd2d130fa687617e5d3efc002a4d96f3

--- ietf/doc/storage_backends.py
+++ ietf/doc/storage_backends.py
@@ -8,9 +8,8 @@ from contextlib import contextmanager
 from hashlib import sha384
 from io import BufferedReader
 from storages.backends.s3 import S3Storage
-from typing import Optional, Union
+from typing import Optional, Union, Protocol
 
-from django.conf import settings
 from django.core.files.base import File
 
 from ietf.doc.models import StoredObject
@@ -19,13 +18,32 @@ from ietf.utils.timezone import timezone
 
 
 class MetadataFile(File):
-    def __init__(self, name, file, allow_overwrite=False, doc_name=None, doc_rev=None):
+    def __init__(self, name, file, doc_name=None, doc_rev=None):
         super().__init__(file, name)
-        self.allow_overwrite = allow_overwrite
         self.doc_name = doc_name
         self.doc_rev = doc_rev
-        self._custom_metadata = {}
-        
+        self._custom_metadata = None 
+
+    @property
+    def custom_metadata(self):
+        if self._custom_metadata is None:
+            try:
+                self.file.seek(0)
+            except AttributeError:  # TODO-BLOBSTORE
+                debug.say("Encountered Non-Seekable content")
+                raise NotImplementedError("cannot handle unseekable content")
+            content_bytes = self.file.read()
+            if not isinstance(
+                content_bytes, bytes
+            ):  # TODO-BLOBSTORE: This is sketch-development only -remove before committing
+                raise Exception(f"Expected bytes - got {type(content_bytes)}")
+            self.file.seek(0)
+            self._custom_metadata = {
+                "len": f"{len(content_bytes)}",
+                "sha384": f"{sha384(content_bytes).hexdigest()}",
+            }
+        return self._custom_metadata
+
 
 @contextmanager
 def maybe_log_timing(enabled, op, **kwargs):
@@ -59,75 +77,8 @@ def maybe_log_timing(enabled, op, **kwargs):
 # TODO-BLOBSTORE
 # Consider overriding save directly so that
 # we capture metadata for, e.g., ImageField objects
-class StorageObjectStorageMixin:
+class StorageObjectStorageMixin: #(BucketStorageProtocol):
     commit_on_save = True  # if True, blobs are immediately treated as committed
-
-    def _save(self, name, content: File):
-        with maybe_log_timing(
-            self.ietf_log_blob_timing, "_save", bucket_name=self.bucket_name, name=name
-        ):
-            if not isinstance(content, MetadataFile):
-                raise NotImplementedError("Only handle MetadataFile so far")
-            is_new = not self.exists_in_storage(self.bucket_name, name)
-            # debug.show('f"Asked to store {name} in {self.bucket_name}: is_new={is_new}, allow_overwrite={content.allow_overwrite}"')
-            if not content.allow_overwrite and not is_new:
-                log(f"Failed to save {self.bucket_name}:{name} - name already exists in store")
-                debug.show('f"Failed to save {self.bucket_name}:{name} - name already exists in store"')
-                # raise Exception("Not ignoring overwrite attempts while testing")
-                return "--write-failed--"  # did not save, not sure what else to return
-            else:
-                try:
-                    new_name = super()._save(name, content)
-                    now = timezone.now()
-                    record, created = StoredObject.objects.get_or_create(
-                        store=self.bucket_name,
-                        name=name,
-                        defaults=dict(
-                            sha384=content._custom_metadata["sha384"],
-                            len=int(content._custom_metadata["len"]),
-                            store_created=now,
-                            created=now,
-                            modified=now,
-                            committed=now if self.commit_on_save else None,
-                            doc_name=content.doc_name,  # Note that these are assumed to be invariant
-                            doc_rev=content.doc_rev,  # for a given name
-                        ),
-                    )
-                    if not created:
-                        record.sha384 = content._custom_metadata["sha384"]
-                        record.len = int(content._custom_metadata["len"])
-                        record.modified = now
-                        record.deleted = None
-                        record.committed = None
-                        record.save()
-                    if new_name != name:
-                        complaint = f"Error encountered saving '{name}' - results stored in '{new_name}' instead."
-                        log(complaint)
-                        debug.show("complaint")
-                        # Note that we are otherwise ignoring this condition - it should become an error later.
-                except Exception as e:
-                    # Log and then swallow the exception while we're learning.
-                    # Don't let failure pass so quietly when these are the autoritative bits.
-                    complaint = f"Failed to save {self.bucket_name}:{name}"
-                    log(complaint, e)
-                    debug.show('f"{complaint}: {e}"')
-                return new_name
-
-    def _open(self, name, mode="rb"):
-        with maybe_log_timing(
-            self.ietf_log_blob_timing,
-            "_open",
-            bucket_name=self.bucket_name,
-            name=name,
-            mode=mode,
-        ):
-            return super()._open(name, mode)
-
-    def delete(self, name):
-        with maybe_log_timing(
-            self.ietf_log_blob_timing, "delete", bucket_name=self.bucket_name, name=name
-        ):
-            super().delete(name)
 
     def commit(self, name):
         StoredObject.objects.filter(
@@ -147,16 +98,54 @@ class StorageObjectStorageMixin:
     ):
         if kind != self.bucket_name:
             raise RuntimeError(f"Called store_file() for {kind} against the {self.bucket_name} Storage")
-        self.save(
-            name,
-            content=MetadataFile(
+        is_new = not self.exists_in_storage(kind, name)
+        # debug.show('f"Asked to store {name} in {kind}: is_new={is_new}, allow_overwrite={allow_overwrite}"')
+        if not allow_overwrite and not is_new:
+            log(f"Failed to save {kind}:{name} - name already exists in store")
+            debug.show('f"Failed to save {kind}:{name} - name already exists in store"')
+            # raise Exception("Not ignoring overwrite attempts while testing")
+        else:
+            content = MetadataFile(
                 name=name,
                 file=file,
-                allow_overwrite=allow_overwrite,
                 doc_name=doc_name,
                 doc_rev=doc_rev,
             )
-        )
+            try:
+                new_name = self.save(name, content)
+                now = timezone.now()
+                record, created = StoredObject.objects.get_or_create(
+                    store=kind,
+                    name=name,
+                    defaults=dict(
+                        sha384=content.custom_metadata["sha384"],
+                        len=int(content.custom_metadata["len"]),
+                        store_created=now,
+                        created=now,
+                        modified=now,
+                        committed=now if self.commit_on_save else None,
+                        doc_name=content.doc_name,  # Note that these are assumed to be invariant
+                        doc_rev=content.doc_rev,  # for a given name
+                    ),
+                )
+                if not created:
+                    record.sha384 = content.custom_metadata["sha384"]
+                    record.len = int(content.custom_metadata["len"])
+                    record.modified = now
+                    record.deleted = None
+                    record.committed = None
+                    record.save()
+                if new_name != name:
+                    complaint = f"Error encountered saving '{name}' - results stored in '{new_name}' instead."
+                    log(complaint)
+                    debug.show("complaint")
+                    # Note that we are otherwise ignoring this condition - it should become an error later.
+            except Exception as e:
+                # Log and then swallow the exception while we're learning.
+                # Don't let failure pass so quietly when these are the autoritative bits.
+                complaint = f"Failed to save {kind}:{name}"
+                log(complaint, e)
+                debug.show('f"{complaint}: {e}"')
 
     def exists_in_storage(self, kind: str, name: str) -> bool:
         try:
@@ -192,6 +181,36 @@ class StorageObjectStorageMixin:
             # Note that existing_record is a queryset that will have one matching object
             existing_record.filter(deleted__isnull=True).update(deleted=now)
 
+
+class CustomS3Storage(StorageObjectStorageMixin, S3Storage):
+    def get_default_settings(self):
+        # add a default for the ietf_log_blob_timing boolean
+        return super().get_default_settings() | {"ietf_log_blob_timing": False}
+
+    def _save(self, name, content: File):
+        with maybe_log_timing(
+            self.ietf_log_blob_timing, "_save", bucket_name=self.bucket_name, name=name
+        ):
+            if not isinstance(content, MetadataFile):
+                raise NotImplementedError("Only handle MetadataFile so far")
+                return new_name
+
+    def _open(self, name, mode="rb"):
+        with maybe_log_timing(
+            self.ietf_log_blob_timing,
+            "_open",
+            bucket_name=self.bucket_name,
+            name=name,
+            mode=mode,
+        ):
+            return super()._open(name, mode)
+
+    def delete(self, name):
+        with maybe_log_timing(
+            self.ietf_log_blob_timing, "delete", bucket_name=self.bucket_name, name=name
+        ):
+            super().delete(name)
+
     def _get_write_parameters(self, name, content=None):
         # debug.show('f"getting write parameters for {name}"')
         params = super()._get_write_parameters(name, content)
@@ -199,36 +218,12 @@ class StorageObjectStorageMixin:
             params["Metadata"] = {}
         if not isinstance(content, MetadataFile):
             raise NotImplementedError("Can only handle content of type MetadataFile")
-        try:
-            content.seek(0)
-        except AttributeError:  # TODO-BLOBSTORE
-            debug.say("Encountered Non-Seekable content")
-            raise NotImplementedError("cannot handle unseekable content")
-        content_bytes = content.read()
-        if not isinstance(
-            content_bytes, bytes
-        ):  # TODO-BLOBSTORE: This is sketch-development only -remove before committing
-            raise Exception(f"Expected bytes - got {type(content_bytes)}")
-        content.seek(0)
-        metadata = {
-            "len": f"{len(content_bytes)}",
-            "sha384": f"{sha384(content_bytes).hexdigest()}",
-        }
-        params["Metadata"].update(metadata)
-        content._custom_metadata = metadata
+        params["Metadata"].update(content.custom_metadata)
         return params
-
-
-class CustomS3Storage(StorageObjectStorageMixin, S3Storage):
-    def get_default_settings(self):
-        # add a default for the ietf_log_blob_timing boolean
-        return super().get_default_settings() | {"ietf_log_blob_timing": False}
 
 
 class StagedBlobStorage(Storage):
     """Storage using an intermediate staging step"""
-    commit_on_save = False  # files not committed until they're moved to the final_storage
-    ietf_log_blob_timing = True
 
     def __init__(self, staging_storage: Union[str, Storage], final_storage: Union[str, Storage]):
         self._staging_storage = staging_storage
@@ -250,6 +245,13 @@ class StagedBlobStorage(Storage):
     def bucket_name(self):
         return self.final_storage.bucket_name
 
+    def _open(self, name, mode="rb"):
+        try:
+            return self.staging_storage.open(name, mode)
+        except FileNotFoundError:
+            pass
+        return self.final_storage.open(name, mode)
+
     def _save(self, name, content):
         return self.staging_storage.save(name, content)
 
@@ -258,6 +260,9 @@ class StagedBlobStorage(Storage):
 
 
 class StorageObjectStagedBlogStorage(StorageObjectStorageMixin, StagedBlobStorage):
+    commit_on_save = False  # files not committed until they're moved to the final_storage
+    ietf_log_blob_timing = True
+
     def commit(self, name):
         with self.staging_storage.open(name) as staged:
             self.final_storage.save(
