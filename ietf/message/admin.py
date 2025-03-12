a53575594d8a8895c00995c5a51fa95b102d4cfd
Jennifer Richards <jennifer@staff.ietf.org>
2025-03-12 12:35:31 -0700

    feat: admin action to queue redelivery

--- ietf/message/admin.py
+++ ietf/message/admin.py
@@ -1,15 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models import QuerySet
 
 from ietf.message.models import Message, MessageAttachment, SendQueue, AnnouncementFrom
+from ietf.message.tasks import retry_send_messages_by_pk_task
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = ["subject", "by", "time", "groups"]
     search_fields = ["subject", "body"]
     raw_id_fields = ["by", "related_groups", "related_docs"]
     ordering = ["-time"]
+    actions = ["retry_send"]
 
     def groups(self, instance):
         return ", ".join(g.acronym for g in instance.related_groups.all())
+
+    @admin.action(description="Send selected messages if unsent")
+    def retry_send(self, request, queryset: QuerySet[Message]):
+        retry_send_messages_by_pk_task.delay(
+            message_pks=list(queryset.values_list("pk", flat=True)),
+            resend=False,
+        )
+        self.message_user(
+            request,
+            "Messages queued for delivery",
+            messages.SUCCESS)
 admin.site.register(Message, MessageAdmin)
 
 class MessageAttachmentAdmin(admin.ModelAdmin):
