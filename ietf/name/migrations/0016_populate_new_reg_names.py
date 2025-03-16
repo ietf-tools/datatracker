5fcc1bbf4ff7b712296de377a111e242c63db5cb
Ryan Cross <rcross@amsl.com>
2025-03-16 15:42:00 +0700

    fix: merge migrations

--- ietf/name/migrations/0016_populate_new_reg_names.py
+++ ietf/name/migrations/0016_populate_new_reg_names.py
@@ -10,6 +10,7 @@ def forward(apps, schema_editor):
     AttendanceTypeName.objects.create(slug='hackathon_onsite', name='Hackathon Onsite')
     AttendanceTypeName.objects.create(slug='hackathon_remote', name='Hackathon Remote')
     AttendanceTypeName.objects.create(slug='anrw_onsite', name='ANRW Onsite')
+    AttendanceTypeName.objects.create(slug='unknown', name='Unknown')
     RegistrationTicketTypeName.objects.create(slug='week_pass', name='Week Pass')
     RegistrationTicketTypeName.objects.create(slug='one_day', name='One Day')
     RegistrationTicketTypeName.objects.create(slug='student', name='Student')
@@ -17,6 +18,7 @@ def forward(apps, schema_editor):
     RegistrationTicketTypeName.objects.create(slug='hackathon_combo', name='Hackathon Combo')
     RegistrationTicketTypeName.objects.create(slug='anrw_only', name='ANRW Only')
     RegistrationTicketTypeName.objects.create(slug='anrw_combo', name='ANRW Combo')
+    RegistrationTicketTypeName.objects.create(slug='unknown', name='Unknown')
 
 
 def reverse(apps, schema_editor):
