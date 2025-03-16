25ffdd7081c08ad122487a50cbe7978b66bc1c94
Robert Sparks <rjsparks@nostrum.com>
2025-03-16 12:55:35 +0700

    fix: show title and add test

--- ietf/doc/views_review.py
+++ ietf/doc/views_review.py
@@ -911,7 +911,6 @@ def complete_review(request, name, assignment_id=None, acronym=None):
                         body = render_to_string("review/completed_review.txt", {
                             "assignment": assignment,
                             "content": content,
-                            "draftname": name,
                         }),
                     )
                 msg.related_groups.add(*related_groups)
