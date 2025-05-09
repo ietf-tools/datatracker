098392cddafcdb403a06011a4f658dddcb277a9e
Jennifer Richards <jennifer@staff.ietf.org>
2025-05-09 18:08:20 -0300

    refactor: remove StagedBlobStorage

--- docker/configs/settings_local.py
+++ docker/configs/settings_local.py
@@ -43,32 +43,8 @@ INTERNAL_IPS = [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips] + ['127.0.0.
 # ]
 for storagename in MORE_STORAGE_NAMES:
     STORAGES[storagename] = {
-        "BACKEND": "ietf.doc.storage.StoredObjectStagedBlobStorage",
-        "OPTIONS": {
-            "kind": storagename,
-            "async_commit": True,  # TODO-BLOBSTORE should probably be false for Celery containers
-            "staging_storage": {
-                "BACKEND": "ietf.blobdb.storage.BlobdbStorage",
-                "OPTIONS": {"bucket_name": storagename},
-            },
-            "final_storage": {
-                "BACKEND": "ietf.doc.storage.MetadataS3Storage",
-                "OPTIONS": dict(
-                    endpoint_url="http://blobstore:9000",
-                    access_key="minio_root",
-                    secret_key="minio_pass",
-                    security_token=None,
-                    client_config=botocore.config.Config(
-                        signature_version="s3v4",
-                        connect_timeout=BLOBSTORAGE_CONNECT_TIMEOUT,
-                        read_timeout=BLOBSTORAGE_READ_TIMEOUT,
-                        retries={"total_max_attempts": BLOBSTORAGE_MAX_ATTEMPTS},
-                    ),
-                    verify=False,
-                    bucket_name=storagename,
-                ),
-            },
-        },
+        "BACKEND": "ietf.doc.storage.StoredObjectBlobdbStorage",
+        "OPTIONS": {"bucket_name": storagename},
     }
 
 
--- ietf/settings_test.py
+++ ietf/settings_test.py
@@ -49,8 +49,9 @@ DATABASES = {
         },
     }
 
-DATABASE_ROUTERS = []  # test with a single DB - do not use a DB router
-
+# test with a single DB - do not use a DB router
+BLOBDB_DATABASE = "default"
+DATABASE_ROUTERS = []
 
 if TEST_CODE_COVERAGE_CHECKER and not TEST_CODE_COVERAGE_CHECKER._started: # pyflakes:ignore
     TEST_CODE_COVERAGE_CHECKER.start()                          # pyflakes:ignore
@@ -120,31 +121,6 @@ _blob_store_enable_profiling = (
 )
 for storagename in MORE_STORAGE_NAMES:
     STORAGES[storagename] = {
-        "BACKEND": "ietf.doc.storage.StoredObjectStagedBlobStorage",
-        "OPTIONS": {
-            "kind": storagename,
-            "async_commit": False,  # write immediately to the final_storage
-            "staging_storage": {
-                "BACKEND": "ietf.blobdb.storage.BlobdbStorage",
-                "OPTIONS": {"bucket_name": storagename},
-            },
-            "final_storage": {
-                "BACKEND": "ietf.doc.storage.MetadataS3Storage",
-                "OPTIONS": dict(
-                    endpoint_url=_blob_store_endpoint_url,
-                    access_key=_blob_store_access_key,
-                    secret_key=_blob_store_secret_key,
-                    security_token=None,
-                    client_config=botocore.config.Config(
-                        signature_version="s3v4",
-                        connect_timeout=BLOBSTORAGE_CONNECT_TIMEOUT,
-                        read_timeout=BLOBSTORAGE_READ_TIMEOUT,
-                        retries={"total_max_attempts": BLOBSTORAGE_MAX_ATTEMPTS},
-                    ),
-                    verify=False,
-                    bucket_name=f"{_blob_store_bucket_prefix}{storagename}",
-                    ietf_log_blob_timing=_blob_store_enable_profiling,
-                ),
-            },
-        },
+        "BACKEND": "ietf.doc.storage.StoredObjectBlobdbStorage",
+        "OPTIONS": {"bucket_name": storagename},
     }
