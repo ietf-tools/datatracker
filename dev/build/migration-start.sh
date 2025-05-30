fc3ffaf11892d7dca928c6339a922e83768619ac
Jennifer Richards <jennifer@staff.ietf.org>
2025-05-30 16:33:55 -0300

    chore: blobdb migrations in k8s

--- dev/build/migration-start.sh
+++ dev/build/migration-start.sh
@@ -3,4 +3,7 @@
 echo "Running Datatracker migrations..."
 ./ietf/manage.py migrate --settings=settings_local
 
+echo "Running Blobdb migrations ..."
+./ietf/manage.py migrate --settings=settings_local --database=blobdb
+
 echo "Done!"
