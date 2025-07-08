70f093dfbe9303177293954f9b8aabda61cb9f66
Jennifer Richards <jennifer@staff.ietf.org>
2025-07-08 15:54:22 -0300

    refactor: reimplement purple API in django-rest-framework (#9097)
    * refactor: rpc_person -> PersonViewSet
    
    * refactor: rpc_subject_person -> SubjectPersonView
    
    * refactor: rpc_persons -> RpcPersonsView
    
    * refactor: move get_persons into PersonViewSet
    
    Changes the interface to return a list of Persons instead of a map from ID to name.
    
    * refactor: rpc_draft -> DraftViewSet
    
    * refactor: drafts_by_names -> DraftsByNameView
    
    * refactor: submitted_to_rpc -> DraftViewSet
    
    * refactor: rfc_original_stream -> RfcViewSet
    
    * refactor: rpc demo APIs -> viewset
    
    * refactor: get_draft_refs -> DraftViewSet
    
    * refactor: persons_by_email -> PersonViewSet
    
    * refactor: rfc_authors -> RfcViewSet
    
    * refactor: draft_authors -> DraftViewSet
    
    * refactor: avoid \x00 in regex validator
    
    Gets turned into a literal nul somewhere in the process of generating a schema and building a Python client for purple. This has the same effect but avoids the nul.
    
    * fix: missing arg on references() action
    
    * style: ruff, remove unused imports
    
    * style: ruff ruff
    
    * chore: remove rpcapi.yaml
    
    * refactor: move API to /api/purple
    
    Side effect is that the purple API client is named PurpleApi instead of RpcApi.
    
    * fix: get_draft_authors returns DraftWithAuthors
    
    * fix: distinguish CharField flavors
    
    * fix: no serializer validators for draft name/title
    
    This prevents at least one existing draft from being looked up.
    
    * fix: get_draft_authors works with str, not int
    
    * Revert "refactor: avoid \x00 in regex validator"
    
    This reverts commit 63f40cf2
    
    * Revert "Revert "refactor: avoid \x00 in regex validator"" (#9111)
    
    This reverts commit d8656f470045c21542824d7b9b9be41bdcb8866d.

--- ietf/api/urls_rpc.py
+++ ietf/api/urls_rpc.py
@@ -1,30 +1,33 @@
-# Copyright The IETF Trust 2023-2024, All Rights Reserved
+# Copyright The IETF Trust 2023-2025, All Rights Reserved
+
+from rest_framework import routers
 
 from django.conf import settings
+from django.urls import include, path
 
 from ietf.api import views_rpc, views_rpc_demo
 from ietf.utils.urls import url
 
-urlpatterns = [
-    url(r"^doc/drafts/(?P<doc_id>[0-9]+)/$", views_rpc.rpc_draft),
-    url(r"^doc/drafts/(?P<doc_id>[0-9]+)/references/$", views_rpc.rpc_draft_refs),
-    url(r"^doc/drafts_by_names/$", views_rpc.drafts_by_names),
-    url(r"^doc/submitted_to_rpc/$", views_rpc.submitted_to_rpc),
-    url(r"^doc/rfc/original_stream/$", views_rpc.rfc_original_stream),
-    url(r"^doc/rfc/authors/$", views_rpc.rfc_authors),
-    url(r"^doc/draft/authors/$", views_rpc.draft_authors),
-    url(r"^person/persons_by_email/$", views_rpc.persons_by_email),
-    url(r"^person/(?P<person_id>[0-9]+)/$", views_rpc.rpc_person),
-    url(r"^persons/$", views_rpc.rpc_persons),
-    url(r"^persons/search/", views_rpc.RpcPersonSearch.as_view()),
-    url(r"^subject/(?P<subject_id>[0-9]+)/person/$", views_rpc.rpc_subject_person),
-]
+router = routers.DefaultRouter()
+router.register(r"draft", views_rpc.DraftViewSet, basename="draft")
+router.register(r"person", views_rpc.PersonViewSet)
+router.register(r"rfc", views_rpc.RfcViewSet, basename="rfc")
 
 if settings.SERVER_MODE not in {"production", "test"}:
     # for non production demos
-    urlpatterns.append(
-        url(r"^doc/create_demo_draft/$", views_rpc_demo.create_demo_draft)
-    )
-    urlpatterns.append(
-        url(r"^person/create_demo_person/$", views_rpc_demo.create_demo_person)
-    )
+    router.register(r"demo", views_rpc_demo.DemoViewSet, basename="demo")
+
+
+urlpatterns = [
+    url(r"^doc/drafts_by_names/", views_rpc.DraftsByNamesView.as_view()),
+    url(r"^persons/search/", views_rpc.RpcPersonSearch.as_view()),
+    path(r"subject/<str:subject_id>/person/", views_rpc.SubjectPersonView.as_view()),
+]
+
+# add routers at the end so individual routes can steal parts of their address
+# space (specifically, ^person/ routes so far)
+urlpatterns.extend(
+    [
+        path("", include(router.urls)),
+    ]
+)
