26327338bc5b7f921a908887f2019ae1530ad312
Jennifer Richards <jennifer@staff.ietf.org>
2025-08-06 14:22:03 -0300

    fix: avoid limit on a queryset delete

--- ietf/ietfauth/admin.py
+++ ietf/ietfauth/admin.py
@@ -28,10 +28,12 @@ class CustomUserAdmin(UserAdmin):
         as default_delete_selected()) without having to adjust its template (and maybe
         other things) to make it work with a different action name.
         """
+        already_confirmed = bool(request.POST.get("post"))
         personless_queryset = queryset.filter(person__isnull=True)
         original_count = queryset.count()
         personless_count = personless_queryset.count()
         if personless_count > original_count:
+            # Refuse to act if the count increased!
             self.message_user(
                 request,
                 (
@@ -42,29 +44,32 @@ class CustomUserAdmin(UserAdmin):
                 ),
                 level=messages.ERROR,
             )
-            return
-        elif personless_count < original_count:
-            self.message_user(
-                request,
-                (
-                    "Limiting the selection to Users without a Person reduced the "
-                    "count from {} to {}. Only {} will be deleted.".format(
-                        original_count, personless_count, personless_count
-                    )
-                ),
-                level=messages.WARNING,
-            )
-        else:
-            self.message_user(
-                request, "Confirmed that all selected Users had no Persons."
-            )
+            return None  # return to changelist
+
+        # Display warning/info if this is showing the confirmation page
+        if not already_confirmed:
+            if personless_count < original_count:
+                self.message_user(
+                    request,
+                    (
+                        "Limiting the selection to Users without a Person reduced the "
+                        "count from {} to {}. Only {} will be deleted.".format(
+                            original_count, personless_count, personless_count
+                        )
+                    ),
+                    level=messages.WARNING,
+                )
+            else:
+                self.message_user(
+                    request,
+                    "Confirmed that all selected Users had no Persons.",
+                )
 
         # Django limits the number of fields in a request. The delete form itself
         # includes a few metadata fields, so give it a little padding. The default
         # limit is 1000 and everything will break if it's a small number, so not
         # bothering to check that it's > 10.
         max_count = settings.DATA_UPLOAD_MAX_NUMBER_FIELDS - 10
-        personless_queryset = personless_queryset.order_by("pk")[:max_count]
         if personless_count > max_count:
             self.message_user(
                 request,
@@ -74,4 +79,19 @@ class CustomUserAdmin(UserAdmin):
                 ),
                 level=messages.WARNING,
             )
+            # delete() doesn't like a queryset limited via [:max_count], so do an
+            # equivalent filter.
+            last_to_delete = personless_queryset.order_by("pk")[max_count]
+            personless_queryset = personless_queryset.filter(pk__lt=last_to_delete.pk)
+
+        if already_confirmed and personless_count != original_count:
+            # After confirmation, none of the above filtering should change anything.
+            # Refuse to delete if the DB moved underneath us.
+            self.message_user(
+                request,
+                "Queryset count changed, nothing deleted. Please try again.",
+                level=messages.ERROR,
+            )
+            return None
+
         return default_delete_selected(self, request, personless_queryset)
