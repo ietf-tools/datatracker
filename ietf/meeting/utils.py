6772ed0e80c1843e8ffe10531636fdc342f4f591
Jennifer Richards <jennifer@staff.ietf.org>
2025-10-21 23:50:10 -0300

    fix: don't use DocHistory to validate revs
    The DocHistory records are incomplete and, in particular, -00 revs are
    often missing.

--- ietf/meeting/utils.py
+++ ietf/meeting/utils.py
@@ -861,7 +861,7 @@ class BlobSpec:
 
 
 def resolve_one_material(
-    doc: Document | DocHistory, rev: str | None, ext: str | None
+    doc: Document, rev: str | None, ext: str | None
 ) -> BlobSpec | None:
     if doc.type_id is None:
         log(f"Cannot resolve a doc with no type: {doc.name}")
@@ -995,12 +995,7 @@ def resolve_materials_for_one_meeting(meeting: Meeting):
         other_revisions = doc.revisions_by_newrevisionevent()
         other_revisions.remove(doc.rev)
         for rev in other_revisions:
-            old_doc = DocHistory.objects.filter(
-                doc=doc, rev=rev
-            ).order_by("-time").first()
-            if old_doc is None:
-                continue
-            blob = resolve_one_material(old_doc, rev=rev, ext=None)
+            blob = resolve_one_material(doc, rev=rev, ext=None)
             if blob is not None:
                 resolved.append(
                     ResolvedMaterial(
