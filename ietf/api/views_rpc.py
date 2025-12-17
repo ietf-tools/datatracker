166df5e4b0db69274d0e991148a2a43ba3c403ea
Jennifer Richards <jennifer@staff.ietf.org>
2025-12-17 12:18:25 -0400

    refactor: _destination() helper

--- ietf/api/views_rpc.py
+++ ietf/api/views_rpc.py
@@ -367,6 +367,15 @@ class RfcPubFilesView(APIView):
     api_key_endpoint = "ietf.api.views_rpc"
     parser_classes = [parsers.MultiPartParser]
 
+    def _destination(self, filename: str | Path) -> Path:
+        """Destination for an uploaded RFC file
+        
+        Strips any path components in filename and returns an absolute Path.
+        """
+        rfc_path = Path(settings.RFC_PATH)
+        filename = Path(filename)  # could potentially have directory components
+        return rfc_path / filename.name
+
     @extend_schema(
         operation_id="upload_rfc_files",
         summary="Upload files for a published RFC",
@@ -383,11 +392,10 @@ class RfcPubFilesView(APIView):
         uploaded_files: list[UploadedFile] = serializer.validated_data["contents"]
         replace = serializer.validated_data["replace"]
         dest_stem = f"rfc{rfc.rfc_number}"
-        dest_path = Path(settings.RFC_PATH)
 
         # List of files that might exist for an RFC
         possible_rfc_files = [
-            (dest_path / dest_stem).with_suffix(ext)
+            self._destination(dest_stem + ext)
             for ext in serializer.allowed_extensions
         ]
         if not replace:
@@ -400,23 +408,24 @@ class RfcPubFilesView(APIView):
                     )
 
         with TemporaryDirectory() as tempdir:
-            # save files with desired names in a temporary directory
+            # Save files in a temporary directory. Use the uploaded filename
+            # extensions to identify files, but ignore the stems and generate our own.
             files_to_move: list[Path] = []
             tmpfile_stem = Path(tempdir) / dest_stem
             for upfile in uploaded_files:
-                uploaded_filename = Path(upfile.name)
+                uploaded_filename = Path(upfile.name)  # name supplied by request
                 uploaded_ext = "".join(uploaded_filename.suffixes)
-                dest_filename = tmpfile_stem.with_suffix(uploaded_ext)
-                with dest_filename.open("wb") as dest:
+                tempfile_path = tmpfile_stem.with_suffix(uploaded_ext)
+                with tempfile_path.open("wb") as dest:
                     for chunk in upfile.chunks():
                         dest.write(chunk)
-                files_to_move.append(dest_filename)
+                files_to_move.append(tempfile_path)
             # copy files to final location, removing any existing ones first if the
             # remove flag was set
             if replace:
                 for possible_existing_file in possible_rfc_files:
                     possible_existing_file.unlink(missing_ok=True)
             for ftm in files_to_move:
-                shutil.move(ftm, dest_path)
+                shutil.move(ftm, self._destination(ftm))
 
         return Response(NotificationAckSerializer().data)
