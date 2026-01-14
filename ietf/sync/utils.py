eb9e458fd6e7d08add33c5af189a8942224dfddf
Robert Sparks <rjsparks@nostrum.com>
2026-01-14 10:38:07 -0600

    fix: use list for typing

--- ietf/sync/utils.py
+++ ietf/sync/utils.py
@@ -1,7 +1,7 @@
 # Copyright The IETF Trust 2026, All Rights Reserved
 
 import subprocess
-from typing import List
 
-def rsync_helper(subprocess_arg_array:List[str]):
+
+def rsync_helper(subprocess_arg_array: list[str]):
     subprocess.run(subprocess_arg_array)
