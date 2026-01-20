c310d6101d7d44525753606d8128d5e2358f1468
Robert Sparks <rjsparks@nostrum.com>
2026-01-20 12:23:00 -0600

    fix: restrict rfc bulkload to those with a Document object

--- ietf/sync/utils.py
+++ ietf/sync/utils.py
@@ -6,8 +6,8 @@ import subprocess
 from pathlib import Path
 
 from django.conf import settings
-
 from ietf.utils import log
+from ietf.doc.models import Document
 from ietf.doc.storage_utils import AlreadyExistsError, store_bytes
 
 
@@ -26,17 +26,40 @@ def build_from_file_content(rfc_numbers: list[int]) -> str:
 
 def load_rfcs_into_blobdb(numbers: list[int]):
     types_to_load = settings.RFC_FILE_TYPES + ("json",)
+    rfc_docs = Document.objects.filter(type="rfc",rfc_number__in=numbers)
     for num in numbers:
-        for ext in types_to_load:
-            fs_path = Path(settings.RFC_PATH) / f"rfc{num}.{ext}"
-            if fs_path.is_file():
-                with fs_path.open("rb") as f:
+        if rfc_docs.filter(rfc_number=num).exists():
+            for ext in types_to_load:
+                fs_path = Path(settings.RFC_PATH) / f"rfc{num}.{ext}"
+                if fs_path.is_file():
+                    with fs_path.open("rb") as f:
+                        bytes = f.read()
+                    mtime = fs_path.stat().st_mtime
+                    try:
+                        store_bytes(
+                            kind="rfc",
+                            name=f"{ext}/rfc{num}.{ext}",
+                            content=bytes,
+                            allow_overwrite=False,  # Intentionally not allowing overwrite.
+                            doc_name=f"rfc{num}",
+                            doc_rev=None,
+                            # Not setting content_type
+                            mtime=datetime.datetime.fromtimestamp(mtime, tz=datetime.UTC),
+                        )
+                    except AlreadyExistsError as e:
+                        log.log(str(e))
+
+            # store the not-prepped xml
+            name = f"rfc{num}.notprepped.xml"
+            source = Path(settings.RFC_PATH) / "prerelease" / name
+            if source.is_file():
+                with open(source, "rb") as f:
                     bytes = f.read()
-                mtime = fs_path.stat().st_mtime
+                mtime = source.stat().st_mtime
                 try:
                     store_bytes(
                         kind="rfc",
-                        name=f"{ext}/rfc{num}.{ext}",
+                        name=f"notprepped/{name}",
                         content=bytes,
                         allow_overwrite=False,  # Intentionally not allowing overwrite.
                         doc_name=f"rfc{num}",
@@ -46,24 +69,5 @@ def load_rfcs_into_blobdb(numbers: list[int]):
                     )
                 except AlreadyExistsError as e:
                     log.log(str(e))
-
-        # store the not-prepped xml
-        name = f"rfc{num}.notprepped.xml"
-        source = Path(settings.RFC_PATH) / "prerelease" / name
-        if source.is_file():
-            with open(source, "rb") as f:
-                bytes = f.read()
-            mtime = source.stat().st_mtime
-            try:
-                store_bytes(
-                    kind="rfc",
-                    name=f"notprepped/{name}",
-                    content=bytes,
-                    allow_overwrite=False,  # Intentionally not allowing overwrite.
-                    doc_name=f"rfc{num}",
-                    doc_rev=None,
-                    # Not setting content_type
-                    mtime=datetime.datetime.fromtimestamp(mtime, tz=datetime.UTC),
-                )
-            except AlreadyExistsError as e:
-                log.log(str(e))
+        else:
+            log.log(f"Skipping loading rfc{num} into blobdb as no matching Document exists")
