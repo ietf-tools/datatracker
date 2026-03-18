249c3c43fcb5245c6efa66afb2aeb3eaea361f36
Robert Sparks <rjsparks@nostrum.com>
2026-03-18 15:56:38 -0500

    chore: remove old comment, simplify request call

--- ietf/doc/utils_red.py
+++ ietf/doc/utils_red.py
@@ -8,10 +8,8 @@ from ietf.utils.log import log
 
 
 def trigger_red_precomputer(rfc_number_list=()):
-    # TODO load the following into settings from the env
     url = getattr(settings, "TRIGGER_RED_PRECOMPUTE_MULTIPLE_URL", None)
     if url is not None:
-        headers = {"Content-Type": "application/json"}
         payload = {
             "rfcs": ",".join([str(n) for n in rfc_number_list]),
         }
@@ -21,7 +19,6 @@ def trigger_red_precomputer(rfc_number_list=()):
                 url,
                 json=payload,
                 timeout=settings.DEFAULT_REQUESTS_TIMEOUT,
-                headers=headers,
             )
         except requests.Timeout as e:
             log(f"POST request timed out for {url} ]: {e}")
