821a23410e4d0fd383875a46441a5c0fd0f885e3
Jennifer Richards <jennifer@staff.ietf.org>
2026-03-20 03:05:41 -0300

    fix: roll back accidental commit

--- ietf/settings.py
+++ ietf/settings.py
@@ -1511,7 +1511,7 @@ if SERVER_MODE != 'production':
     if 'NOMCOM_APP_SECRET' not in locals():
         NOMCOM_APP_SECRET = b'\x9b\xdas1\xec\xd5\xa0SI~\xcb\xd4\xf5t\x99\xc4i\xd7\x9f\x0b\xa9\xe8\xfeY\x80$\x1e\x12tN:\x84'
 
-    ALLOWED_HOSTS = ['borkbork.org',]
+    ALLOWED_HOSTS = ['*',]
 
     try:
         # see https://github.com/omarish/django-cprofile-middleware
