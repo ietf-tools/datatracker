983f7aba3f08c5a37911f373c960b47e1e5de5cd
Robert Sparks <rjsparks@nostrum.com>
2026-03-23 10:21:58 -0500

    fix: improve red utils, test red and r2 utils

--- ietf/doc/utils_red.py
+++ ietf/doc/utils_red.py
@@ -14,18 +14,18 @@ def trigger_red_precomputer(rfc_number_list=()):
             "rfcs": ",".join([str(n) for n in rfc_number_list]),
         }
         try:
-            log.log(f"Triggering red precompute multiple for RFCs {rfc_number_list}")
+            log(f"Triggering red precompute multiple for RFCs {rfc_number_list}")
             response = requests.post(
-                url,
+                url=url,
                 json=payload,
                 timeout=settings.DEFAULT_REQUESTS_TIMEOUT,
             )
         except requests.Timeout as e:
-            log(f"POST request timed out for {url} ]: {e}")
+            log(f"POST request timed out for {url} : {e}")
             return
         if response.status_code != 200:
             log(
-                f"POST request failed for {url} ]: {response.status_code} {response.text}"
+                f"POST request failed for {url} : status_code={response.status_code}"
             )
     else:
         log("No URL configured for triggering red precompute multiple, skipping")
