c956a05c649d3db4104c64d9a9239f961184e476
Jennifer Richards <jennifer@staff.ietf.org>
2026-04-13 21:57:45 -0300

    style: ruff + modernize utils/admin.py

--- ietf/utils/admin.py
+++ ietf/utils/admin.py
@@ -1,14 +1,15 @@
 # Copyright The IETF Trust 2011-2026, All Rights Reserved
-# -*- coding: utf-8 -*-
 
 
 from django.contrib import admin
 from django.utils.encoding import force_str
+from .models import DumpInfo, DirtyBits
+
 
 def name(obj):
-    if hasattr(obj, 'abbrev'):
+    if hasattr(obj, "abbrev"):
         return obj.abbrev()
-    elif hasattr(obj, 'name'):
+    elif hasattr(obj, "name"):
         if callable(obj.name):
             name = obj.name()
         else:
@@ -16,12 +17,14 @@ def name(obj):
         if name:
             return name
     return str(obj)
-    
+
+
 def admin_link(field, label=None, ordering="", display=name, suffix=""):
     if not label:
         label = field.capitalize().replace("_", " ").strip()
     if ordering == "":
         ordering = field
+
     def _link(self):
         obj = self
         for attr in field.split("__"):
@@ -33,19 +36,28 @@ def admin_link(field, label=None, ordering="", display=name, suffix=""):
         elif callable(obj):
             objects = obj()
             if not hasattr(objects, "__iter__"):
-                objects = [ objects ]
+                objects = [objects]
         elif hasattr(obj, "__iter__"):
             objects = obj
         else:
-            objects = [ obj ]
+            objects = [obj]
         chunks = []
         for obj in objects:
             app = obj._meta.app_label
             model = obj.__class__.__name__.lower()
             id = obj.pk
-            chunks += [ '<a href="/admin/%(app)s/%(model)s/%(id)s/%(suffix)s">%(display)s</a>' %
-                {'app':app, "model": model, "id":id, "display": display(obj), "suffix":suffix, } ]
+            chunks += [
+                '<a href="/admin/%(app)s/%(model)s/%(id)s/%(suffix)s">%(display)s</a>'
+                % {
+                    "app": app,
+                    "model": model,
+                    "id": id,
+                    "display": display(obj),
+                    "suffix": suffix,
+                }
+            ]
         return ", ".join(chunks)
+
     _link.allow_tags = True
     _link.short_description = label
     _link.admin_order_field = ordering
@@ -54,21 +66,22 @@ def admin_link(field, label=None, ordering="", display=name, suffix=""):
 
 class SaferStackedInline(admin.StackedInline):
     """StackedInline without delete by default"""
+
     can_delete = False  # no delete button
     show_change_link = True  # show a link to the resource (where it can be deleted)
 
 
 class SaferTabularInline(admin.TabularInline):
     """TabularInline without delete by default"""
+
     can_delete = False  # no delete button
     show_change_link = True  # show a link to the resource (where it can be deleted)
 
 
-from .models import DumpInfo, DirtyBits
+@admin.register(DumpInfo)
 class DumpInfoAdmin(admin.ModelAdmin):
-    list_display = ['date', 'host', 'tz']
-    list_filter = ['date']
-admin.site.register(DumpInfo, DumpInfoAdmin)
+    list_display = ["date", "host", "tz"]
+    list_filter = ["date"]
 
 
 @admin.register(DirtyBits)
