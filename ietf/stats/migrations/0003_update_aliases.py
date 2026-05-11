6346066e0e6a1b58454b4e6ddfa8ea93734c247a
Eric Vyncke <eric@vyncke.org>
2026-05-11 12:02:27 +0000

    ID field is automatic

--- ietf/stats/migrations/0003_update_aliases.py
+++ ietf/stats/migrations/0003_update_aliases.py
@@ -121,7 +121,6 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='AffiliationMainName',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('main_name', models.CharField(max_length=255, unique=True)),
             ],
             options={
