6d08e3d1e57a5f10573c5e601873a476e421c991
Jennifer Richards <jennifer@staff.ietf.org>
2026-09-23 11:08:30 -0300

    feat: expose group state through red "doc" API (#11813)
    * feat: expose group state through red "doc" API
    
    * fix: simpler, working API
    
    Using a QuerySet to build choices does not work with tests, so
    simplifying to a fixed set of state values.

--- ietf/group/serializers.py
+++ ietf/group/serializers.py
@@ -4,14 +4,40 @@
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
+from ietf.name.models import GroupStateName
 from ietf.person.models import Email
 from .models import Group, Role
 
 
+class GroupStateField(serializers.ChoiceField):
+    """API representation of group state
+
+    Only represents "active" and "conclude". The "bof-conc" state is treated as
+    "conclude". Any other state is represented as "other".
+    """
+
+    def __init__(self, **kwargs):
+        if not kwargs.pop("read_only", True):
+            raise RuntimeError("GroupStateField is read-only")
+        choices = ("active", "conclude", "other")
+        super().__init__(choices, read_only=True, **kwargs)
+
+    def to_representation(self, value):
+        if not isinstance(value, GroupStateName):
+            raise ValueError(f"value is a {type(value)}, not a GroupStateName")
+        return {
+            "active": "active",
+            "bof-conc": "conclude",
+            "conclude": "conclude",
+        }.get(value.slug, "other")
+
+
 class GroupSerializer(serializers.ModelSerializer):
+    state = GroupStateField(allow_null=True)
+
     class Meta:
         model = Group
-        fields = ["acronym", "name", "type", "list_email"]
+        fields = ["acronym", "name", "type", "state", "list_email"]
 
 
 class AreaDirectorSerializer(serializers.Serializer):
@@ -25,7 +51,7 @@ class AreaDirectorSerializer(serializers.Serializer):
 
     @extend_schema_field(serializers.CharField)
     def get_name(self, instance: Email | Role):
-        person = getattr(instance, 'person', None)
+        person = getattr(instance, "person", None)
         return person.plain_name() if person else None
 
     @extend_schema_field(serializers.EmailField)
