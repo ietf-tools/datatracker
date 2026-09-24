a4d099942b3d4ec40ea7ea325d5357ff86d1c7ad
Jennifer Richards <jennifer@staff.ietf.org>
2026-09-23 21:59:19 -0300

    fix: avoid null group state in API (#11816)
    * fix: avoid null group state in API
    
    * fix: lint

--- ietf/group/tests_serializers.py
+++ ietf/group/tests_serializers.py
@@ -1,5 +1,5 @@
 # Copyright The IETF Trust 2026, All Rights Reserved
-from ietf.group.factories import RoleFactory, GroupFactory
+from ietf.group.factories import GroupFactory, RoleFactory
 from ietf.group.serializers import (
     AreaDirectorSerializer,
     AreaSerializer,
@@ -29,7 +29,7 @@ class GroupSerializerTests(TestCase):
             wg.save()
             self.assertEqual(
                 GroupSerializer(wg).data,
-                partial_expected_json | {"state": "conclude"},
+                partial_expected_json | {"state": "concluded"},
             )
         for state in GroupStateName.objects.filter(used=True).exclude(
             slug__in=["active", "bof-conc", "conclude"]
@@ -45,7 +45,7 @@ class GroupSerializerTests(TestCase):
         wg.save()
         self.assertEqual(
             GroupSerializer(wg).data,
-            partial_expected_json | {"state": None},
+            partial_expected_json | {"state": "other"},
         )
 
 
