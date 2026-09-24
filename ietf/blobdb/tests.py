789d985d7a537f44aaaa16001494d4c22ec41530
Jennifer Richards <jennifer@staff.ietf.org>
2026-09-23 23:23:40 -0300

    test: tests that blob replication is queued

--- ietf/blobdb/tests.py
+++ ietf/blobdb/tests.py
@@ -1,13 +1,18 @@
 # Copyright The IETF Trust 2025-2026, All Rights Reserved
 import datetime
 from hashlib import sha384
+from unittest import mock
 
+from django.contrib import admin
 from django.core.files.base import ContentFile
 
 from ietf.utils.test_utils import TestCase
+
+from .admin import BlobAdmin
+from .apps import get_blobdb
 from .factories import BlobFactory
 from .models import Blob
-from .storage import BlobFile, BlobdbStorage
+from .storage import BlobdbStorage, BlobFile
 
 
 class StorageTests(TestCase):
@@ -118,3 +123,50 @@ class BlobModelTests(TestCase):
         self.assertEqual(stored.content_type, "application/octet-stream")
         self.assertEqual(bytes(stored.content), b"original")
         self.assertEqual(stored.checksum, self._digest(b"original"))
+
+
+class ReplicationQueuingTests(TestCase):
+    def test_save_queues_replication(self):
+        blob = BlobFactory()
+        with mock.patch("ietf.blobdb.models.queue_for_replication") as mock_queue:
+            blob.content = b"replaced"
+            blob.save()
+        mock_queue.assert_called_once_with(blob.bucket, blob.name, using=get_blobdb())
+
+    def test_partial_save_queues_replication(self):
+        blob = BlobFactory()
+        with mock.patch("ietf.blobdb.models.queue_for_replication") as mock_queue:
+            blob.content_type = "text/plain"
+            blob.save(update_fields=["content_type"])
+        mock_queue.assert_called_once_with(blob.bucket, blob.name, using=get_blobdb())
+
+    def test_delete_queues_replication(self):
+        blob = BlobFactory()
+        bucket, name = blob.bucket, blob.name
+        with mock.patch("ietf.blobdb.models.queue_for_replication") as mock_queue:
+            blob.delete()
+        mock_queue.assert_called_once_with(bucket, name, using=get_blobdb())
+
+    def test_storage_force_replication(self):
+        storage = BlobdbStorage(bucket_name="my-bucket")
+        with mock.patch("ietf.blobdb.storage.queue_for_replication") as mock_queue:
+            storage.force_replication("myfile.txt")
+        mock_queue.assert_called_once_with(bucket="my-bucket", name="myfile.txt")
+
+    def test_admin_replicate_blob_action(self):
+        blobs = BlobFactory.create_batch(2)
+        model_admin = BlobAdmin(Blob, admin.site)
+        with (
+            mock.patch("ietf.blobdb.admin.queue_for_replication") as mock_queue,
+            mock.patch.object(model_admin, "message_user"),
+        ):
+            model_admin.replicate_blob(
+                request=None, queryset=Blob.objects.filter(pk__in=[b.pk for b in blobs])
+            )
+        self.assertCountEqual(
+            mock_queue.call_args_list,
+            [
+                mock.call(bucket=b.bucket, name=b.name, using=get_blobdb())
+                for b in blobs
+            ],
+        )
